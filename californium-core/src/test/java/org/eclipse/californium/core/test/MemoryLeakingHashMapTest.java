f411cce733baeeaefd30d5f32b76b6442fccaa48
Javimka <martin.lanter@gmx.ch>
2014-07-02 03:04:53 +0200

    Converted test case for memory leaking hashmaps into a proper JUnit Test.

--- californium-core/src/test/java/org/eclipse/californium/core/test/MemoryLeakingHashMapTest.java
+++ californium-core/src/test/java/org/eclipse/californium/core/test/MemoryLeakingHashMapTest.java
@@ -6,8 +6,6 @@ import java.net.InetAddress;
 import java.net.InetSocketAddress;
 import java.util.concurrent.Semaphore;
 
-import javax.swing.JButton;
-import javax.swing.JFrame;
 import javax.swing.Timer;
 
 import junit.framework.Assert;
@@ -26,90 +24,52 @@ import org.eclipse.californium.core.coap.Response;
 import org.eclipse.californium.core.network.CoAPEndpoint;
 import org.eclipse.californium.core.network.config.NetworkConfig;
 import org.eclipse.californium.core.network.config.NetworkConfigDefaults;
-import org.eclipse.californium.core.network.interceptors.MessageTracer;
 import org.eclipse.californium.core.server.resources.CoapExchange;
-import org.eclipse.californium.core.test.MemoryLeakingHashMapTest.TestResource.Mode;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
 
 public class MemoryLeakingHashMapTest {
 
 	// Configuration for this test
-	public static final int TEST_EXCHANGE_LIFECYCLE = 2470; // 2.47 seconds
-	public static final int TEST_SWEEP_DEDUPLICATOR_INTERVAL = 1000; // 1 second
+	public static final int TEST_EXCHANGE_LIFECYCLE = 247; // 0.247 seconds
+	public static final int TEST_SWEEP_DEDUPLICATOR_INTERVAL = 100; // 1 second
 	public static final int TEST_BLOCK_SIZE = 16; // 16 bytes
 
-	public static final int OBS_NOTIFICATION_INTERVALL = 500; // send one notification per 500 ms
+	public static final int OBS_NOTIFICATION_INTERVALL = 50; // send one notification per 500 ms
 	public static final int HOW_MANY_NOTIFICATION_WE_WAIT_FOR = 3;
-	
-	public static final int SERVER_PORT = 5683;
-	public static final String SERVER_URI = "coap://localhost:"+SERVER_PORT;
-	
+
 	// The names of the two resources of the server
 	public static final String PIGGY = "piggy";
 	public static final String SEPARATE = "separate";
 	
 	// The server endpoint that we test
-	private static CoAPEndpoint serverEndpoint;
-	private static CoAPEndpoint clientEndpoint;
-	private static EndpointSurveillant serverSurveillant;
-	private static EndpointSurveillant clientSurveillant;
+	private CoAPEndpoint serverEndpoint;
+	private CoAPEndpoint clientEndpoint;
+	private EndpointSurveillant serverSurveillant;
+	private EndpointSurveillant clientSurveillant;
 
-	private static String currentRequestText;
-	private static String currentResponseText;
+	private String currentRequestText;
+	private String currentResponseText;
 	
-	public static void main(String[] args) throws Exception {
+	private CoapServer server;
+	private int serverPort;
 		
-		NetworkConfig config = new NetworkConfig()
-			// We make sure that the sweep deduplicator is used
-			.setString(NetworkConfigDefaults.DEDUPLICATOR, NetworkConfigDefaults.DEDUPLICATOR_MARK_AND_SWEEP)
-			.setInt(NetworkConfigDefaults.MARK_AND_SWEEP_INTERVAL, TEST_EXCHANGE_LIFECYCLE)
-			.setLong(NetworkConfigDefaults.EXCHANGE_LIFECYCLE, TEST_SWEEP_DEDUPLICATOR_INTERVAL)
-			
-			// We set the block size to 16 bytes
-			.setInt(NetworkConfigDefaults.MAX_MESSAGE_SIZE, TEST_BLOCK_SIZE)
-			.setInt(NetworkConfigDefaults.DEFAULT_BLOCK_SIZE, TEST_BLOCK_SIZE);
-		
-		// Create the endpoint for the server and create surveillant
-		serverEndpoint = new CoAPEndpoint(
-				new InetSocketAddress((InetAddress) null, SERVER_PORT), config);
-//		serverEndpoint.addInterceptor(new MessageTracer());
-		serverSurveillant = new EndpointSurveillant("server", serverEndpoint);
-		
-		clientEndpoint = new CoAPEndpoint(config);
-		clientEndpoint.addInterceptor(new MessageTracer());
-		clientEndpoint.start();
-		clientSurveillant = new EndpointSurveillant("client", clientEndpoint);
-		
-		// Create a server with two resources: one that sends piggy-backed
-		// responses and one that sends separate responses
-		CoapServer server = new CoapServer(config);
-		server.addEndpoint(serverEndpoint);
-		server.add(new TestResource(PIGGY,    Mode.PIGGY_BACKED_RESPONSE));
-		server.add(new TestResource(SEPARATE, Mode.SEPARATE_RESPONE));
-		server.start();
-		
-		/* TODO: remove*/ {
-		JButton btnStatus = new JButton("Print Status");
-		btnStatus.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent e) {
-				System.out.println("\nServer:");
-				serverSurveillant.printHashmaps();
-				
-				System.out.println("\nClient:");
-				clientSurveillant.printHashmaps();
-			}
-		});
-		
-		JFrame frame = new JFrame("Memory Debug Helper");
-		frame.add(btnStatus);
-		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		frame.setSize(400, 100);
-		frame.setLocation(200, 200);
-		frame.setVisible(true); }
-
-		testServer();
+	private Timer timer;
+	
+	@Before
+	public void startupServer() throws Exception {
+		System.out.println("\nStart "+getClass().getSimpleName());
+		createServerAndClientEntpoints();
 	}
 	
-	private static void testServer() throws Exception {
+	@After
+	public void shutdownServer() {
+		server.destroy();
+		System.out.println("End "+getClass().getSimpleName());
+	}
+	@Test
+	public void testServer() throws Exception {
 		testSimpleNONGet(uriOf(PIGGY));
 		
 		testSimpleGet(uriOf(PIGGY));
@@ -118,13 +78,10 @@ public class MemoryLeakingHashMapTest {
 		testBlockwise(uriOf(PIGGY));
 		testBlockwise(uriOf(SEPARATE));
 		
-		// TODO Bug: The NON notifications are still in exchangesByMID
-//		testObserve(uriOf(PIGGY));
-		
-		System.out.println("success");
+		testObserve(uriOf(PIGGY));
 	}
 	
-	private static void testSimpleNONGet(String uri) throws Exception {
+	private void testSimpleNONGet(String uri) throws Exception {
 		System.out.println("Test simple NON GET to "+uri);
 		
 		Request request = Request.newGet();
@@ -141,7 +98,7 @@ public class MemoryLeakingHashMapTest {
 		clientSurveillant.assertHashMapsEmpty();
 	}
 	
-	private static void testSimpleGet(String uri) throws Exception {
+	private void testSimpleGet(String uri) throws Exception {
 		System.out.println("Test simple GET to "+uri);
 		
 		CoapClient client = new CoapClient(uri);
@@ -156,7 +113,7 @@ public class MemoryLeakingHashMapTest {
 		clientSurveillant.assertHashMapsEmpty();
 	}
 	
-	private static void testBlockwise(String uri) throws Exception {
+	private void testBlockwise(String uri) throws Exception {
 		System.out.println("Test blockwise POST to "+uri);
 		
 		CoapClient client = new CoapClient(uri);
@@ -173,7 +130,7 @@ public class MemoryLeakingHashMapTest {
 		clientSurveillant.assertHashMapsEmpty();
 	}
 	
-	private static void testObserve(final String uri) throws Exception {
+	private void testObserve(final String uri) throws Exception {
 		System.out.println("Test observe relation to "+uri);
 		
 		// We use a semaphore to return after the test has completed
@@ -224,17 +181,48 @@ public class MemoryLeakingHashMapTest {
 		clientSurveillant.assertHashMapsEmpty();
 	}
 	
-	private static String uriOf(String resourcePath) {
-		return SERVER_URI + "/" + resourcePath;
+	private void createServerAndClientEntpoints() throws Exception {
+		timer = new Timer(OBS_NOTIFICATION_INTERVALL, null);
+		
+		NetworkConfig config = new NetworkConfig()
+			// We make sure that the sweep deduplicator is used
+			.setString(NetworkConfigDefaults.DEDUPLICATOR, NetworkConfigDefaults.DEDUPLICATOR_MARK_AND_SWEEP)
+			.setInt(NetworkConfigDefaults.MARK_AND_SWEEP_INTERVAL, TEST_EXCHANGE_LIFECYCLE)
+			.setLong(NetworkConfigDefaults.EXCHANGE_LIFECYCLE, TEST_SWEEP_DEDUPLICATOR_INTERVAL)
+			
+			// We set the block size to 16 bytes
+			.setInt(NetworkConfigDefaults.MAX_MESSAGE_SIZE, TEST_BLOCK_SIZE)
+			.setInt(NetworkConfigDefaults.DEFAULT_BLOCK_SIZE, TEST_BLOCK_SIZE);
+		
+		// Create the endpoint for the server and create surveillant
+		serverEndpoint = new CoAPEndpoint(new InetSocketAddress((InetAddress) null, 0), config);
+		serverSurveillant = new EndpointSurveillant("server", serverEndpoint);
+		
+		clientEndpoint = new CoAPEndpoint(config);
+		clientEndpoint.start();
+		clientSurveillant = new EndpointSurveillant("client", clientEndpoint);
+		
+		// Create a server with two resources: one that sends piggy-backed
+		// responses and one that sends separate responses
+
+		server = new CoapServer(config);
+		server.addEndpoint(serverEndpoint);
+		server.add(new TestResource(PIGGY,    Mode.PIGGY_BACKED_RESPONSE));
+		server.add(new TestResource(SEPARATE, Mode.SEPARATE_RESPONE));
+		server.start();
+		serverPort = serverEndpoint.getAddress().getPort();
+		
+		timer.start();
 	}
 	
-	public static class TestResource extends CoapResource implements ActionListener {
+	private String uriOf(String resourcePath) {
+		return "coap://localhost:" + serverPort + "/" + resourcePath;
+	}
+	
+	public enum Mode { PIGGY_BACKED_RESPONSE, SEPARATE_RESPONE; }
+	
+	public class TestResource extends CoapResource implements ActionListener {
 
-		public enum Mode { PIGGY_BACKED_RESPONSE, SEPARATE_RESPONE; }
-		
-		private static Timer timer = new Timer(OBS_NOTIFICATION_INTERVALL, null);
-		static { timer.start(); }
-		
 		private Mode mode;
 		private int status;
 		
