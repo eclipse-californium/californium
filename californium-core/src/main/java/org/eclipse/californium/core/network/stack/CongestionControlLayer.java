7857a1dd1556dd8beb22a7a273c20748797ddc49
August Betzler <august.betzler@entel.upc.edu>
2014-09-29 14:52:20 +0200

    - Included dithering in the calculation of the initial RTO for a transmissions (was missing after restructuring project)

--- californium-core/src/main/java/org/eclipse/californium/core/network/stack/CongestionControlLayer.java
+++ californium-core/src/main/java/org/eclipse/californium/core/network/stack/CongestionControlLayer.java
@@ -146,7 +146,7 @@ public class CongestionControlLayer extends ReliabilityLayer {
 	private boolean checkNSTART(Exchange exchange){	
 		getRemoteEndpoint(exchange).checkForDeletedExchanges();
 		if(getRemoteEndpoint(exchange).getNumberOfOngoingExchanges(exchange) < config.getInt("NSTART")){
-			System.out.println("Processing exchange (NSTART OK!)");
+			//System.out.println("Processing exchange (NSTART OK!)");
 			
 			// NSTART allows to start the exchange, proceed normally
 			getRemoteEndpoint(exchange).registerExchange(exchange, calculateVBF(getRemoteEndpoint(exchange).getRTO()));
@@ -156,17 +156,17 @@ public class CongestionControlLayer extends ReliabilityLayer {
 			return true;
 		}else{
 			// NSTART does not allow any further parallel exchanges towards the remote endpoint
-			System.out.println("Nstart does not allow further exchanges with " + getRemoteEndpoint(exchange).getRemoteAddress().toString());
+			//System.out.println("Nstart does not allow further exchanges with " + getRemoteEndpoint(exchange).getRemoteAddress().toString());
 			
 			// Check if the queue limit for exchanges is already reached
 			if(getRemoteEndpoint(exchange).getConfirmableQueue().size() == EXCHANGELIMIT){
 				// Request cannot be queued TODO: does this trigger some feedback for other layers?
-				System.out.println("Confirmable exchange queue limit reached! Message dropped...");
+				//System.out.println("Confirmable exchange queue limit reached! Message dropped...");
 				
 			}else{
 				// Queue exchange in the CON-Queue
 				getRemoteEndpoint(exchange).getConfirmableQueue().add(exchange);		
-				System.out.println("Added exchange to the queue (NSTART limit reached)");		
+				//System.out.println("Added exchange to the queue (NSTART limit reached)");		
 			}
 		}		
 		return false;
@@ -298,13 +298,18 @@ public class CongestionControlLayer extends ReliabilityLayer {
 	@Override
 	protected void prepareRetransmission(Exchange exchange, RetransmissionTask task) {
 		int timeout, expectedmaxduration;
-		System.out.println("TRXCount: " + exchange.getFailedTransmissionCount());
+		System.out.println("TXCount: " + exchange.getFailedTransmissionCount());
 		if (exchange.getFailedTransmissionCount() == 0) {
 			timeout = (int)getRemoteEndpoint(exchange).getRTO();	
-			
+			// Apply dithering by randomly choosing RTO from [RTO, RTO * 1.5]
+			float ack_random_factor = config.getFloat(NetworkConfigDefaults.ACK_RANDOM_FACTOR);
+			timeout = getRandomTimeout(timeout, (int) (timeout*ack_random_factor));
+			System.out.println("meanrto:" + timeout + ";" + System.currentTimeMillis());
 		} else {
 				int tempTimeout= (int)(getRemoteEndpoint(exchange).getExchangeVBF(exchange) * exchange.getCurrentTimeout());
 				timeout = (tempTimeout < MAX_RTO) ? tempTimeout : MAX_RTO;
+				getRemoteEndpoint(exchange).setCurrentRTO(timeout);
+				System.out.println("RTX");
 		}
 		exchange.setCurrentTimeout(timeout);
 		expectedmaxduration = calculateMaxTransactionDuration(exchange);
