0c75d626c1a58603ca94d5532f1df9fc8a32e133
abetzler <august.betzler@entel.upc.edu>
2014-10-01 21:19:45 +0200

    - Added license headers

--- californium-core/src/main/java/org/eclipse/californium/core/network/RemoteEndpointManager.java
+++ californium-core/src/main/java/org/eclipse/californium/core/network/RemoteEndpointManager.java
@@ -1,3 +1,20 @@
+/*******************************************************************************
+ * Copyright (c) 2014 Wireless Networks Group, UPC Barcelona and i2CAT.
+ * 
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * and Eclipse Distribution License v1.0 which accompany this distribution.
+ * 
+ * The Eclipse Public License is available at
+ *    http://www.eclipse.org/legal/epl-v10.html
+ * and the Eclipse Distribution License is available at
+ *    http://www.eclipse.org/org/documents/edl-v10.html.
+ * 
+ * Contributors:
+ *    August Betzler    – CoCoA implementation
+ *    Matthias Kovatsch - Embedding of CoCoA in Californium
+ ******************************************************************************/
+
 package org.eclipse.californium.core.network;
 
 import java.net.InetAddress;
