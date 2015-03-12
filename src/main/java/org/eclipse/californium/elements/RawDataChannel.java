fb3e8abdd53b6cf534d53816f6ad081586aa03f4
Kai Hudalla <kai.hudalla@bosch-si.com>
2015-03-12 16:04:44 +0100

    Improved documentation
    Rewrote JavaDocs to better describe RawDataChannel's purpose in the
    context of the generic element connector framework.
    
    Signed-off-by: Kai Hudalla <kai.hudalla@bosch-si.com>

--- src/main/java/org/eclipse/californium/elements/RawDataChannel.java
+++ src/main/java/org/eclipse/californium/elements/RawDataChannel.java
@@ -13,23 +13,36 @@
  * Contributors:
  *    Matthias Kovatsch - creator and main architect
  *    Martin Lanter - architect and initial implementation
+ *    Kai Hudalla (Bosch Software Innovations GmbH) - Documentation improvements
  ******************************************************************************/
 package org.eclipse.californium.elements;
 
 /**
- * This is the interface needed between a CoAP stack and a connector. The
- * connector forwards raw data to the method receiveData() and the CoAP stack
- * forwards messages to the corresponding method sendX(). 
+ * A processor for messages received from the network layer.
+ * 
+ * Applications should register an implementation of this interface with
+ * a <code>Connector</code> via its {@link Connector#setRawDataReceiver(RawDataChannel)}
+ * method in order to get notified about incoming messages.
+ * 
+ * Applications should use the {@link Connector#send(RawData)} method to send
+ * messages to receivers connected via the network.
  */
 public interface RawDataChannel {
 
 	/**
-	 * Forwards the specified data to the stack. First, they must be parsed. Second, the
-	 * matcher finds the corresponding exchange and finally, the stack will
-	 * process the message.
+	 * Processes a raw message received from the network.
+	 * 
+	 * It is assumed that an implementation can either derive the message format
+	 * by introspection or knows upfront about the message format to expect.
+	 * 
+	 * An implementation of this method should return quickly in order to improve
+	 * message processing throughput. In cases where processing of a message is expected
+	 * to take some time, implementations should consider off-loading the processing of the
+	 * messages to a separate <code>Thread</code>, e.g. by employing a
+	 * <code>java.util.concurrent.ExecutorService</code>.
 	 * 
 	 * @param raw
-	 *            the raw data
+	 *            the raw message to process
 	 */
 	public void receiveData(RawData raw);
 
