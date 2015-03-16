98aaaa4f9172063aa698daa83eefe64899b8cad7
Kai Hudalla <kai.hudalla@bosch-si.com>
2015-03-16 18:04:42 +0100

    Provide access to (optional) client identity.
    - added property for passing on an authenticated client's identity
    - added convenience constructors
    
    Bug: 462267 
    Signed-off-by: Kai Hudalla <kai.hudalla@bosch-si.com>

--- src/main/java/org/eclipse/californium/elements/RawData.java
+++ src/main/java/org/eclipse/californium/elements/RawData.java
@@ -13,39 +13,50 @@
  * Contributors:
  *    Matthias Kovatsch - creator and main architect
  *    Martin Lanter - architect and initial implementation
+ *    Kai Hudalla (Bosch Software Innovations GmbH) - several additions and improvements
  ******************************************************************************/
 package org.eclipse.californium.elements;
 
 import java.net.InetAddress;
 import java.net.InetSocketAddress;
+import java.security.Principal;
+import java.util.Arrays;
 
 /**
  * A container object for the data received or sent via a <code>Connector</code>.
  * 
- * In addition to the raw bytes the source or destination address and port are contained as well.
+ * The following meta-data is included:
+ * <ul>
+ * <li>the source/destination IP address</li>
+ * <li>the source/destination port</li>
+ * <li>a flag indicating whether the message is a multicast message (default is
+ * <code>false</code>)</li>
+ * </ul>
+ * 
+ * A message received from a client via the network may also optionally contain the
+ * authenticated sender's identity as a <code>java.security.Principal</code> object.
  */
 public class RawData {
 
-	/** The serialized message. */
+	/** The raw message. */
 	public final byte[] bytes;
 	
-	/** The address. */
-	private InetAddress address;
+	/** The source/destination address. */
+	private InetSocketAddress address;
 	
-	/** The port. */
-	private int port;
-	
-	/** Indicates if this message was a multicast message */
+	/** Indicates if this message is a multicast message */
 	private boolean multicast;
+	
+	private Principal senderIdentity;
 	
 	/**
 	 * Instantiates a new raw data.
 	 *
-	 * @param bytes the bytes
+	 * @param data the data that is to be sent or has been received
 	 * @deprecated Use one of the other constructors instead.
 	 */
-	public RawData(byte[] bytes) {
-		this(bytes, null, 0);
+	public RawData(byte[] data) {
+		this(data, null, 0, null, false);
 	}
 	
 	/**
@@ -56,7 +67,20 @@ public class RawData {
 	 * @throws NullPointerException if any of the given parameters is <code>null</code>
 	 */
 	public RawData(byte[] data, InetSocketAddress address) {
-		this(data, address.getAddress(), address.getPort());
+		this(data, address, null, false);
+	}
+	
+	/**
+	 * Instantiates a new raw data.
+	 *
+	 * @param data the data that is to be sent or has been received
+	 * @param address the IP address and port the data is to be sent to or has been received from
+	 * @param clientIdentity the identity of the authenticated sender of the message
+	 *     (or <code>null</code> if sender is not authenticated)
+	 * @throws NullPointerException if any of the given parameters is <code>null</code>
+	 */
+	public RawData(byte[] data, InetSocketAddress address, Principal clientIdentity) {
+		this(data, address, clientIdentity, false);
 	}
 	
 	/**
@@ -68,7 +92,21 @@ public class RawData {
 	 * @throws NullPointerException if data is <code>null</code>
 	 */
 	public RawData(byte[] data, InetAddress address, int port) {
-		this(data, address, port, false);
+		this(data, address, port, null, false);
+	}
+	
+	/**
+	 * Instantiates a new raw data.
+	 *
+	 * @param data the data that is to be sent or has been received
+	 * @param address the IP address the data is to be sent to or has been received from
+	 * @param port the port the data is to be sent to or has been received from
+	 * @param clientIdentity the identity of the authenticated sender of the message
+	 *     (or <code>null</code> if sender is not authenticated)
+	 * @throws NullPointerException if data is <code>null</code>
+	 */
+	public RawData(byte[] data, InetAddress address, int port, Principal clientIdentity) {
+		this(data, address, port, clientIdentity, false);
 	}
 	
 	/**
@@ -80,9 +118,32 @@ public class RawData {
 	 * @throws NullPointerException if data or address is <code>null</code>
 	 */
 	public RawData(byte[] data, InetSocketAddress address, boolean multicast) {
-		this(data, address.getAddress(), address.getPort(), multicast);
+		this(data, address, null, multicast);
 	}
 
+	/**
+	 * Instantiates a new raw data.
+	 *
+	 * @param data the data that is to be sent or has been received
+	 * @param address the IP address and port the data is to be sent to or has been received from
+	 * @param clientIdentity the identity of the authenticated sender of the message
+	 *     (or <code>null</code> if sender is not authenticated)
+	 * @param multicast indicates whether the data represents a multicast message
+	 * @throws NullPointerException if data or address is <code>null</code>
+	 */
+	public RawData(byte[] data, InetSocketAddress address, Principal clientIdentity, boolean multicast) {
+		if (data == null) {
+			throw new NullPointerException("Data must not be null");
+		}
+		if (address == null) {
+			throw new NullPointerException("Address must not be null");
+		}
+		this.bytes = data;
+		this.address = address;
+		this.senderIdentity = clientIdentity;
+		this.multicast = multicast;
+	}
+	
 	/**
 	 * Instantiates a new raw data.
 	 *
@@ -93,21 +154,31 @@ public class RawData {
 	 * @throws NullPointerException if data is <code>null</code>
 	 */
 	public RawData(byte[] data, InetAddress address, int port, boolean multicast) {
-		if (data == null)
-			throw new NullPointerException();
-		this.bytes = data;
-		this.address = address;
-		this.port = port;
-		this.multicast = multicast;
+		this(data, address, port, null, multicast);
 	}
 	
 	/**
-	 * Gets the serialized message.
+	 * Instantiates a new raw data.
 	 *
-	 * @return the bytes
+	 * @param data the data that is to be sent or has been received
+	 * @param address the IP address the data is to be sent to or has been received from
+	 * @param port the port the data is to be sent to or has been received from
+	 * @param clientIdentity the identity of the authenticated sender of the message
+	 *     (or <code>null</code> if sender is not authenticated)
+	 * @param multicast indicates whether the data represents a multicast message
+	 * @throws NullPointerException if data is <code>null</code>
+	 */
+	public RawData(byte[] data, InetAddress address, int port, Principal clientIdentity, boolean multicast) {
+		this(data, new InetSocketAddress(address, port), clientIdentity, multicast);
+	}
+	
+	/**
+	 * Gets the raw message.
+	 *
+	 * @return a copy of the raw message bytes
 	 */
 	public byte[] getBytes() {
-		return bytes;
+		return Arrays.copyOf(bytes, bytes.length);
 	}
 	
 	/**
@@ -125,7 +196,7 @@ public class RawData {
 	 * @return the address
 	 */
 	public InetAddress getAddress() {
-		return address;
+		return address.getAddress();
 	}
 
 	/**
@@ -134,8 +205,8 @@ public class RawData {
 	 * @param address the new address
 	 * @deprecated Use constructor instead.
 	 */
-	public void setAddress(InetAddress address) {
-		this.address = address;
+	public void setAddress(InetAddress newAddress) {
+		this.address = new InetSocketAddress(newAddress, address.getPort());
 	}
 
 	/**
@@ -144,7 +215,7 @@ public class RawData {
 	 * @return the port
 	 */
 	public int getPort() {
-		return port;
+		return address.getPort();
 	}
 
 	/**
@@ -154,7 +225,7 @@ public class RawData {
 	 * @deprecated Use constructor instead.
 	 */
 	public void setPort(int port) {
-		this.port = port;
+		this.address = new InetSocketAddress(address.getAddress(), port);
 	}
 
 	/**
@@ -177,11 +248,24 @@ public class RawData {
 	}
 	
 	/**
-	 * Gets the address as {@link InetSocketAddress}.
+	 * Gets the source/destination IP address and port.
 	 *
-	 * @return the endpoint address
+	 * @return the address
 	 */
 	public InetSocketAddress getInetSocketAddress() {
-		return new InetSocketAddress(address, port);
+		return address;
+	}
+	
+	/**
+	 * Gets the identity of the sender of the message.
+	 * 
+	 * This property is only meaningful for messages
+	 * received from a client.
+	 * 
+	 * @return the identity or <code>null</code> if the
+	 *      sender has not been authenticated
+	 */
+	public Principal getSenderIdentity() {
+		return senderIdentity;
 	}
 }
