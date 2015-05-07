7eb3e4d218b66498b636598f6ebd8bb0e8302c2e
Kai Hudalla <kai.hudalla@bosch-si.com>
2015-05-07 13:27:37 +0200

    Add missing copyright/license header.

--- src/test/java/org/eclipse/californium/scandium/auth/RawPublicKeyIdentityTest.java
+++ src/test/java/org/eclipse/californium/scandium/auth/RawPublicKeyIdentityTest.java
@@ -1,3 +1,18 @@
+/*******************************************************************************
+ * Copyright (c) 2015 Bosch Software Innovations GmbH and others.
+ * 
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * and Eclipse Distribution License v1.0 which accompany this distribution.
+ * 
+ * The Eclipse Public License is available at
+ *    http://www.eclipse.org/legal/epl-v10.html
+ * and the Eclipse Distribution License is available at
+ *    http://www.eclipse.org/org/documents/edl-v10.html.
+ * 
+ * Contributors:
+ *    Kai Hudalla (Bosch Software Innovations GmbH) - Initial creation
+ ******************************************************************************/
 package org.eclipse.californium.scandium.auth;
 
 import static org.junit.Assert.*;
@@ -24,5 +39,4 @@ public class RawPublicKeyIdentityTest {
 		RawPublicKeyIdentity id = new RawPublicKeyIdentity(key);
 		assertArrayEquals(id.getKey().getEncoded(), id.getSubjectInfo());
 	}
-
 }
