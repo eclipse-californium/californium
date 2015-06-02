3d17b9bdccbe0a4226b63d00d70e58e03d3ca1c8
Kai Hudalla <kai.hudalla@bosch-si.com>
2015-06-02 17:22:04 +0200

    Add missing copyright header.

--- src/test/java/org/eclipse/californium/scandium/dtls/ClientHelloTest.java
+++ src/test/java/org/eclipse/californium/scandium/dtls/ClientHelloTest.java
@@ -1,3 +1,18 @@
+/*******************************************************************************
+ * Copyright (c) 2015 Bosch Software Innovations GmbH and others.
+ * 
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * and Eclipse Distribution License v1.0 which accompany this distribution.
+ * 
+ * The Eclipse Public License is available at
+ *    http://www.eclipse.org/legal/epl-v10.html
+ * and the Eclipse Distribution License is available at
+ *    http://www.eclipse.org/org/documents/edl-v10.html.
+ * 
+ * Contributors:
+ *    Kai Hudalla (Bosch Software Innovations GmbH) - initial creation
+ ******************************************************************************/
 package org.eclipse.californium.scandium.dtls;
 
 import static org.hamcrest.CoreMatchers.is;
