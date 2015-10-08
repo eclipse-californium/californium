4b552feeaa16f96b5f855dbd41c71c9e6e904ff1
Matthias Kovatsch <kovatsch@inf.ethz.ch>
2015-10-08 21:32:43 +0200

    Copyright year update.

--- californium-core/src/main/java/org/eclipse/californium/core/network/stack/congestioncontrol/PeakhopperRto.java
+++ californium-core/src/main/java/org/eclipse/californium/core/network/stack/congestioncontrol/PeakhopperRto.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2014 Wireless Networks Group, UPC Barcelona and i2CAT.
+ * Copyright (c) 2015 Wireless Networks Group, UPC Barcelona and i2CAT.
  * 
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
--- cf-cocoa/src/main/java/org/eclipse/californium/examples/CocoaClient.java
+++ cf-cocoa/src/main/java/org/eclipse/californium/examples/CocoaClient.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2014 Institute for Pervasive Computing, ETH Zurich and others.
+ * Copyright (c) 2015 Institute for Pervasive Computing, ETH Zurich and others.
  * 
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
