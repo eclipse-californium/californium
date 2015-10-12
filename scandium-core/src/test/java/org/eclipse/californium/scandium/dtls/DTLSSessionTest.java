5e948fb8fe6bd0a5458e4c420d7876ca33d09d6c
Kai Hudalla <kai.hudalla@bosch-si.com>
2015-10-12 17:59:36 +0200

    Calculate max fragment size based on (P)MTU, explicit value provided by peer and current write state.
    This is in preparation for supporting the max_fragment_length Hello
    extension as described in #473351.

--- scandium-core/src/test/java/org/eclipse/californium/scandium/dtls/DTLSSessionTest.java
+++ scandium-core/src/test/java/org/eclipse/californium/scandium/dtls/DTLSSessionTest.java
@@ -15,10 +15,14 @@
  ******************************************************************************/
 package org.eclipse.californium.scandium.dtls;
 
+import static org.hamcrest.CoreMatchers.*;
+import static org.junit.Assert.*;
+
+import java.net.InetAddress;
 import java.net.InetSocketAddress;
 
 import org.eclipse.californium.scandium.category.Small;
-import org.junit.Assert;
+import org.eclipse.californium.scandium.dtls.cipher.CipherSuite;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.experimental.categories.Category;
@@ -26,25 +30,78 @@ import org.junit.experimental.categories.Category;
 @Category(Small.class)
 public class DTLSSessionTest {
 
+	static final int DEFAULT_MAX_FRAGMENT_LENGTH = 16384; //2^14 as defined in DTLS 1.2 spec
 	InetSocketAddress peerAddress;
 	DTLSSession session;
-	
+
 	@Before
 	public void setUp() throws Exception {
-		peerAddress = new InetSocketAddress(5000);
+		peerAddress = new InetSocketAddress(InetAddress.getLocalHost(), 0);
 		session = new DTLSSession(peerAddress, false);
+	}
+
+	@Test
+	public void testDefaultMaxFragmentLengthCompliesWithSpec() {
+		// when instantiating a default server session
+		session = new DTLSSession(peerAddress, false);
+
+		// then the max fragment size is as specified in DTLS spec
+		assertThat(session.getMaxFragmentLength(), is(DEFAULT_MAX_FRAGMENT_LENGTH));
+	}
+
+	@Test
+	public void testMaxFragmentLengthIsAdjustedToMtu() {
+		// given an ethernet network interface
+		int mtu = 1500;
+
+		// when setting the session's maximumTransmissionUnit property
+		session.setMaxTransmissionUnit(mtu);
+
+		// then the maxFragmentLength is as adjusted so that a fragment
+		// fits into a single unfragmented UDP datagram
+		assertAnyFragmentFitsIntoUnfragmentedDatagram(mtu);
+	}
+
+	@Test
+	public void testMaxFragmentLengthIsAdjustedToCipherSuite() {
+		// given a handshake over an ethernet connection (MTU 1500 bytes)
+		int mtu = 1500;
+		session.setMaxTransmissionUnit(mtu);
+		int initialMaxFragmentLength = session.getMaxFragmentLength();
+
+		// when negotiating a cipher suite introducing ciphertext expansion
+		CipherSuite cipherSuite = CipherSuite.TLS_ECDHE_ECDSA_WITH_AES_128_CBC_SHA256;
+		DTLSConnectionState newWriteState =
+				new DTLSConnectionState(cipherSuite, CompressionMethod.NULL, null, null, null);
+		session.setWriteState(newWriteState);
+
+		// then the maxFragmentLength is adjusted so that any fragment using
+		// current write state fits into a single unfragmented UDP datagram
+		assertAnyFragmentFitsIntoUnfragmentedDatagram(mtu);
+		assertTrue(session.getMaxFragmentLength() < initialMaxFragmentLength);
+	}
+
+	private void assertAnyFragmentFitsIntoUnfragmentedDatagram(int mtu) {
+		int datagramSize = session.getMaxFragmentLength()
+				+ session.getWriteState().getMaxCiphertextExpansion()
+				+ 12 // DTLS session headers
+				+ 13 // DTLS record headers
+				+ 8 // UDP headers
+				+ 20; // IP headers
+		assertTrue(datagramSize <= mtu);
+		assertThat(session.getMaxDatagramSize(), is(datagramSize));
 	}
 
 	@Test
 	public void testRecordFromPreviousEpochIsDiscarded() {
 		session.setReadEpoch(1);
-		Assert.assertFalse(session.isRecordProcessable(0, 15));
+		assertFalse(session.isRecordProcessable(0, 15));
 	}
 
 	@Test
 	public void testRecordFromFutureEpochIsDiscarded() {
 		session.setReadEpoch(1);
-		Assert.assertFalse(session.isRecordProcessable(2, 15));
+		assertFalse(session.isRecordProcessable(2, 15));
 	}
 
 	@Test
@@ -53,45 +110,44 @@ public class DTLSSessionTest {
 		session.setReadEpoch(epoch);
 		session.markRecordAsRead(epoch, 0);
 		session.markRecordAsRead(epoch, 2);
-		Assert.assertFalse(session.isRecordProcessable(0, 0));
-		Assert.assertTrue(session.isRecordProcessable(0, 1));
-		Assert.assertFalse(session.isRecordProcessable(0, 2));
-		Assert.assertTrue(session.isRecordProcessable(0, 64));
+		assertFalse(session.isRecordProcessable(0, 0));
+		assertTrue(session.isRecordProcessable(0, 1));
+		assertFalse(session.isRecordProcessable(0, 2));
+		assertTrue(session.isRecordProcessable(0, 64));
 
 		// make a right shift by 1 position
 		session.markRecordAsRead(epoch, 64);
-		Assert.assertFalse(session.isRecordProcessable(0, 0));
-		Assert.assertTrue(session.isRecordProcessable(0, 1));
-		Assert.assertFalse(session.isRecordProcessable(0, 2));
-		Assert.assertFalse(session.isRecordProcessable(0, 64));
+		assertFalse(session.isRecordProcessable(0, 0));
+		assertTrue(session.isRecordProcessable(0, 1));
+		assertFalse(session.isRecordProcessable(0, 2));
+		assertFalse(session.isRecordProcessable(0, 64));
 	}
-	
+
 	@Test
 	public void testEpochSwitchResetsReceiveWindow() {
 
 		int epoch = session.getReadEpoch();
 		session.markRecordAsRead(epoch, 0);
 		session.markRecordAsRead(epoch, 2);
-		Assert.assertFalse(session.isRecordProcessable(session.getReadEpoch(), 0));
-		Assert.assertFalse(session.isRecordProcessable(session.getReadEpoch(), 2));
-		
+		assertFalse(session.isRecordProcessable(session.getReadEpoch(), 0));
+		assertFalse(session.isRecordProcessable(session.getReadEpoch(), 2));
+
 		session.setReadState(session.getReadState()); // dummy invocation to provoke epoch switch
-		Assert.assertTrue(session.isRecordProcessable(session.getReadEpoch(), 0));
-		Assert.assertTrue(session.isRecordProcessable(session.getReadEpoch(), 2));
-		
+		assertTrue(session.isRecordProcessable(session.getReadEpoch(), 0));
+		assertTrue(session.isRecordProcessable(session.getReadEpoch(), 2));
 	}
-	
+
 	@Test
 	public void testConstructorEnforcesMaxSequenceNo() {
 		session = new DTLSSession(peerAddress, false, DtlsTestTools.MAX_SEQUENCE_NO); // should succeed
 		try {
 			session = new DTLSSession(peerAddress, false, DtlsTestTools.MAX_SEQUENCE_NO + 1); // should fail
-			Assert.fail("DTLSSession constructor should have refused initial sequence number > 2^48 - 1");
+			fail("DTLSSession constructor should have refused initial sequence number > 2^48 - 1");
 		} catch (IllegalArgumentException e) {
 			// ok
 		}
 	}
-	
+
 	@Test(expected = IllegalStateException.class)
 	public void testGetSequenceNumberEnforcesMaxSequenceNo() {
 		session = new DTLSSession(peerAddress, false, DtlsTestTools.MAX_SEQUENCE_NO);
