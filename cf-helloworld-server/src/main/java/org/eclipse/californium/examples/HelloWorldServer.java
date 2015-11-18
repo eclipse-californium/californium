748084257313aa0e441ef57c0a8633419dac6162
Matthias Kovatsch <kovatsch@inf.ethz.ch>
2015-11-18 23:22:54 +0100

    Updated specific endpoints example.

--- cf-helloworld-server/src/main/java/org/eclipse/californium/examples/HelloWorldServer.java
+++ cf-helloworld-server/src/main/java/org/eclipse/californium/examples/HelloWorldServer.java
@@ -19,13 +19,12 @@ package org.eclipse.californium.examples;
 import java.net.Inet4Address;
 import java.net.InetAddress;
 import java.net.InetSocketAddress;
-import java.net.NetworkInterface;
 import java.net.SocketException;
-import java.util.Collections;
 
 import org.eclipse.californium.core.CoapResource;
 import org.eclipse.californium.core.CoapServer;
 import org.eclipse.californium.core.network.CoapEndpoint;
+import org.eclipse.californium.core.network.EndpointManager;
 import org.eclipse.californium.core.network.config.NetworkConfig;
 import org.eclipse.californium.core.server.resources.CoapExchange;
 
@@ -52,19 +51,16 @@ public class HelloWorldServer extends CoapServer {
     }
 
     /**
-     * Add endpoints listening on default CoAP port on all IP addresses of all network interfaces.
-     * 
-     * @throws SocketException if network interfaces cannot be determined
+     * Add individual endpoints listening on default CoAP port on all IPv4 addresses of all network interfaces.
      */
-    private void addEndpoints() throws SocketException {
-    	for (NetworkInterface ni : Collections.list(NetworkInterface.getNetworkInterfaces())) {
-    		for (InetAddress addr : Collections.list(ni.getInetAddresses())) {
-    			if (!addr.isLoopbackAddress() && addr instanceof Inet4Address) {
-    				InetSocketAddress bindToAddress = new InetSocketAddress(addr, COAP_PORT);
-    				addEndpoint(new CoapEndpoint(bindToAddress));
-    			}
-    		}
-    	}
+    private void addEndpoints() {
+    	for (InetAddress addr : EndpointManager.getEndpointManager().getNetworkInterfaces()) {
+    		// only binds to IPv4 addresses and localhost
+			if (addr instanceof Inet4Address || addr.isLoopbackAddress()) {
+				InetSocketAddress bindToAddress = new InetSocketAddress(addr, COAP_PORT);
+				addEndpoint(new CoapEndpoint(bindToAddress));
+			}
+		}
     }
 
     /*
