4d117130814dff88e5b676f319e95aa8af57171f
Kai Hudalla <kai.hudalla@bosch-si.com>
2016-02-26 09:16:00 +0100

    Make test cases independent from each other.

---
+++ californium-core/src/test/java/org/eclipse/californium/core/test/lockstep/IntegrationTestTools.java
@@ -0,0 +1,106 @@
+/*******************************************************************************
+ * Copyright (c) 2016 Bosch Software Innovations GmbH
+ * 
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * and Eclipse Distribution License v1.0 which accompany this distribution.
+ * 
+ * The Eclipse Public License is available at
+ *    http://www.eclipse.org/legal/epl-v10.html
+ * and the Eclipse Distribution License is available at
+ *    http://www.eclipse.org/org/documents/edl-v10.html.
+ * 
+ * Contributors:
+ *    Bosch Software Innovations GmbH - refactor common functionality for integration
+ *                                      tests into separate utility class
+ ******************************************************************************/
+package org.eclipse.californium.core.test.lockstep;
+
+import static org.eclipse.californium.core.coap.CoAP.ResponseCode.CONTENT;
+import static org.hamcrest.CoreMatchers.*;
+import static org.junit.Assert.*;
+
+import java.net.InetSocketAddress;
+import java.util.Random;
+
+import org.eclipse.californium.core.coap.Request;
+import org.eclipse.californium.core.coap.Response;
+import org.eclipse.californium.core.test.BlockwiseTransferTest.ServerBlockwiseInterceptor;
+import org.eclipse.californium.core.coap.CoAP.Code;
+import org.eclipse.californium.core.coap.CoAP.ResponseCode;
+
+/**
+ * Common functionality for integration tests.
+ *
+ */
+public final class IntegrationTestTools {
+
+	private static final Random RAND = new Random();
+	private static int currentToken = 10;
+
+	private IntegrationTestTools() {
+		// empty
+	}
+
+	public static LockstepEndpoint createLockstepEndpoint(InetSocketAddress destination) throws Exception {
+		LockstepEndpoint endpoint = new LockstepEndpoint();
+		endpoint.setDestination(destination);
+		return endpoint;
+	}
+
+	public static Request createRequest(Code code, String path, LockstepEndpoint server) throws Exception {
+		Request request = new Request(code);
+		String uri = String.format("coap://%s:%d/%s", server.getAddress().getHostAddress(), server.getPort(), path);
+		request.setURI(uri);
+		return request; 
+	}
+
+	public static void assertResponseContainsExpectedPayload(Response response, String expectedPayload) {
+		assertResponseContainsExpectedPayload(response, CONTENT, expectedPayload);
+	}
+
+	public static void assertResponseContainsExpectedPayload(Response response, ResponseCode expectedResponseCode,
+			String expectedPayload) {
+		assertNotNull("Client received no notification", response);
+		assertThat("Client received wrong response code:", response.getCode(), is(expectedResponseCode));
+		assertThat("Client received wrong payload:", response.getPayloadString(), is(expectedPayload));
+	}
+
+	public static void printServerLog(ClientBlockwiseInterceptor interceptor) {
+		System.out.println(interceptor.toString());
+		interceptor.clear();
+	}
+
+	public static void printServerLog(ServerBlockwiseInterceptor interceptor) {
+		System.out.println(interceptor.toString());
+		interceptor.clear();
+	}
+
+	public static String generateRandomPayload(int length) {
+		StringBuffer buffer = new StringBuffer();
+		while(buffer.length() < length) {
+			buffer.append(RAND.nextInt());
+		}
+		return buffer.substring(0, length);
+	}
+
+	public static String generatePayload(int length) {
+		StringBuffer buffer = new StringBuffer();
+		int n = 1;
+		while(buffer.length() < length) {
+			buffer.append(n++);
+		}
+		return buffer.substring(0, length);
+	}
+
+	public static byte[] generateNextToken() {
+		return b(++currentToken);
+	}
+
+	private static byte[] b(int... is) {
+		byte[] bytes = new byte[is.length];
+		for (int i=0;i<bytes.length;i++)
+			bytes[i] = (byte) is[i];
+		return bytes;
+	}
+}
--- californium-core/src/test/java/org/eclipse/californium/core/test/lockstep/ObserveClientSideTest.java
+++ californium-core/src/test/java/org/eclipse/californium/core/test/lockstep/ObserveClientSideTest.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2015 Institute for Pervasive Computing, ETH Zurich and others.
+ * Copyright (c) 2015, 2016 Institute for Pervasive Computing, ETH Zurich and others.
  * 
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
@@ -16,20 +16,19 @@
  *    Dominique Im Obersteg - parsers and initial implementation
  *    Daniel Pauli - parsers and initial implementation
  *    Kai Hudalla - logging
+ *    Bosch Software Innovations GmbH - reduce code duplication, split up into
+ *                                      separate test cases, remove wait cycles
  ******************************************************************************/
 package org.eclipse.californium.core.test.lockstep;
 
 import static org.eclipse.californium.core.coap.CoAP.Code.GET;
 import static org.eclipse.californium.core.coap.CoAP.ResponseCode.CONTENT;
 import static org.eclipse.californium.core.coap.CoAP.Type.*;
+import static org.eclipse.californium.core.test.lockstep.IntegrationTestTools.*;
 
-import java.io.IOException;
-import java.net.InetAddress;
 import java.net.InetSocketAddress;
-import java.util.Random;
 
 import org.junit.Assert;
-import org.eclipse.californium.core.coap.CoAP.Code;
 import org.eclipse.californium.core.coap.Request;
 import org.eclipse.californium.core.coap.Response;
 import org.eclipse.californium.core.network.CoapEndpoint;
@@ -37,7 +36,9 @@ import org.eclipse.californium.core.network.Endpoint;
 import org.eclipse.californium.core.network.config.NetworkConfig;
 import org.eclipse.californium.core.network.interceptors.MessageTracer;
 import org.junit.After;
+import org.junit.AfterClass;
 import org.junit.Before;
+import org.junit.BeforeClass;
 import org.junit.Test;
 
 
@@ -46,149 +47,135 @@ import org.junit.Test;
  */
 public class ObserveClientSideTest {
 
-	private static boolean RANDOM_PAYLOAD_GENERATION = true;
-	
+	private static NetworkConfig CONFIG;
+
 	private LockstepEndpoint server;
-	
 	private Endpoint client;
-	private int clientPort;
-	
 	private int mid = 8000;
-	
 	private String respPayload;
-	
 	private ClientBlockwiseInterceptor clientInterceptor = new ClientBlockwiseInterceptor();
-	
+
+	@BeforeClass
+	public static void init() {
+		System.out.println(System.lineSeparator() + "Start " + ObserveClientSideTest.class.getSimpleName());
+		CONFIG = new NetworkConfig()
+				.setInt(NetworkConfig.Keys.MAX_MESSAGE_SIZE, 16)
+				.setInt(NetworkConfig.Keys.PREFERRED_BLOCK_SIZE, 16)
+				.setInt(NetworkConfig.Keys.ACK_TIMEOUT, 200) // client retransmits after 200 ms
+				.setFloat(NetworkConfig.Keys.ACK_RANDOM_FACTOR, 1f)
+				.setFloat(NetworkConfig.Keys.ACK_TIMEOUT_SCALE, 1f);
+	}
+
 	@Before
-	public void setupClient() throws IOException {
-		System.out.println("\nStart "+getClass().getSimpleName());
-		
-		NetworkConfig config = new NetworkConfig()
-			.setInt(NetworkConfig.Keys.MAX_MESSAGE_SIZE, 16)
-			.setInt(NetworkConfig.Keys.PREFERRED_BLOCK_SIZE, 16)
-			.setInt(NetworkConfig.Keys.ACK_TIMEOUT, 200) // client retransmits after 200 ms
-			.setFloat(NetworkConfig.Keys.ACK_RANDOM_FACTOR, 1f)
-			.setFloat(NetworkConfig.Keys.ACK_TIMEOUT_SCALE, 1f);
-		client = new CoapEndpoint(new InetSocketAddress(0), config);
+	public void setupEndpoints() throws Exception {
+
+		client = new CoapEndpoint(new InetSocketAddress(0), CONFIG);
 		client.addInterceptor(clientInterceptor);
 		client.addInterceptor(new MessageTracer());
 		client.start();
-		clientPort = client.getAddress().getPort();
-		System.out.println("Client binds to port "+clientPort);
+		System.out.println("Client binds to port " + client.getAddress().getPort());
+		server = createLockstepEndpoint(client.getAddress());
 	}
-	
+
 	@After
-	public void shutdownClient() {
-		System.out.println();
+	public void shutdownEndpoints() {
 		client.destroy();
-		System.out.println("End "+getClass().getSimpleName());
+		server.destroy();
 	}
-	
+
+	@AfterClass
+	public static void end() {
+		System.out.println("End " + ObserveClientSideTest.class.getSimpleName());
+	}
+
 	@Test
-	public void test() throws Throwable {
-		try {
-			testGETObserveWithLostACK();
-			testBlockwiseObserve();
-			
-		} catch (Exception e) {
-			e.printStackTrace();
-			System.err.println("Make sure you did not forget a .go() at the end of a line.");
-			throw e;
-		} catch (Throwable t) {
-			System.err.println(t);
-			throw t;
-		}
-	}
-	
-	private void testGETObserveWithLostACK() throws Exception {
+	public void testGETObserveWithLostACK() throws Exception {
 		System.out.println("Observe with lost ACKs:");
-		respPayload = generatePayload(10);
+		respPayload = generateRandomPayload(10);
 		String path = "test";
-		server = createLockstepEndpoint();
 		int obs = 100;
-		
-		Request request = createRequest(GET, path);
+
+		Request request = createRequest(GET, path, server);
 		request.setObserve();
 		client.sendRequest(request);
-		
+
 		server.expectRequest(CON, GET, path).storeMID("A").storeToken("B").observe(0).go();
 		server.sendEmpty(ACK).loadMID("A").go();
 		Thread.sleep(50);
 		server.sendResponse(CON, CONTENT).loadToken("B").payload(respPayload).mid(++mid).observe(++obs).go();
 		server.expectEmpty(ACK, mid).go(); // lost
 		clientInterceptor.log(" // lost");
-		server.sendResponse(CON, CONTENT).loadToken("B").payload(respPayload).mid(mid).observe(obs).go();
+		// retransmit notification using modified payload
+		// client must be able to identify this notification as a duplicate and ignore it
+		server.sendResponse(CON, CONTENT).loadToken("B").payload(respPayload + "_DUPLICATE").mid(mid).observe(obs).go();
 		server.expectEmpty(ACK, mid).go();
-		
-		Response response = request.waitForResponse(1000);
-		printServerLog();
-		
-		Assert.assertNotNull("Client received no response", response);
-		Assert.assertEquals("Client received wrong response code:", CONTENT, response.getCode());
-		Assert.assertEquals("Client received wrong payload:", respPayload, response.getPayloadString());
-		System.out.println("Relation established");
-		Thread.sleep(1000);
-		
-		respPayload = generatePayload(10); // changed
-		server.sendResponse(CON, CONTENT).loadToken("B").payload(respPayload).mid(++mid).observe(++obs).go();
 
+		Response response = request.waitForResponse(1000);
+		printServerLog(clientInterceptor);
+
+		assertResponseContainsExpectedPayload(response, respPayload);
+		System.out.println("Relation established");
+
+		respPayload = generateRandomPayload(10); // changed
+		server.sendResponse(CON, CONTENT).loadToken("B").payload(respPayload).mid(++mid).observe(++obs).go();
 		server.expectEmpty(ACK, mid).go(); // lost
-		
 		clientInterceptor.log(" // lost");
-		server.sendResponse(CON, CONTENT).loadToken("B").payload(respPayload).mid(mid).observe(obs).go();
+		server.sendResponse(CON, CONTENT).loadToken("B").payload(respPayload + "_DUPLICATE").mid(mid).observe(obs).go();
 		server.expectEmpty(ACK, mid).go();
 
 		response = request.waitForResponse(1000);
-		printServerLog();
-		
-		Assert.assertNotNull("Client received no response", response);
-		Assert.assertEquals("Client received wrong response code:", CONTENT, response.getCode());
-		Assert.assertEquals("Client received wrong payload:", respPayload, response.getPayloadString());
+		printServerLog(clientInterceptor);
+
+		assertResponseContainsExpectedPayload(response, respPayload);
 	}
-	
-	private void testBlockwiseObserve() throws Exception {
+
+	/**
+	 * Verifies behavior of observing a resource that is transferred in multiple chunks using blockwise transfer.
+	 * 
+	 * THIS TEST CASE FAILS SPORADICALLY BECAUSE OF ITS (INVALID) ASSUMPTION THAT MESSAGES ALWAYS ARRIVE AT
+	 * THE SERVER IN THE ORDER THEY HAVE BEEN SENT IN BY THE CLIENT WHEN USING UDP AS THE TRANSPORT PROTOCOL.
+	 * 
+	 * @throws Exception
+	 */
+	@Test
+	public void testBlockwiseObserve() throws Exception {
 		System.out.println("Blockwise Observe:");
-		respPayload = generatePayload(40);
+		respPayload = generateRandomPayload(40);
 		String path = "test";
-		server = createLockstepEndpoint();
-		
-		Request request = createRequest(GET, path);
+
+		Request request = createRequest(GET, path, server);
 		request.setObserve();
 		client.sendRequest(request);
-		
+
 		server.expectRequest(CON, GET, path).storeBoth("A").storeToken("T").go();
 		server.sendResponse(ACK, CONTENT).loadBoth("A").observe(0).block2(0, true, 16).payload(respPayload.substring(0, 16)).go();
 		server.expectRequest(CON, GET, path).storeBoth("B").block2(1, false, 16).go();
 		server.sendResponse(ACK, CONTENT).loadBoth("B").block2(1, true, 16).payload(respPayload.substring(16, 32)).go();
 		server.expectRequest(CON, GET, path).storeBoth("C").block2(2, false, 16).go();
 		server.sendResponse(ACK, CONTENT).loadBoth("C").block2(2, false, 16).payload(respPayload.substring(32, 40)).go();
-		
+
 		Response response = request.waitForResponse(1000);
-		printServerLog();
-		
-		Assert.assertNotNull("Client received no response", response);
-		Assert.assertEquals("Client received wrong response code:", CONTENT, response.getCode());
-		Assert.assertEquals("Client received wrong payload:", respPayload, response.getPayloadString());
-		
+		printServerLog(clientInterceptor);
+
+		assertResponseContainsExpectedPayload(response, respPayload);
+
 		// normal notification
-		Thread.sleep(50);
 		server.sendResponse(CON, CONTENT).loadToken("T").mid(++mid).observe(1).block2(0, true, 16).payload(respPayload.substring(0, 16)).go();
+		// TODO: allow for messages to be received in arbitrary order
 		server.expectEmpty(ACK, mid).go();
 		server.expectRequest(CON, GET, path).storeBoth("B").block2(1, false, 16).go();
 		server.sendResponse(ACK, CONTENT).loadBoth("B").block2(1, true, 16).payload(respPayload.substring(16, 32)).go();
 		server.expectRequest(CON, GET, path).storeBoth("C").block2(2, false, 16).go();
 		server.sendResponse(ACK, CONTENT).loadBoth("C").block2(2, false, 16).payload(respPayload.substring(32, 40)).go();
-		
+
 		response = request.waitForResponse(1000);
-		printServerLog();
-		
-		Assert.assertNotNull("Client received no notification", response);
-		Assert.assertEquals("Client received wrong response code:", CONTENT, response.getCode());
-		Assert.assertEquals("Client received wrong payload:", respPayload, response.getPayloadString());
-		
+		printServerLog(clientInterceptor);
+
+		assertResponseContainsExpectedPayload(response, respPayload);
+
 		// override transfer with new notification
-		Thread.sleep(50);
 		server.sendResponse(CON, CONTENT).loadToken("T").mid(++mid).observe(2).block2(0, true, 16).payload(respPayload.substring(0, 16)).go();
+		// TODO: allow for messages to be received in arbitrary order
 		server.expectEmpty(ACK, mid).go();
 		server.expectRequest(CON, GET, path).storeBoth("B").block2(1, false, 16).go();
 		server.sendResponse(ACK, CONTENT).loadBoth("B").block2(1, true, 16).payload(respPayload.substring(16, 32)).go();
@@ -204,17 +191,15 @@ public class ObserveClientSideTest {
 		server.sendResponse(ACK, CONTENT).loadBoth("D").block2(1, true, 16).payload(respPayload3.substring(16, 32)).go();
 		server.expectRequest(CON, GET, path).storeBoth("E").block2(2, false, 16).go();
 		server.sendResponse(ACK, CONTENT).loadBoth("E").block2(2, false, 16).payload(respPayload3.substring(32, 40)).go();
-		
+
 		response = request.waitForResponse(1000);
-		printServerLog();
-		
-		Assert.assertNotNull("Client received no notification", response);
-		Assert.assertEquals("Client received wrong response code:", CONTENT, response.getCode());
-		Assert.assertEquals("Client received wrong payload:", respPayload3, response.getPayloadString());
-		
+		printServerLog(clientInterceptor);
+
+		assertResponseContainsExpectedPayload(response, respPayload3);
+
 		// override transfer with new notification and conflicting block number
-		Thread.sleep(50);
 		server.sendResponse(CON, CONTENT).loadToken("T").mid(++mid).observe(4).block2(0, true, 16).payload(respPayload.substring(0, 16)).go();
+		// TODO: allow for messages to be received in arbitrary order
 		server.expectEmpty(ACK, mid).go();
 		server.expectRequest(CON, GET, path).storeBoth("F").block2(1, false, 16).go();
 		clientInterceptor.log("\n\n//////// Overriding notification 2 ////////");
@@ -229,77 +214,36 @@ public class ObserveClientSideTest {
 		server.sendResponse(ACK, CONTENT).loadBoth("G").block2(1, true, 16).payload(respPayload4.substring(16, 32)).go();
 		server.expectRequest(CON, GET, path).storeBoth("H").block2(2, false, 16).go();
 		server.sendResponse(ACK, CONTENT).loadBoth("H").block2(2, false, 16).payload(respPayload4.substring(32, 40)).go();
-		
+
 		response = request.waitForResponse(1000);
-		printServerLog();
-		
-		Assert.assertNotNull("Client received no notification", response);
-		Assert.assertEquals("Client received wrong response code:", CONTENT, response.getCode());
-		Assert.assertEquals("Client received wrong payload:", respPayload4, response.getPayloadString());
-		
+		printServerLog(clientInterceptor);
+
+		assertResponseContainsExpectedPayload(response, respPayload4);
+
 		// cancel
-		Thread.sleep(50);
 		clientInterceptor.log("\n\n//////// Notification after cancellation ////////");
 		server.sendResponse(CON, CONTENT).loadToken("T").mid(++mid).observe(6).block2(0, true, 16).payload(respPayload.substring(0, 16)).go();
+		// TODO: allow for messages to be received in arbitrary order
 		server.expectEmpty(ACK, mid).go();
 		server.expectRequest(CON, GET, path).storeBoth("B").block2(1, false, 16).go();
 		// canceling in the middle of blockwise transfer
 		request.cancel();
 		server.sendResponse(ACK, CONTENT).loadBoth("B").block2(1, true, 16).payload(respPayload.substring(16, 32)).go();
-		
-		// notification may not be delivered
+
+		// notification must not be delivered
 		response = request.waitForResponse(1000);
-		printServerLog();
-		
+		printServerLog(clientInterceptor);
+
 		Assert.assertNull("Client received notification although canceled", response);
-		
+
 		// next notification must be rejected
-		Thread.sleep(50);
-		server.sendResponse(CON, CONTENT).loadToken("T").mid(++mid).observe(5).block2(0, true, 16).payload(respPayload.substring(0, 16)).go();
+		server.sendResponse(CON, CONTENT).loadToken("T").mid(++mid).observe(7).block2(0, true, 16).payload(respPayload.substring(0, 16)).go();
 		server.expectEmpty(RST, mid).go();
-		
-		// notification may not be delivered
+
+		// notification must not be delivered
 		response = request.waitForResponse(1000);
-		printServerLog();
-		
+		printServerLog(clientInterceptor);
+
 		Assert.assertNull("Client received notification although canceled", response);
-	}
-	
-	private LockstepEndpoint createLockstepEndpoint() {
-		try {
-			LockstepEndpoint endpoint = new LockstepEndpoint();
-			endpoint.setDestination(new InetSocketAddress(InetAddress.getByName("localhost"), clientPort));
-			return endpoint;
-		} catch (Exception e) {
-			throw new RuntimeException(e);
-		}
-	}
-	
-	private Request createRequest(Code code, String path) throws Exception {
-		Request request = new Request(code);
-		String uri = "coap://localhost:"+(server.getPort())+"/"+path;
-		request.setURI(uri);
-		return request; 
-	}
-	
-	private void printServerLog() {
-		System.out.println(clientInterceptor.toString());
-		clientInterceptor.clear();
-	}
-	
-	private static String generatePayload(int length) {
-		StringBuffer buffer = new StringBuffer();
-		if (RANDOM_PAYLOAD_GENERATION) {
-			Random rand = new Random();
-			while(buffer.length() < length) {
-				buffer.append(rand.nextInt());
-			}
-		} else { // Deterministic payload
-			int n = 1;
-			while(buffer.length() < length) {
-				buffer.append(n++);
-			}
-		}
-		return buffer.substring(0, length);
 	}
 }
