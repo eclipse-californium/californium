a193d327801c3668be98587f17726019455695c8
Kai Hudalla <kai.hudalla@bosch-si.com>
2016-04-13 15:29:43 +0200

    Turn DataSerializer into static utility class.

--- californium-core/src/main/java/org/eclipse/californium/core/network/serialization/DataSerializer.java
+++ californium-core/src/main/java/org/eclipse/californium/core/network/serialization/DataSerializer.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2015 Institute for Pervasive Computing, ETH Zurich and others.
+ * Copyright (c) 2015, 2016 Institute for Pervasive Computing, ETH Zurich and others.
  * 
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
@@ -16,6 +16,7 @@
  *    Dominique Im Obersteg - parsers and initial implementation
  *    Daniel Pauli - parsers and initial implementation
  *    Kai Hudalla - logging
+ *    Bosch Software Innovations GmbH - turn into utility class with static methods only
  ******************************************************************************/
 package org.eclipse.californium.core.network.serialization;
 
@@ -31,41 +32,41 @@ import static org.eclipse.californium.core.coap.CoAP.MessageFormat.VERSION_BITS;
 
 import java.util.List;
 
+import org.eclipse.californium.core.coap.CoAP.Code;
 import org.eclipse.californium.core.coap.Message;
 import org.eclipse.californium.core.coap.Option;
 import org.eclipse.californium.core.coap.Request;
 import org.eclipse.californium.core.coap.Response;
-import org.eclipse.californium.core.coap.CoAP.Code;
-
 
 /**
  * The DataSerialized serializes outgoing messages to byte arrays.
  */
 // TODO: Should we call this "Encoder/Decoder"?
-public class DataSerializer {
-	
-	private DatagramWriter writer;
-	
-	public byte[] serializeRequest(Request request) {
-		writer = new DatagramWriter();
+public final class DataSerializer {
+
+	private DataSerializer() {
+	}
+
+	public static byte[] serializeRequest(Request request) {
+		DatagramWriter writer = new DatagramWriter();
 		Code code = request.getCode();
-		serializeMessage(request, code == null ? 0 : code.value);
+		serializeMessage(writer, request, code == null ? 0 : code.value);
 		return writer.toByteArray();
 	}
-	
-	public byte[] serializeResponse(Response response) {
-		writer = new DatagramWriter();
-		serializeMessage(response, response.getCode().value);
+
+	public static byte[] serializeResponse(Response response) {
+		DatagramWriter writer = new DatagramWriter();
+		serializeMessage(writer, response, response.getCode().value);
 		return writer.toByteArray();
 	}
-	
-	public byte[] serializeEmptyMessage(Message message) {
-		writer = new DatagramWriter();
-		serializeMessage(message, 0);
+
+	public static byte[] serializeEmptyMessage(Message message) {
+		DatagramWriter writer = new DatagramWriter();
+		serializeMessage(writer, message, 0);
 		return writer.toByteArray();
 	}
-	
-	private void serializeMessage(Message message, int code) {
+
+	private static void serializeMessage(final DatagramWriter writer, final Message message, final int code) {
 		if (message.getToken() == null)
 			throw new NullPointerException("No Token has been set, not even an empty byte[0]");
 		writer.write(VERSION, VERSION_BITS);
@@ -74,28 +75,29 @@ public class DataSerializer {
 		writer.write(code, CODE_BITS);
 		writer.write(message.getMID(), MESSAGE_ID_BITS);
 		writer.writeBytes(message.getToken());
-		
-		List<Option> options = message.getOptions().asSortedList(); // already sorted
+
+		List<Option> options = message.getOptions().asSortedList(); // already
+																	// sorted
 		int lastOptionNumber = 0;
-		for (Option option:options) {
-			
+		for (Option option : options) {
+
 			// write 4-bit option delta
 			int optionDelta = option.getNumber() - lastOptionNumber;
 			int optionDeltaNibble = getOptionNibble(optionDelta);
 			writer.write(optionDeltaNibble, OPTION_DELTA_BITS);
-			
+
 			// write 4-bit option length
 			int optionLength = option.getLength();
 			int optionLengthNibble = getOptionNibble(optionLength);
 			writer.write(optionLengthNibble, OPTION_LENGTH_BITS);
-			
+
 			// write extended option delta field (0 - 2 bytes)
 			if (optionDeltaNibble == 13) {
 				writer.write(optionDelta - 13, 8);
 			} else if (optionDeltaNibble == 14) {
 				writer.write(optionDelta - 269, 16);
 			}
-			
+
 			// write extended option length field (0 - 2 bytes)
 			if (optionLengthNibble == 13) {
 				writer.write(optionLength - 13, 8);
@@ -109,7 +111,7 @@ public class DataSerializer {
 			// update last option number
 			lastOptionNumber = option.getNumber();
 		}
-		
+
 		byte[] payload = message.getPayload();
 		if (payload != null && payload.length > 0) {
 			// if payload is present and of non-zero length, it is prefixed by
@@ -119,15 +121,14 @@ public class DataSerializer {
 			writer.writeBytes(payload);
 		}
 	}
-	
+
 	/**
 	 * Returns the 4-bit option header value.
 	 * 
-	 * @param optionValue
-	 *            the option value (delta or length) to be encoded.
+	 * @param optionValue the option value (delta or length) to be encoded.
 	 * @return the 4-bit option header value.
 	 */
-	private int getOptionNibble(int optionValue) {
+	private static int getOptionNibble(int optionValue) {
 		if (optionValue <= 12) {
 			return optionValue;
 		} else if (optionValue <= 255 + 13) {
@@ -135,8 +136,7 @@ public class DataSerializer {
 		} else if (optionValue <= 65535 + 269) {
 			return 14;
 		} else {
-			throw new IllegalArgumentException("Unsupported option delta "+optionValue);
+			throw new IllegalArgumentException("Unsupported option delta " + optionValue);
 		}
 	}
-	
 }
