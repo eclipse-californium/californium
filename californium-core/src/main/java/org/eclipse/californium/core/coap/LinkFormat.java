6670db771f7e4b6e679d4a1fde4cf2358dc2b0bc
Adrian Mardari <mardari.adri@gmail.com>
2016-05-19 15:32:10 +0200

    Fixed matches() method in class LinkFormat, cf-core
    The matches method in the LinkFormat class was returning true
    for any 1 matching attribute in the querry as opposed to
    returning the result after checking against all the attributes in
    the query
    
    Signed-off-by: Adrian Mardari <mardari.adri@gmail.com>

--- californium-core/src/main/java/org/eclipse/californium/core/coap/LinkFormat.java
+++ californium-core/src/main/java/org/eclipse/californium/core/coap/LinkFormat.java
@@ -34,218 +34,221 @@ import org.eclipse.californium.core.WebLink;
 import org.eclipse.californium.core.server.resources.Resource;
 import org.eclipse.californium.core.server.resources.ResourceAttributes;
 
-
 public class LinkFormat {
-	
-	public static final String RESOURCE_TYPE         = "rt";
+
+	public static final String RESOURCE_TYPE = "rt";
 	public static final String INTERFACE_DESCRIPTION = "if";
-	public static final String CONTENT_TYPE          = "ct";
-	public static final String MAX_SIZE_ESTIMATE     = "sz";
-	public static final String TITLE                 = "title";
-	public static final String OBSERVABLE            = "obs";
-	public static final String LINK                  = "href";
+	public static final String CONTENT_TYPE = "ct";
+	public static final String MAX_SIZE_ESTIMATE = "sz";
+	public static final String TITLE = "title";
+	public static final String OBSERVABLE = "obs";
+	public static final String LINK = "href";
 
 	// for Resource Directory
-	public static final String HOST		     		 = "h";
-	public static final String LIFE_TIME     		 = "lt";
-	public static final String INSTANCE		   		 = "ins";
-	public static final String DOMAIN	     		 = "d";
-	public static final String CONTEXT		   		 = "con";
-	public static final String END_POINT     		 = "ep";
-	public static final String END_POINT_TYPE		 = "et";
+	public static final String HOST = "h";
+	public static final String LIFE_TIME = "lt";
+	public static final String INSTANCE = "ins";
+	public static final String DOMAIN = "d";
+	public static final String CONTEXT = "con";
+	public static final String END_POINT = "ep";
+	public static final String END_POINT_TYPE = "et";
 
 	// for parsing
-	public static final Pattern DELIMITER      = Pattern.compile("\\s*,+\\s*");
-	public static final Pattern SEPARATOR      = Pattern.compile("\\s*;+\\s*");
-	public static final Pattern WORD           = Pattern.compile("\\w+");
-	public static final Pattern QUOTED_STRING  = Pattern.compile("\\G\".*?\"");
-	public static final Pattern CARDINAL       = Pattern.compile("\\G\\d+");
-	
+	public static final Pattern DELIMITER = Pattern.compile("\\s*,+\\s*");
+	public static final Pattern SEPARATOR = Pattern.compile("\\s*;+\\s*");
+	public static final Pattern WORD = Pattern.compile("\\w+");
+	public static final Pattern QUOTED_STRING = Pattern.compile("\\G\".*?\"");
+	public static final Pattern CARDINAL = Pattern.compile("\\G\\d+");
+
 	public static String serializeTree(Resource resource) {
 		StringBuilder buffer = new StringBuilder();
 		List<String> noQueries = Collections.emptyList();
-		
+
 		// only include children, not the entry point itself
-		for (Resource child:resource.getChildren()) {
+		for (Resource child : resource.getChildren()) {
 			serializeTree(child, noQueries, buffer);
 		}
-		
-		if (buffer.length()>1)
-			buffer.delete(buffer.length()-1, buffer.length());
+
+		if (buffer.length() > 1)
+			buffer.delete(buffer.length() - 1, buffer.length());
 		return buffer.toString();
 	}
 
 	public static void serializeTree(Resource resource, List<String> queries, StringBuilder buffer) {
 		// add the current resource to the buffer
-		if (resource.isVisible()
-				&& LinkFormat.matches(resource, queries)) {
+		if (resource.isVisible() && LinkFormat.matches(resource, queries)) {
 			buffer.append(LinkFormat.serializeResource(resource));
 		}
-		
+
 		// sort by resource name
 		List<Resource> childs = new ArrayList<Resource>(resource.getChildren());
 		Collections.sort(childs, new Comparator<Resource>() {
-		    @Override
-		    public int compare(Resource o1, Resource o2) {
-		        return o1.getName().compareTo(o2.getName());
-		    }
+			@Override
+			public int compare(Resource o1, Resource o2) {
+				return o1.getName().compareTo(o2.getName());
+			}
 		});
-		
-		for (Resource child:childs) {
+
+		for (Resource child : childs) {
 			serializeTree(child, queries, buffer);
 		}
 	}
 
 	public static StringBuilder serializeResource(Resource resource) {
 		StringBuilder buffer = new StringBuilder();
-		buffer.append("<")
-			.append(resource.getPath())
-			.append(resource.getName())
-			.append(">")
-			.append(LinkFormat.serializeAttributes(resource.getAttributes()))
-			.append(",");
+		buffer.append("<").append(resource.getPath()).append(resource.getName()).append(">")
+				.append(LinkFormat.serializeAttributes(resource.getAttributes())).append(",");
 		return buffer;
 	}
-	
+
 	public static StringBuilder serializeAttributes(ResourceAttributes attributes) {
 		StringBuilder buffer = new StringBuilder();
-		
 
 		List<String> attributesList = new ArrayList<String>(attributes.getAttributeKeySet());
 		Collections.sort(attributesList);
 		for (String attr : attributesList) {
 			List<String> values = attributes.getAttributeValues(attr);
-			if (values == null) continue;
+			if (values == null)
+				continue;
 			buffer.append(";");
-			
-			// Make a copy to not  depend on thread-safety
+
+			// Make a copy to not depend on thread-safety
 			buffer.append(serializeAttribute(attr, new LinkedList<String>(values)));
 		}
 		return buffer;
 	}
-	
+
 	public static StringBuilder serializeAttribute(String key, List<String> values) {
-		
+
 		String delimiter = "=";
-		
+
 		StringBuilder linkFormat = new StringBuilder();
 		boolean quotes = false;
-		
+
 		linkFormat.append(key);
-		
-		if (values==null) {
+
+		if (values == null) {
 			throw new RuntimeException("Values null");
 		}
-		
-		if (values.isEmpty() || values.get(0).equals("")) 
+
+		if (values.isEmpty() || values.get(0).equals(""))
 			return linkFormat;
-		
+
 		linkFormat.append(delimiter);
-		
-		if (values.size()>1 || !values.get(0).matches("^[0-9]+$")) {
+
+		if (values.size() > 1 || !values.get(0).matches("^[0-9]+$")) {
 			linkFormat.append('"');
 			quotes = true;
 		}
-		
+
 		Iterator<String> it = values.iterator();
 		while (it.hasNext()) {
 			linkFormat.append(it.next());
-			
+
 			if (it.hasNext()) {
 				linkFormat.append(' ');
 			}
 		}
-		
+
 		if (quotes) {
 			linkFormat.append('"');
 		}
-		
+
 		return linkFormat;
 	}
-	
+
 	public static boolean matches(Resource resource, List<String> queries) {
-		
-		if (resource==null) return false;
-		if (queries==null || queries.size()==0) return true;
-		
+
+		if (resource == null)
+			return false;
+		if (queries == null || queries.size() == 0)
+			return true;
+
 		ResourceAttributes attributes = resource.getAttributes();
-		String path = resource.getPath()+resource.getName();
-		
+		String path = resource.getPath() + resource.getName();
+
 		for (String s : queries) {
 			int delim = s.indexOf("=");
 			if (delim != -1) {
-				
+
 				// split name-value-pair
 				String attrName = s.substring(0, delim);
-				String expected = s.substring(delim+1);
-
+				String expected = s.substring(delim + 1);
 				if (attrName.equals(LinkFormat.LINK)) {
 					if (expected.endsWith("*")) {
-						return path.startsWith(expected.substring(0, expected.length()-1));
+						if (!path.startsWith(expected.substring(0, expected.length() - 1)))
+							return false;
 					} else {
-						return path.equals(expected);
+						if (!path.equals(expected))
+							return false;
 					}
 				} else if (attributes.containsAttribute(attrName)) {
 					// lookup attribute value
 					for (String actual : attributes.getAttributeValues(attrName)) {
-					
+
 						// get prefix length according to "*"
 						int prefixLength = expected.indexOf('*');
 						if (prefixLength >= 0 && prefixLength < actual.length()) {
-					
+
 							// reduce to prefixes
 							expected = expected.substring(0, prefixLength);
 							actual = actual.substring(0, prefixLength);
 						}
-						
+
 						// handle case like rt=[Type1 Type2]
-						if (actual.indexOf(" ") > -1) { // if contains white space
+						if (actual.indexOf(" ") > -1) { // if contains white
+														// space
 							String[] parts = actual.split(" ");
-							for (String part : parts) { // check each part for match
-								if (part.equals(expected)) {
-									return true;
+							for (String part : parts) { // check each part for
+														// match
+								if (!part.equals(expected)) {
+									return false;
 								}
 							}
 						}
-						
+
 						// compare strings
-						if (expected.equals(actual)) {
-							return true;
+						if (!expected.equals(actual)) {
+							return false;
 						}
 					}
+				} else if (!attributes.containsAttribute(attrName)) {
+					return false;
 				}
 			} else {
 				// flag attribute
-				if (attributes.getAttributeValues(s).size()>0) {
+				if (attributes.getAttributeValues(s).size() > 0) {
 					return true;
 				}
 			}
 		}
-		return false;
+		return true;
 	}
-	
+
 	public static Set<WebLink> parse(String linkFormat) {
 		Pattern DELIMITER = Pattern.compile("\\s*,+\\s*");
 
 		Set<WebLink> links = new ConcurrentSkipListSet<WebLink>();
-		
-		if (linkFormat!=null) {
+
+		if (linkFormat != null) {
 			Scanner scanner = new Scanner(linkFormat);
 			String path = null;
 			while ((path = scanner.findInLine("<[^>]*>")) != null) {
-				
+
 				// Trim <...>
 				path = path.substring(1, path.length() - 1);
-				
+
 				WebLink link = new WebLink(path);
-				
+
 				// Read link format attributes
 				String attr = null;
-				while (scanner.findWithinHorizon(DELIMITER, 1)==null && (attr = scanner.findInLine(WORD))!=null) {
+				while (scanner.findWithinHorizon(DELIMITER, 1) == null && (attr = scanner.findInLine(WORD)) != null) {
 					if (scanner.findWithinHorizon("=", 1) != null) {
 						String value = null;
 						if ((value = scanner.findInLine(QUOTED_STRING)) != null) {
-							value = value.substring(1, value.length()-1); // trim " "
+							value = value.substring(1, value.length() - 1); // trim
+																			// "
+																			// "
 							if (attr.equals(TITLE)) {
 								link.getAttributes().addAttribute(attr, value);
 							} else {
@@ -260,13 +263,13 @@ public class LinkFormat {
 						} else if (scanner.hasNext()) {
 							value = scanner.next();
 						}
-						
+
 					} else {
 						// flag attribute without value
 						link.getAttributes().addAttribute(attr);
 					}
 				}
-				
+
 				links.add(link);
 			}
 			scanner.close();
