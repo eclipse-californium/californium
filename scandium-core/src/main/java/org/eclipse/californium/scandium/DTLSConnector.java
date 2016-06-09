eclipse-californium/californium

commit 295217ddeb7e53089ae23571946b6d7a0b0dd8d7

#32: Name thread in threadpools for easier jstack reading.
This commit:
- introduces a new Utils.NamedThreadFactory class
- makes all executor services to use this factory, so theads can receive meaningful names
- in addition, all threads are created using thread group
- the following thread groups are available: Californium, Californium/Elements, Californium/Scandium

Bug: https://github.com/eclipse/californium/issues/32

Signed-off-by: Valentin Valchev <v.valchev@prosyst.bg>

scandium-core/src/main/java/org/eclipse/californium/scandium/DTLSConnector.java
@@ -48,11 +48,14 @@ import java.security.cert.Certificate;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
-import java.util.Timer;
 import java.util.TimerTask;
 import java.util.concurrent.BlockingQueue;
+import java.util.concurrent.Executors;
 import java.util.concurrent.LinkedBlockingQueue;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.ThreadFactory;
 import java.util.concurrent.TimeUnit;
+import java.util.concurrent.atomic.AtomicInteger;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
@@ -117,6 +120,8 @@ public class DTLSConnector implements Connector {
 			+ 13 // DTLS record headers
 			+ MAX_CIPHERTEXT_EXPANSION;
 
+	static final ThreadGroup SCANDIUM_THREAD_GROUP = new ThreadGroup("Californium/Scandium"); //$NON-NLS-1$
+
 	private InetSocketAddress lastBindAddress;
 	private int maximumTransmissionUnit = 1280; // min. IPv6 MTU
 	private int inboundDatagramBufferSize = MAX_DATAGRAM_BUFFER_SIZE;
@@ -133,7 +138,8 @@ public class DTLSConnector implements Connector {
 	private DatagramSocket socket;
 
 	/** The timer daemon to schedule retransmissions. */
-	private Timer timer;
+	//private Timer timer;
+	private ScheduledExecutorService timer;
 
 	/** The thread that receives messages */
 	private Worker receiver;
@@ -294,7 +300,19 @@ public class DTLSConnector implements Connector {
 		if (running) {
 			return;
 		}
-		timer = new Timer(true); // run as daemon
+		timer = Executors.newSingleThreadScheduledExecutor(new ThreadFactory() {
+
+			private final AtomicInteger index = new AtomicInteger(1);
+
+			@Override
+			public Thread newThread(Runnable r) {
+				final Thread ret = new Thread(SCANDIUM_THREAD_GROUP, r,
+						"DTLS RetransmitTask " + index.getAndIncrement(), 0);
+				ret.setDaemon(true);
+				ret.setPriority(Thread.NORM_PRIORITY);
+				return ret;
+			}
+		});
 		socket = new DatagramSocket(null);
 		// make it easier to stop/start a server consecutively without delays
 		socket.setReuseAddress(true);
@@ -381,7 +399,7 @@ public class DTLSConnector implements Connector {
 			return;
 		}
 		LOGGER.log(Level.INFO, "Stopping DTLS connector on [{0}]", lastBindAddress);
-		timer.cancel();
+		timer.shutdownNow();
 		releaseSocket();
 	}
 
@@ -1374,9 +1392,9 @@ public class DTLSConnector implements Connector {
 				// double timeout
 				flight.incrementTimeout();
 			}
-	
+
 			// schedule retransmission task
-			timer.schedule(flight.getRetransmitTask(), flight.getTimeout());
+			timer.schedule(flight.getRetransmitTask(), flight.getTimeout(), TimeUnit.MILLISECONDS);
 		}
 	}
 
@@ -1490,8 +1508,8 @@ public class DTLSConnector implements Connector {
 		 *
 		 * @param name the name, e.g., of the transport protocol
 		 */
-		private Worker(String name) {
-			super(name);
+		protected Worker(String name) {
+			super(SCANDIUM_THREAD_GROUP, name);
 		}
 
 		@Override

californium-core/src/main/java/org/eclipse/californium/core/CoapClient.java
@@ -159,7 +159,7 @@ public class CoapClient {
 	 * @return the CoAP client
 	 */
 	public CoapClient useExecutor() {
-		this.executor = Executors.newSingleThreadExecutor();
+		this.executor = Executors.newSingleThreadExecutor(new Utils.NamedThreadFactory("CoapClient#")); //$NON-NLS-1$
 		
 		// activates the executor so that this user thread starts deterministically
 		executor.execute(new Runnable() {

californium-core/src/main/java/org/eclipse/californium/core/CoapObserveRelation.java
@@ -36,9 +36,10 @@ import org.eclipse.californium.core.observe.ObserveNotificationOrderer;
  * successfully established and to cancel or refresh the relation.
  */
 public class CoapObserveRelation {
-	
+
 	/** A executor service to schedule re-registrations */
-	private static ScheduledExecutorService scheduler = Executors.newSingleThreadScheduledExecutor(new Utils.DaemonThreadFactory());
+	private static ScheduledExecutorService scheduler = Executors.newSingleThreadScheduledExecutor(//
+			new Utils.DaemonThreadFactory("CoapObserveRelation#")); //$NON-NLS-1$
 
 	/** The request. */
 	private Request request;

californium-core/src/main/java/org/eclipse/californium/core/CoapServer.java
@@ -156,7 +156,9 @@ public class CoapServer implements ServerInterface {
 		// endpoints
 		this.endpoints = new ArrayList<Endpoint>();
 		// sets the central thread pool for the protocol stage over all endpoints
-		this.executor = Executors.newScheduledThreadPool( config.getInt(NetworkConfig.Keys.PROTOCOL_STAGE_THREAD_COUNT) );
+		this.executor = Executors.newScheduledThreadPool(//
+				config.getInt(NetworkConfig.Keys.PROTOCOL_STAGE_THREAD_COUNT), //
+				new Utils.NamedThreadFactory("CoapServer#")); //$NON-NLS-1$
 		// create endpoint for each port
 		for (int port:ports)
 			addEndpoint(new CoapEndpoint(port, this.config));

californium-core/src/main/java/org/eclipse/californium/core/Utils.java
@@ -22,6 +22,7 @@
 package org.eclipse.californium.core;
 
 import java.util.concurrent.ThreadFactory;
+import java.util.concurrent.atomic.AtomicInteger;
 
 import org.eclipse.californium.core.coap.MediaTypeRegistry;
 import org.eclipse.californium.core.coap.Request;
@@ -143,15 +144,93 @@ public class Utils {
 	
 	        return sb.toString();
 	}
-	
+
+	static final ThreadGroup COAP_THREAD_GROUP = new ThreadGroup("Californium"); //$NON-NLS-1$
+
 	/**
 	 * A factory to create executor services with daemon threads.
 	 */
-	public static class DaemonThreadFactory implements ThreadFactory {
-	    public Thread newThread(Runnable r) {
-	        Thread thread = new Thread(r);
-	        thread.setDaemon(true);
-	        return thread;
-	    }
+	public static class DaemonThreadFactory extends NamedThreadFactory {
+
+		/**
+		 * Creates a new factory and sets the thread group to Californium
+		 * default group.
+		 *
+		 * @param threadPrefix the prefix, that becomes part of the name of all
+		 *            threads, created by this factory.
+		 */
+		public DaemonThreadFactory(final String threadPrefix) {
+			super(threadPrefix, null);
+		}
+
+		/**
+		 * Creates a new factory.
+		 *
+		 * @param threadPrefix the prefix, that becomes part of the name of all
+		 *            threads, created by this factory.
+		 * @param threadGroup the thread group or <code>null</code>
+		 */
+		public DaemonThreadFactory(final String threadPrefix, final ThreadGroup threadGroup) {
+			super(threadPrefix, threadGroup);
+		}
+
+		/**
+		 * @see java.util.concurrent.ThreadFactory#newThread(java.lang.Runnable)
+		 */
+		@Override
+		public Thread newThread(Runnable runnable) {
+			final Thread thread = super.newThread(runnable);
+			thread.setDaemon(true);
+			return thread;
+		}
 	}
+
+	/**
+	 * The default thread factory
+	 */
+	public static class NamedThreadFactory implements ThreadFactory {
+
+		private final ThreadGroup group;
+		private final AtomicInteger index = new AtomicInteger(1);
+		private final String prefix;
+
+		/**
+		 * Creates a new factory and sets the thread group to Californium
+		 * default group.
+		 *
+		 * @param threadPrefix the prefix, that becomes part of the name of all
+		 *            threads, created by this factory.
+		 */
+		public NamedThreadFactory(final String threadPrefix) {
+			this(threadPrefix, null);
+		}
+
+		/**
+		 * Creates a new factory.
+		 *
+		 * @param threadPrefix the prefix, that becomes part of the name of all
+		 *            threads, created by this factory.
+		 * @param threadGroup the thread group or <code>null</code>
+		 */
+		public NamedThreadFactory(final String threadPrefix, final ThreadGroup threadGroup) {
+			group = null == threadGroup ? COAP_THREAD_GROUP : threadGroup;
+			prefix = threadPrefix;
+		}
+
+		/**
+		 * @see java.util.concurrent.ThreadFactory#newThread(java.lang.Runnable)
+		 */
+		@Override
+		public Thread newThread(Runnable runnable) {
+			final Thread ret = new Thread(group, runnable, prefix + index.getAndIncrement(), 0);
+			if (ret.isDaemon()) {
+				ret.setDaemon(false);
+			}
+			if (ret.getPriority() != Thread.NORM_PRIORITY) {
+				ret.setPriority(Thread.NORM_PRIORITY);
+			}
+			return ret;
+		}
+	}
+
 }

californium-core/src/main/java/org/eclipse/californium/core/network/CoapEndpoint.java
@@ -247,7 +247,8 @@ public class CoapEndpoint implements Endpoint {
 		if (this.executor == null) {
 			LOGGER.log(Level.CONFIG, "Endpoint [{0}] requires an executor to start, using default single-threaded daemon executor", getAddress());
 
-			final ScheduledExecutorService executor = Executors.newSingleThreadScheduledExecutor(new Utils.DaemonThreadFactory());
+			final ScheduledExecutorService executor = Executors.newSingleThreadScheduledExecutor(//
+					new Utils.DaemonThreadFactory("CoapEndpoint-" + connector.getAddress() + '#')); //$NON-NLS-1$
 			setExecutor(executor);
 			addObserver(new EndpointObserver() {
 				public void started(Endpoint endpoint) { }

californium-core/src/main/java/org/eclipse/californium/core/server/resources/ConcurrentCoapResource.java
@@ -24,6 +24,7 @@ import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
 import org.eclipse.californium.core.CoapResource;
+import org.eclipse.californium.core.Utils;
 import org.eclipse.californium.core.network.Exchange;
 
 /**
@@ -101,7 +102,8 @@ public class ConcurrentCoapResource extends CoapResource {
 	public ConcurrentCoapResource(String name) {
 		super(name);
 		this.threads = getAvailableProcessors();
-		setExecutor(Executors.newFixedThreadPool(threads));
+		setExecutor(Executors.newFixedThreadPool(threads,
+				new Utils.NamedThreadFactory("ConcurrentCoapResource-" + name + '#'))); //$NON-NLS-1$
 	}
 	
 	/**
@@ -114,7 +116,8 @@ public class ConcurrentCoapResource extends CoapResource {
 	public ConcurrentCoapResource(String name, int threads) {
 		super(name);
 		this.threads = threads;
-		setExecutor(Executors.newFixedThreadPool(threads));
+		setExecutor(Executors.newFixedThreadPool(threads,
+				new Utils.NamedThreadFactory("ConcurrentCoapResource-" + name + '#'))); //$NON-NLS-1$
 	}
 	
 	/**

element-connector/src/main/java/org/eclipse/californium/elements/UDPConnector.java
@@ -49,7 +49,9 @@ public class UDPConnector implements Connector {
 	public final static Logger LOGGER = Logger.getLogger(UDPConnector.class.getName());
 	
 	public static final int UNDEFINED = 0;
-	
+
+	static final ThreadGroup ELEMENTS_THREAD_GROUP = new ThreadGroup("Californium/Elements"); //$NON-NLS-1$
+
 	private boolean running;
 	
 	private DatagramSocket socket;
@@ -207,8 +209,8 @@ public class UDPConnector implements Connector {
 		 *
 		 * @param name the name
 		 */
-		private NetworkStageThread(String name) {
-			super(name);
+		protected NetworkStageThread(String name) {
+			super(ELEMENTS_THREAD_GROUP, name);
 			setDaemon(true);
 		}
 

commit e9b269dd9f14a616210d408233870d69313118c2

[bug #68] fix broken DTLS handshake
295217d introduced a regression which caused the last flight of a
successful handshake to always be retransmitted again and again. This
was due to the fact that we switched from java.util.Timer to
java.util.concurrent.ScheduledExecutorService for scheduling
retransmission of flights. The mechanism for cancelling scheduled tasks
in the latter is different from the former which had not been considered
when we made the change.

scandium-core/src/main/java/org/eclipse/californium/scandium/DTLSConnector.java
@@ -48,11 +48,11 @@ import java.security.cert.Certificate;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
-import java.util.TimerTask;
 import java.util.concurrent.BlockingQueue;
 import java.util.concurrent.Executors;
 import java.util.concurrent.LinkedBlockingQueue;
 import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.ScheduledFuture;
 import java.util.concurrent.ThreadFactory;
 import java.util.concurrent.TimeUnit;
 import java.util.concurrent.atomic.AtomicInteger;
@@ -1375,14 +1375,10 @@ public class DTLSConnector implements Connector {
 
 	private void scheduleRetransmission(DTLSFlight flight) {
 
-		// cancel existing schedule (if any)
-		if (flight.getRetransmitTask() != null) {
-			flight.getRetransmitTask().cancel();
-		}
+		// cancel existing retransmission task (if any)
+		flight.cancelRetransmission();
 
 		if (flight.isRetransmissionNeeded()) {
-			// create new retransmission task
-			flight.setRetransmitTask(new RetransmitTask(flight));
 
 			// calculate timeout using exponential back-off
 			if (flight.getTimeout() == 0) {
@@ -1394,7 +1390,8 @@ public class DTLSConnector implements Connector {
 			}
 
 			// schedule retransmission task
-			timer.schedule(flight.getRetransmitTask(), flight.getTimeout(), TimeUnit.MILLISECONDS);
+			ScheduledFuture<?> f = timer.schedule(new RetransmitTask(flight), flight.getTimeout(), TimeUnit.MILLISECONDS);
+			flight.setRetransmitTask(f);
 		}
 	}
 
@@ -1484,11 +1481,11 @@ public class DTLSConnector implements Connector {
 		};
 	}
 
-	private class RetransmitTask extends TimerTask {
+	private class RetransmitTask implements Runnable {
 
 		private DTLSFlight flight;
 
-		RetransmitTask(DTLSFlight flight) {
+		RetransmitTask(final DTLSFlight flight) {
 			this.flight = flight;
 		}
 

scandium-core/src/main/java/org/eclipse/californium/scandium/dtls/Connection.java
@@ -153,8 +153,7 @@ public final class Connection implements SessionListener {
 	 */
 	public void cancelPendingFlight() {
 		if (pendingFlight != null) {
-			pendingFlight.getRetransmitTask().cancel();
-			pendingFlight.setRetransmitTask(null);
+			pendingFlight.cancelRetransmission();
 			pendingFlight = null;
 		}
 	}

scandium-core/src/main/java/org/eclipse/californium/scandium/dtls/DTLSFlight.java
@@ -26,7 +26,7 @@ import java.security.GeneralSecurityException;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
-import java.util.TimerTask;
+import java.util.concurrent.ScheduledFuture;
 
 /**
  * A container for a set of DTLS records that are to be (re-)transmitted
@@ -35,8 +35,8 @@ import java.util.TimerTask;
  * DTLS messages are grouped into a series of message flights. One flight
  * consists of at least one message and needs to be re-transmitted until the
  * peer's next flight has arrived in its total. A flight needs not only consist of
- * {@link HandshakeMessage}s but may also contain {@link AlertMessage}s
- * and {@link ChangeCipherSpecMessage}s. See <a
+ * {@code HandshakeMessage}s but may also contain {@code AlertMessage}s
+ * and {@code ChangeCipherSpecMessage}s. See <a
  * href="http://tools.ietf.org/html/rfc6347#section-4.2.4">RFC 6347</a> for
  * details.
  */
@@ -70,7 +70,7 @@ public class DTLSFlight {
 	private boolean retransmissionNeeded = false;
 
 	/** The retransmission task. Needed when to cancel the retransmission. */
-	private TimerTask retransmitTask;
+	private ScheduledFuture<?> retransmitTask;
 
 	/**
 	 * Initializes an empty, fresh flight. The timeout is set to 0, it will be
@@ -96,7 +96,7 @@ public class DTLSFlight {
 	 * @param peerAddress the IP address and port to send the records to
 	 * @throws NullPointerException if peerAddress is <code>null</code>
 	 */
-	public DTLSFlight(InetSocketAddress peerAddress) {
+	public DTLSFlight(final InetSocketAddress peerAddress) {
 		if (peerAddress == null) {
 			throw new NullPointerException("Peer address must not be null");
 		}
@@ -114,20 +114,35 @@ public class DTLSFlight {
 	 *                 when sending out the flight
 	 * @throws NullPointerException if session is <code>null</code>
 	 */
-	public DTLSFlight(DTLSSession session) {
+	public DTLSFlight(final DTLSSession session) {
 		this(session.getPeer());
 		this.session = session;
 		retransmissionNeeded = true;
 	}
 
-	public void addMessage(List<Record> message) {
-		messages.addAll(message);
+	/**
+	 * Adds multiple messages to this flight.
+	 * 
+	 * @param messagesToAdd the messages to add.
+	 */
+	public void addMessage(final List<Record> messagesToAdd) {
+		this.messages.addAll(messagesToAdd);
 	}
 
-	public void addMessage(Record message) {
-		messages.add(message);
+	/**
+	 * Adds a single message to this flight.
+	 * 
+	 * @param messageToAdd the message to add.
+	 */
+	public void addMessage(final Record messageToAdd) {
+		this.messages.add(messageToAdd);
 	}
 
+	/**
+	 * Gets the messages to be sent as part of this flight.
+	 * 
+	 * @return an unmodifiable list of the messages.
+	 */
 	public List<Record> getMessages() {
 		return Collections.unmodifiableList(messages);
 	}
@@ -196,11 +211,21 @@ public class DTLSFlight {
 		this.retransmissionNeeded = needsRetransmission;
 	}
 
-	public TimerTask getRetransmitTask() {
+	/**
+	 * Cancels retransmission of this flight.
+	 */
+	public void cancelRetransmission() {
+		if (retransmitTask != null) {
+			retransmitTask.cancel(true);
+			this.retransmitTask = null;
+		}
+	}
+
+	public ScheduledFuture<?> getRetransmitTask() {
 		return retransmitTask;
 	}
 
-	public void setRetransmitTask(TimerTask retransmitTask) {
+	public void setRetransmitTask(final ScheduledFuture<?> retransmitTask) {
 		this.retransmitTask = retransmitTask;
 	}
 