3b3078b5c5d0db69d7cdefc0b92bd662863058d0
Kai Hudalla <kai.hudalla@bosch-si.com>
2016-09-22 13:51:35 +0200

    Reuse compiled regex pattern for literal IP detection.
    - compile regex pattern only once and reuse it
    - add test cases for URI handling

---
+++ californium-core/src/test/java/org/eclipse/californium/core/coap/RequestTest.java
@@ -0,0 +1,89 @@
+/*******************************************************************************
+ * Copyright (c) 2016 Bosch Software Innovations GmbH and others.
+ * 
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * and Eclipse Distribution License v1.0 which accompany this distribution.
+ * 
+ * The Eclipse Public License is available at
+ *    http://www.eclipse.org/legal/epl-v10.html
+ * and the Eclipse Distribution License is available at
+ *    http://www.eclipse.org/org/documents/edl-v10.html.
+ * 
+ * Contributors:
+ *    Bosch Software Innovations - initial creation
+ ******************************************************************************/
+package org.eclipse.californium.core.coap;
+
+import static org.hamcrest.CoreMatchers.*;
+import static org.junit.Assert.assertNull;
+import static org.junit.Assert.assertThat;
+
+import java.net.InetSocketAddress;
+import java.net.UnknownHostException;
+
+import org.eclipse.californium.category.Small;
+import org.junit.Test;
+import org.junit.experimental.categories.Category;
+
+
+/**
+ * Unit test cases validating behavior of the {@link Request} class.
+ *
+ */
+@Category(Small.class)
+public class RequestTest {
+
+	@Test(expected = IllegalArgumentException.class)
+	public void testSetURIRejectsUnsupportedScheme() {
+		Request.newGet().setURI("unknown://localhost");
+	}
+
+	@Test(expected = IllegalArgumentException.class)
+	public void testSetURIRejectsUnresolvableHost() {
+		Request.newGet().setURI("coap://non-existing.host");
+	}
+
+	@Test
+	public void testSetURIDoesNotSetUriHostOptionForIp4Address() {
+		Request req = Request.newGet().setURI("coap://192.168.0.1");
+		assertNull(req.getOptions().getUriHost());
+	}
+
+	@Test
+	public void testSetURIDoesNotSetUriHostOptionForIp6Address() {
+		// use www.google.com's IPv6 address
+		Request req = Request.newGet().setURI("coap://[2a00:1450:4001:817::2003]");
+		assertNull(req.getOptions().getUriHost());
+	}
+
+	@Test
+	public void testSetURISetsDestination() throws UnknownHostException {
+		InetSocketAddress dest = InetSocketAddress.createUnresolved("192.168.0.1", 12000);
+		Request req = Request.newGet().setURI("coap://192.168.0.1:12000");
+		assertThat(req.getDestination().getHostName(), is(dest.getHostName()));
+		assertThat(req.getDestinationPort(), is(dest.getPort()));
+	}
+
+	@Test
+	public void testSetURISetsUriHostOption() throws UnknownHostException {
+		Request req = Request.newGet().setURI("coap://iot.eclipse.org");
+		assertThat(req.getOptions().getUriHost(), is("iot.eclipse.org"));
+	}
+
+	@Test
+	public void testSetURISetsDestinationPortBasedOnUriScheme() throws UnknownHostException {
+		Request req = Request.newGet().setURI("coap://127.0.0.1");
+		assertThat(req.getDestinationPort(), is(CoAP.DEFAULT_COAP_PORT));
+
+		req = Request.newGet().setURI("coaps://127.0.0.1");
+		assertThat(req.getDestinationPort(), is(CoAP.DEFAULT_COAP_SECURE_PORT));
+
+		req = Request.newGet().setURI("coap+tcp://127.0.0.1");
+		assertThat(req.getDestinationPort(), is(CoAP.DEFAULT_COAP_PORT));
+
+		req = Request.newGet().setURI("coaps+tcp://127.0.0.1");
+		assertThat(req.getDestinationPort(), is(CoAP.DEFAULT_COAP_SECURE_PORT));
+	}
+
+}
