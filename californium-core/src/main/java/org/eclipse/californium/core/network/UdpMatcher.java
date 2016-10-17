c7e90f3cbb0d92f07f3716732da0b742a77e7809
Bala Azhagappan <balasubramanian.azhagappan@bosch-si.com>
2016-10-17 10:41:57 +0200

    Added few more comments to the fix.
    Signed-off-by: Bala Azhagappan <balasubramanian.azhagappan@bosch-si.com>

--- californium-core/src/main/java/org/eclipse/californium/core/network/UdpMatcher.java
+++ californium-core/src/main/java/org/eclipse/californium/core/network/UdpMatcher.java
@@ -322,12 +322,17 @@ public final class UdpMatcher extends BaseMatcher {
 
 					@Override
 					public void onResponse(Response response) {
+						// check whether the client has established the observe
+						// requested
 						if (!response.getOptions().hasObserve()) {
+							// Observe response received with no observe option
+							// set. It could be that the Client was not able to
+							// establish the observe. So remove the observe
+							// relation from observation store, which was stored
+							// earlier when the request was sent.
 							LOGGER.log(Level.FINE,
-									"Removed entry from observation store as observe response with token {0} didn't have observe option: ",
+									"Response to observe request with token {0} does not contain observe option, removing request from observation store",
 									idByToken);
-							// no observe option set. Client was not able to
-							// establish observe
 							observationStore.remove(request.getToken());
 						} else {
 							notificationListener.onNotification(request, response);
