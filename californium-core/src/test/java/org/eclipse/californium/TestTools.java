c80db61d12c00db46cc79c2bc6f2af733a916e46
Kai Hudalla <kai.hudalla@bosch-si.com>
2016-11-24 16:07:00 +0100

    Add helper methods for creating random payload.

--- californium-core/src/test/java/org/eclipse/californium/TestTools.java
+++ californium-core/src/test/java/org/eclipse/californium/TestTools.java
@@ -15,6 +15,9 @@
  ******************************************************************************/
 package org.eclipse.californium;
 
+import java.net.InetSocketAddress;
+import java.util.Random;
+
 import org.eclipse.californium.core.network.Endpoint;
 
 /**
@@ -22,6 +25,7 @@ import org.eclipse.californium.core.network.Endpoint;
  */
 public final class TestTools {
 
+	private static final Random RAND = new Random();
 	private static final String URI_TEMPLATE = "coap://%s:%d/%s";
 
 	private TestTools() {
@@ -36,6 +40,46 @@ public final class TestTools {
 	 * @return The URI string.
 	 */
 	public static String getUri(final Endpoint endpoint, final String path) {
-		return String.format(URI_TEMPLATE, endpoint.getAddress().getHostString(), endpoint.getAddress().getPort(), path);
+		return getUri(endpoint.getAddress(), path);
+	}
+
+	/**
+	 * Creates a URI string for a resource hosted on an endpoint.
+	 * 
+	 * @param address The address of the endpoint that the resource is hosted on.
+	 * @param path The path of the resource on the endpoint.
+	 * @return The URI string.
+	 */
+	public static String getUri(final InetSocketAddress address, final String path) {
+		return String.format(URI_TEMPLATE, address.getHostString(), address.getPort(), path);
+	}
+
+	/**
+	 * Creates a random string of numbers of a given length.
+	 * 
+	 * @param length The length of the string to create.
+	 * @return The string.
+	 */
+	public static String generateRandomPayload(int length) {
+		StringBuffer buffer = new StringBuffer();
+		while(buffer.length() < length) {
+			buffer.append(RAND.nextInt());
+		}
+		return buffer.substring(0, length);
+	}
+
+	/**
+	 * Creates a string of ascending single digit numbers of a given length.
+	 * 
+	 * @param length The length of the string to create.
+	 * @return The string.
+	 */
+	public static String generatePayload(int length) {
+		StringBuffer buffer = new StringBuffer();
+		int n = 1;
+		while(buffer.length() < length) {
+			buffer.append(n++);
+		}
+		return buffer.substring(0, length);
 	}
 }
