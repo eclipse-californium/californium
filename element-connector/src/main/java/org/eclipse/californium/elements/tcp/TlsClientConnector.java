4d9c28ae7523ec325652f91e6a6fa521bbf7ae63
Kai Hudalla <kai.hudalla@bosch-si.com>
2016-11-25 09:06:41 +0100

    [#154] Add missing license header.

--- element-connector/src/main/java/org/eclipse/californium/elements/tcp/TlsClientConnector.java
+++ element-connector/src/main/java/org/eclipse/californium/elements/tcp/TlsClientConnector.java
@@ -1,3 +1,18 @@
+/*******************************************************************************
+ * Copyright (c) 2016 Amazon Web Services.
+ * <p>
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * and Eclipse Distribution License v1.0 which accompany this distribution.
+ * <p>
+ * The Eclipse Public License is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ * and the Eclipse Distribution License is available at
+ * http://www.eclipse.org/org/documents/edl-v10.html.
+ * <p>
+ * Contributors:
+ * Joe Magerramov (Amazon Web Services) - CoAP over TCP support.
+ ******************************************************************************/
 package org.eclipse.californium.elements.tcp;
 
 import io.netty.channel.Channel;
