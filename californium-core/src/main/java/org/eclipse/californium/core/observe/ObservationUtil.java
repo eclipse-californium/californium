5bbc40a59ec11a7beebe5a39f671863bee5abe70
Achim Kraus <achim.kraus@bosch-si.com>
2017-01-13 12:41:22 +0100

    Add ObservationUtil.shallowClone.
    Reduce code duplications and complexity for library user.
    
    Signed-off-by: Achim Kraus <achim.kraus@bosch-si.com>

---
+++ californium-core/src/main/java/org/eclipse/californium/core/observe/ObservationUtil.java
@@ -0,0 +1,50 @@
+/*******************************************************************************
+ * Copyright (c) 2017 Bosch Software Innovations GmbH and others.
+ * 
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * and Eclipse Distribution License v1.0 which accompany this distribution.
+ * 
+ * The Eclipse Public License is available at
+ *    http://www.eclipse.org/legal/epl-v10.html
+ * and the Eclipse Distribution License is available at
+ *    http://www.eclipse.org/org/documents/edl-v10.html.
+ * 
+ * Contributors:
+ *    Bosch Software Innovations GmbH - initial implementation. 
+ ******************************************************************************/
+package org.eclipse.californium.core.observe;
+
+import org.eclipse.californium.core.coap.Request;
+
+/**
+ * Utility for observation.
+ */
+public final class ObservationUtil {
+
+	/**
+	 * Create shallow clone of observation and the contained request.
+	 * 
+	 * @return a cloned observation with a shallow clone of request, or null, if
+	 *         null was provided.
+	 * @throws IllegalArgumentException, if observation didn't contain a
+	 *             request.
+	 */
+	public static Observation shallowClone(Observation observation) {
+		if (null == observation) {
+			return null;
+		}
+		Request request = observation.getRequest();
+		if (null == request) {
+			throw new IllegalArgumentException("missing request for observation!");
+		}
+		Request clonedRequest = new Request(request.getCode());
+		clonedRequest.setType(request.getType());
+		clonedRequest.setMID(request.getMID());
+		clonedRequest.setToken(request.getToken());
+		clonedRequest.setOptions(request.getOptions());
+		clonedRequest.setPayload(request.getPayload());
+		clonedRequest.setUserContext(request.getUserContext());
+		return new Observation(clonedRequest, observation.getContext());
+	}
+}
