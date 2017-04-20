8914e441cce90ed1083f9e3cf2dedf48573689c4
Achim Kraus <achim.kraus@bosch-si.com>
2017-04-20 12:56:10 +0200

    Replace "response" with "next" in onAcknowledgement().
    Fix copy & paste error.
    
    Signed-off-by: Achim Kraus <achim.kraus@bosch-si.com>

--- californium-core/src/main/java/org/eclipse/californium/core/network/stack/ObserveLayer.java
+++ californium-core/src/main/java/org/eclipse/californium/core/network/stack/ObserveLayer.java
@@ -20,6 +20,9 @@
  *                                                    add NON notifications only to relation,
  *                                                    if they are really sent as NON.
  *                                                    (issue #258, RFC Section 4.5.2 of RFC 7641)
+ *    Achim Kraus (Bosch Software Innovations GmbH) - fix copy & paste error
+ *                                                    replace "response" with "next" in
+ *                                                    onAcknowledgement()
  ******************************************************************************/
 package org.eclipse.californium.core.network.stack;
 
@@ -179,8 +182,8 @@ public class ObserveLayer extends AbstractLayer {
 					 * The matcher must be able to find the NON notifications to remove
 					 * them from the exchangesByMID hashmap
 					 */
-					if (response.getType() == Type.NON) {
-						relation.addNotification(response);
+					if (next.getType() == Type.NON) {
+						relation.addNotification(next);
 					}
 					// Create a new task for sending next response so that we can leave the sync-block
 					executor.execute(new Runnable() {
