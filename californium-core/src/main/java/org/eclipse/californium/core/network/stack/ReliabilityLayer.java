40b11b9e38136536bef110fedfd94db207511190
Achim Kraus <achim.kraus@bosch-si.com>
2017-04-20 14:26:25 +0200

    Use synchronized to access exchange retry fields.
    Signed-off-by: Achim Kraus <achim.kraus@bosch-si.com>

--- californium-core/src/main/java/org/eclipse/californium/core/network/stack/ReliabilityLayer.java
+++ californium-core/src/main/java/org/eclipse/californium/core/network/stack/ReliabilityLayer.java
@@ -20,6 +20,7 @@
  *                                                    explicit String concatenation
  *    Achim Kraus (Bosch Software Innovations GmbH) - use final for fields and adjust
  *                                                    thread safe random usage
+ *    Achim Kraus (Bosch Software Innovations GmbH) - use synchronized to access exchange.
  ******************************************************************************/
 package org.eclipse.californium.core.network.stack;
 
@@ -161,15 +162,18 @@ public class ReliabilityLayer extends AbstractLayer {
 		 * For a new confirmable message, the initial timeout is set to a random
 		 * number between ACK_TIMEOUT and (ACK_TIMEOUT * ACK_RANDOM_FACTOR)
 		 */
-		int timeout;
-		if (exchange.getFailedTransmissionCount() == 0) {
-			timeout = getRandomTimeout(ack_timeout, (int) (ack_timeout * ack_random_factor));
-		} else {
-			timeout = (int) (ack_timeout_scale * exchange.getCurrentTimeout());
+		synchronized(exchange) {
+			int timeout;
+			if (exchange.getFailedTransmissionCount() == 0) {
+				timeout = getRandomTimeout(ack_timeout, (int) (ack_timeout * ack_random_factor));
+			} else {
+				timeout = (int) (ack_timeout_scale * exchange.getCurrentTimeout());
+			}
+			exchange.setCurrentTimeout(timeout);
+			exchange.setRetransmissionHandle(null); // cancel before reschedule
+			ScheduledFuture<?> f = executor.schedule(task, timeout, TimeUnit.MILLISECONDS);
+			exchange.setRetransmissionHandle(f);
 		}
-		exchange.setCurrentTimeout(timeout);
-		ScheduledFuture<?> f = executor.schedule(task, timeout, TimeUnit.MILLISECONDS);
-		exchange.setRetransmissionHandle(f);
 	}
 
 	/**
@@ -313,9 +317,11 @@ public class ReliabilityLayer extends AbstractLayer {
 			 * number of times.
 			 */
 			try {
-				int failedCount = exchange.getFailedTransmissionCount() + 1;
-				exchange.setFailedTransmissionCount(failedCount);
-
+				int failedCount;
+				synchronized(exchange) {
+					failedCount = exchange.getFailedTransmissionCount() + 1;
+					exchange.setFailedTransmissionCount(failedCount);
+				}
 				if (message.isAcknowledged()) {
 					LOGGER.log(Level.FINEST, "Timeout: message already acknowledged, cancel retransmission of {0}", message);
 					return;
@@ -335,9 +341,11 @@ public class ReliabilityLayer extends AbstractLayer {
 					message.retransmitting();
 
 					// MessageObserver might have canceled
-					if (!message.isCanceled()) {
-						retransmit();
+					if (message.isCanceled()) {
+						LOGGER.log(Level.FINER, "Timeout: canceled (MID={0}), do not retransmit", message.getMID());
+						return;
 					}
+					retransmit();
 				} else {
 					LOGGER.log(Level.FINE, "Timeout: retransmission limit reached, exchange failed, message: {0}", message);
 					exchange.setTimedOut();
