df722698848bfaa159ccc712e3ad6bbb00903200
Kai Hudalla <kai.hudalla@bosch-si.com>
2017-05-16 16:33:15 +0200

    Add digitalSignature key usage to demo certs for client and server.
    Improve certificates so that they can be used in handshakes where a
    server indicates client_certificate_types requiring signing capability.
    
    Conflicts:
    	demo-certs/src/main/resources/create-keystores.sh

--- demo-certs/src/main/resources/create-keystores.sh
+++ demo-certs/src/main/resources/create-keystores.sh
@@ -20,15 +20,14 @@ keytool -keystore $TRUST_STORE -storepass $TRUST_STORE_PWD -certreq -alias ca | 
 echo "creating server key and certificate..."
 keytool -genkeypair -alias server -keyalg EC -dname 'C=CA,L=Ottawa,O=Eclipse IoT,OU=Californium,CN=cf-server' \
         -validity $VALIDITY -keypass $KEY_STORE_PWD -keystore $KEY_STORE -storepass $KEY_STORE_PWD
-keytool -keystore $KEY_STORE -storepass endPass -certreq -alias server | \
-  keytool -keystore $TRUST_STORE -storepass $TRUST_STORE_PWD -alias ca -gencert -validity $VALIDITY -rfc > server.pem
+keytool -keystore $KEY_STORE -storepass $KEY_STORE_PWD -certreq -alias server | \
+  keytool -keystore $TRUST_STORE -storepass $TRUST_STORE_PWD -alias ca -gencert -ext KU=dig -validity $VALIDITY -rfc > server.pem
 keytool -alias server -importcert -keystore $KEY_STORE -storepass $KEY_STORE_PWD -trustcacerts -file server.pem
 
 echo "creating client key and certificate..."
 keytool -genkeypair -alias client -keyalg EC -dname 'C=CA,L=Ottawa,O=Eclipse IoT,OU=Californium,CN=cf-client' \
         -validity $VALIDITY -keypass $KEY_STORE_PWD -keystore $KEY_STORE -storepass $KEY_STORE_PWD
 keytool -keystore $KEY_STORE -storepass $KEY_STORE_PWD -certreq -alias client | \
-  keytool -keystore $TRUST_STORE -storepass $TRUST_STORE_PWD -alias ca -gencert -validity $VALIDITY -rfc > client.pem
+  keytool -keystore $TRUST_STORE -storepass $TRUST_STORE_PWD -alias ca -gencert -ext KU=dig -validity $VALIDITY -rfc > client.pem
 keytool -alias client -importcert -keystore $KEY_STORE -storepass $KEY_STORE_PWD -trustcacerts -file client.pem
-
 
