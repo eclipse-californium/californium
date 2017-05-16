f6c183500eb742fbc2b4b098e4f7bd8386a5ece6
Kai Hudalla <kai.hudalla@bosch-si.com>
2017-05-16 18:36:54 +0200

    [#209] Do not send certs contained in server's trust anchor.
    When sending the client's CERTIFICATE message, client handshaker
    truncates the client certificate chain to only include certs up to one
    issued by one of the trust anchor distinguished names conveyed in the
    server's CERTIFICATE_REQUEST message.

--- scandium-core/src/main/java/org/eclipse/californium/scandium/dtls/ClientHandshaker.java
+++ scandium-core/src/main/java/org/eclipse/californium/scandium/dtls/ClientHandshaker.java
@@ -41,7 +41,6 @@ import java.util.List;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
-import org.eclipse.californium.elements.util.StringUtil;
 import org.eclipse.californium.scandium.auth.PreSharedKeyIdentity;
 import org.eclipse.californium.scandium.auth.RawPublicKeyIdentity;
 import org.eclipse.californium.scandium.auth.X509CertPath;
@@ -583,12 +582,14 @@ public class ClientHandshaker extends Handshaker {
 				if (key != null) {
 					rawPublicKeyBytes = key.getEncoded();
 				}
-				LOGGER.log(Level.FINE, "sending CERTIFICATE with RawPublicKey [{0}] to server", ByteArrayUtils.toHexString(rawPublicKeyBytes));
+				LOGGER.log(Level.FINE, "sending CERTIFICATE message with client RawPublicKey [{0}] to server", ByteArrayUtils.toHexString(rawPublicKeyBytes));
 				clientCertificate = new CertificateMessage(rawPublicKeyBytes, session.getPeer());
 			} else {
 				X509Certificate[] clientChain = determineClientCertificateChain(certificateRequest);
-				LOGGER.log(Level.FINE, "sending CERTIFICATE with certificate chain [length: {0}] to server", clientChain.length);
-				clientCertificate = new CertificateMessage(clientChain, session.getPeer());
+				// make sure we only send certs not part of the server's trust anchor
+				X509Certificate[] truncatedChain = certificateRequest.removeTrustedCertificates(clientChain);
+				LOGGER.log(Level.FINE, "sending CERTIFICATE message with client certificate chain [length: {0}] to server", truncatedChain.length);
+				clientCertificate = new CertificateMessage(truncatedChain, session.getPeer());
 			}
 			flight.addMessage(wrapMessage(clientCertificate));
 		}
