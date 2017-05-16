5f04a8e832f2f2eebb6f7c7d01b2fcec9b0b4a77
Kai Hudalla <kai.hudalla@bosch-si.com>
2017-05-16 12:01:34 +0200

    [#209] Add information to certificate types required for matching.

--- scandium-core/src/main/java/org/eclipse/californium/scandium/dtls/CertificateRequest.java
+++ scandium-core/src/main/java/org/eclipse/californium/scandium/dtls/CertificateRequest.java
@@ -28,16 +28,18 @@ import javax.security.auth.x500.X500Principal;
 
 import org.eclipse.californium.elements.util.DatagramReader;
 import org.eclipse.californium.elements.util.DatagramWriter;
+import org.eclipse.californium.scandium.dtls.SignatureAndHashAlgorithm.HashAlgorithm;
+import org.eclipse.californium.scandium.dtls.SignatureAndHashAlgorithm.SignatureAlgorithm;
 
 
 /**
  * A non-anonymous server can optionally request a certificate from the client,
- * if appropriate for the selected cipher suite. This message, if sent, will
- * immediately follow the {@link ServerKeyExchange} message (if it is sent;
- * otherwise, this message follows the server's {@link CertificateMessage}
- * message). For further details see <a
- * href="http://tools.ietf.org/html/rfc5246#section-7.4.4">RFC 5246, 7.4.4.
- * Certificate Request</a>.
+ * if appropriate for the selected cipher suite.
+ * <p>
+ * This message, if sent, will immediately follow the {@link ServerKeyExchange} message (if it is sent;
+ * otherwise, this message follows the server's {@link CertificateMessage} message).
+ * 
+ * @see <a href="http://tools.ietf.org/html/rfc5246#section-7.4.4">RFC 5246, 7.4.4. Certificate Request</a>
  */
 public final class CertificateRequest extends HandshakeMessage {
 
@@ -54,30 +56,16 @@ public final class CertificateRequest extends HandshakeMessage {
 	private static final int SUPPORTED_SIGNATURE_LENGTH_BITS = 16;
 
 	private static final int CERTIFICATE_AUTHORITIES_LENGTH_BITS = 16;
-	
+
 	private static final int CERTIFICATE_AUTHORITY_LENGTH_BITS = 16;
 
 	private static final int SUPPORTED_SIGNATURE_BITS = 8;
 
 	// Members ////////////////////////////////////////////////////////
 
-	/** A list of the types of certificate types that the client may offer. */
-	private List<ClientCertificateType> certificateTypes;
-
-	/**
-	 * A list of the hash/signature algorithm pairs that the server is able to
-	 * verify, listed in descending order of preference.
-	 */
-	private List<SignatureAndHashAlgorithm> supportedSignatureAlgorithms;
-
-	/**
-	 * A list of the distinguished names of acceptable certificate_authorities,
-	 * represented in DER-encoded format. The list is between 0 and
-	 * 2<sup>16</sup>-1 bytes long, while one distinguished name can range from
-	 * 1 to 2<sup>16</sup>-1 bytes length. Therefore, the length in the
-	 * serialization must be handled carefully.
-	 */
-	private List<DistinguishedName> certificateAuthorities;
+	private final List<ClientCertificateType> certificateTypes = new ArrayList<>();
+	private final List<SignatureAndHashAlgorithm> supportedSignatureAlgorithms = new ArrayList<>();
+	private final List<DistinguishedName> certificateAuthorities = new ArrayList<>();
 
 	// Constructors ///////////////////////////////////////////////////
 	
@@ -88,11 +76,7 @@ public final class CertificateRequest extends HandshakeMessage {
 	 *           message has been received from or should be sent to
 	 */
 	public CertificateRequest(InetSocketAddress peerAddress) {
-		this(
-			new ArrayList<ClientCertificateType>(),
-			new ArrayList<SignatureAndHashAlgorithm>(),
-			new ArrayList<DistinguishedName>(),
-			peerAddress);
+		super(peerAddress);
 	}
 
 	/**
@@ -112,9 +96,15 @@ public final class CertificateRequest extends HandshakeMessage {
 			List<DistinguishedName> certificateAuthorities,
 			InetSocketAddress peerAddress) {
 		super(peerAddress);
-		this.certificateTypes = certificateTypes;
-		this.supportedSignatureAlgorithms = supportedSignatureAlgorithms;
-		this.certificateAuthorities = certificateAuthorities;
+		if (certificateTypes != null) {
+			this.certificateTypes.addAll(certificateTypes);
+		}
+		if (supportedSignatureAlgorithms != null) {
+			this.supportedSignatureAlgorithms.addAll(supportedSignatureAlgorithms);
+		}
+		if (certificateAuthorities != null) {
+			this.certificateAuthorities.addAll(certificateAuthorities);
+		}
 	}
 
 	// Methods ////////////////////////////////////////////////////////
@@ -129,7 +119,6 @@ public final class CertificateRequest extends HandshakeMessage {
 		// fixed: certificate type length field (1 byte) + supported signature
 		// algorithms length field (2 bytes) + certificate authorities length
 		// field (2 bytes) = 5 bytes
-		
 
 		return 5 + certificateTypes.size() + (supportedSignatureAlgorithms.size() * 2) + getCertificateAuthoritiesLength();
 	}
@@ -141,10 +130,10 @@ public final class CertificateRequest extends HandshakeMessage {
 		for (DistinguishedName distinguishedName : certificateAuthorities) {
 			certificateAuthLength += distinguishedName.getName().length + 2;
 		}
-		
+
 		return certificateAuthLength;
 	}
-	
+
 	@Override
 	public String toString() {
 		StringBuilder sb = new StringBuilder(super.toString());
@@ -199,35 +188,34 @@ public final class CertificateRequest extends HandshakeMessage {
 
 	public static HandshakeMessage fromByteArray(byte[] byteArray, InetSocketAddress peerAddress) {
 		DatagramReader reader = new DatagramReader(byteArray);
-		
+
 		int length = reader.read(CERTIFICATE_TYPES_LENGTH_BITS);
-		List<ClientCertificateType> certificateTypes = new ArrayList<ClientCertificateType>();
+		List<ClientCertificateType> certificateTypes = new ArrayList<>();
 		for (int i = 0; i < length; i++) {
 			int code = reader.read(CERTIFICATE_TYPE_BITS);
 			certificateTypes.add(ClientCertificateType.getTypeByCode(code));
 		}
-		
+
 		length = reader.read(SUPPORTED_SIGNATURE_LENGTH_BITS);
-		List<SignatureAndHashAlgorithm> supportedSignatureAlgorithms = new ArrayList<SignatureAndHashAlgorithm>();
+		List<SignatureAndHashAlgorithm> supportedSignatureAlgorithms = new ArrayList<>();
 		for (int i = 0; i < length; i += 2) {
 			int codeHash = reader.read(SUPPORTED_SIGNATURE_BITS);
 			int codeSignature = reader.read(SUPPORTED_SIGNATURE_BITS);
-			supportedSignatureAlgorithms.add(new SignatureAndHashAlgorithm(SignatureAndHashAlgorithm.HashAlgorithm.getAlgorithmByCode(codeHash), SignatureAndHashAlgorithm.SignatureAlgorithm.getAlgorithmByCode(codeSignature)));
+			supportedSignatureAlgorithms.add(new SignatureAndHashAlgorithm(HashAlgorithm.getAlgorithmByCode(codeHash),
+					SignatureAlgorithm.getAlgorithmByCode(codeSignature)));
 		}
-		
+
 		length = reader.read(CERTIFICATE_AUTHORITIES_LENGTH_BITS);
-		List<DistinguishedName> certificateAuthorities = new ArrayList<DistinguishedName>();
+		List<DistinguishedName> certificateAuthorities = new ArrayList<>();
 		while (length > 0) {
 			int nameLength = reader.read(CERTIFICATE_AUTHORITY_LENGTH_BITS);
 			byte[] name = reader.readBytes(nameLength);
 			certificateAuthorities.add(new DistinguishedName(name));
-			
-			length -= 2 + name.length;
-			
-		}
-		
-		return new CertificateRequest(certificateTypes, supportedSignatureAlgorithms, certificateAuthorities, peerAddress);
 
+			length -= 2 + name.length;
+		}
+
+		return new CertificateRequest(certificateTypes, supportedSignatureAlgorithms, certificateAuthorities, peerAddress);
 	}
 
 	// Enums //////////////////////////////////////////////////////////
@@ -237,21 +225,74 @@ public final class CertificateRequest extends HandshakeMessage {
 	 * href="http://tools.ietf.org/html/rfc5246#section-7.4.4">RFC 5246</a> for
 	 * details.
 	 */
-	public enum ClientCertificateType {
-		RSA_SIGN(1), DSS_SIGN(2), RSA_FIXED_DH(3), DSS_FIXED_DH(4), RSA_EPHEMERAL_DH_RESERVED(5),
-		DSS_EPHEMERAL_DH_RESERVED(6), FORTEZZA_DMS_RESERVED(20), ECDSA_SIGN(64), RSA_FIXED_ECDH(65),
-		ECDSA_FIXED_ECDH(66);
+	public static enum ClientCertificateType {
 
-		private int code;
+		RSA_SIGN(1, "RSA", true),
+		DSS_SIGN(2, "DSA", true),
+		RSA_FIXED_DH(3, "DH", false),
+		DSS_FIXED_DH(4, "DH", false),
+		RSA_EPHEMERAL_DH_RESERVED(5, "DH", false),
+		DSS_EPHEMERAL_DH_RESERVED(6, "DH", false),
+		FORTEZZA_DMS_RESERVED(20, "UNKNOWN", false),
+		ECDSA_SIGN(64, "EC", true),
+		RSA_FIXED_ECDH(65, "DH", false),
+		ECDSA_FIXED_ECDH(66, "DH", false);
 
-		private ClientCertificateType(int code) {
+		private final int code;
+		private final String jcaAlgorithm;
+		private final boolean requiresSigningCapability;
+
+		private ClientCertificateType(int code, String algorithm, boolean requiresSigningCapability) {
 			this.code = code;
+			this.jcaAlgorithm = algorithm;
+			this.requiresSigningCapability = requiresSigningCapability;
 		}
 
+		/**
+		 * Gets this certificate type's binary code as defined by
+		 * <a href="http://tools.ietf.org/html/rfc5246#section-7.4.4">RFC 5246, Section 7.4.4</a>.
+		 * 
+		 * @return The code.
+		 */
 		public int getCode() {
 			return code;
 		}
 
+		/**
+		 * Gets the JCA standard key algorithm name this certificate type is compatible with.
+		 * 
+		 * @return The algorithm name.
+		 */
+		public String getJcaAlgorithm() {
+			return jcaAlgorithm;
+		}
+
+		/**
+		 * Indicates whether this certificate type requires the key to allow being used for signing.
+		 * 
+		 * @return {@code true} if signing capability is required.
+		 */
+		public boolean requiresSigningCapability() {
+			return requiresSigningCapability;
+		}
+
+		/**
+		 * Checks if this certificate type is compatible with a given JCA standard key algorithm.
+		 * 
+		 * @param algorithm The key algorithm.
+		 * @return {@code true} if this certificate type is compatible with the given key algorithm.
+		 */
+		public boolean isCompatibleWithKeyAlgorithm(String algorithm) {
+			return this.jcaAlgorithm.equals(algorithm);
+		}
+
+		/**
+		 * Gets a certificate type by its code as defined by
+		 * <a href="http://tools.ietf.org/html/rfc5246#section-7.4.4">RFC 5246, Section 7.4.4</a>.
+		 * 
+		 * @param code The code.
+		 * @return The certificate type or {@code null} if the given code is not defined.
+		 */
 		public static ClientCertificateType getTypeByCode(int code) {
 			for (ClientCertificateType type : values()) {
 				if (type.code == code) {
@@ -277,6 +318,16 @@ public final class CertificateRequest extends HandshakeMessage {
 		public byte[] getName() {
 			return name;
 		}
+
+		/**
+		 * Gets this distinguished name as a principal so that it can be compared to
+		 * other certificates' issuer.
+		 * 
+		 * @return The principal.
+		 */
+		public X500Principal asPrincipal() {
+			return new X500Principal(name);
+		}
 	}
 
 	// Getters and Setters ////////////////////////////////////////////
@@ -290,6 +341,7 @@ public final class CertificateRequest extends HandshakeMessage {
 	}
 
 	public void addCertificateAuthority(DistinguishedName authority) {
+		// TODO make sure that the max size (2^16 - 1 bytes) is not exceeded
 		certificateAuthorities.add(authority);
 	}
 
@@ -297,26 +349,46 @@ public final class CertificateRequest extends HandshakeMessage {
 	 * Takes a list of trusted certificates, extracts the subject principal and
 	 * adds the DER-encoded distinguished name to the certificate authorities.
 	 * 
-	 * @param certificateAuthorities
+	 * @param trustedCas
 	 *            trusted certificates.
 	 */
-	public void addCertificateAuthorities(X509Certificate[] certificateAuthorities) {
-		if (certificateAuthorities != null){
-			for (X509Certificate certificate : certificateAuthorities) {
+	public void addCertificateAuthorities(X509Certificate[] trustedCas) {
+		if (trustedCas != null){
+			for (X509Certificate certificate : trustedCas) {
 				byte[] ca = certificate.getSubjectX500Principal().getEncoded();
 				addCertificateAuthority(new DistinguishedName(ca));
 			}
 		}
 	}
 
+	/**
+	 * Gets the certificate types that the client may offer.
+	 *
+	 * @return The certificate types (never {@code null}.
+	 */
 	public List<ClientCertificateType> getCertificateTypes() {
 		return Collections.unmodifiableList(certificateTypes);
 	}
 
+	/**
+	 * Gets the signature algorithms that the server is able to verify.
+	 * 
+	 * @return The supported algorithms in order of preference (never {@code null}).
+	 */
 	public List<SignatureAndHashAlgorithm> getSupportedSignatureAlgorithms() {
 		return Collections.unmodifiableList(supportedSignatureAlgorithms);
 	}
 
+	/**
+	 * Gets the distinguished names of certificate authorities trusted by the server.
+	 * <p>
+	 * The names are provided in DER-encoded ASN.1 format. The list is between 0 and
+	 * 2<sup>16</sup>-1 bytes long, while one distinguished name can range from
+	 * 1 to 2<sup>16</sup>-1 bytes length. Therefore, the length in the
+	 * serialization must be handled carefully.
+	 * 
+	 * @return The distinguished names (never {@code null}).
+	 */
 	public List<DistinguishedName> getCertificateAuthorities() {
 		return Collections.unmodifiableList(certificateAuthorities);
 	}
