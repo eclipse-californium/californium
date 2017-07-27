299aff01d47216de254a142c8d8fe7e9c6c6a0f0
Achim Kraus <achim.kraus@bosch-si.com>
2017-07-27 13:53:16 +0200

    Limit DISCOVER search to 1 query.
    Signed-off-by: Achim Kraus <achim.kraus@bosch-si.com>

--- californium-core/src/test/java/org/eclipse/californium/core/test/ResourceAttributesTest.java
+++ californium-core/src/test/java/org/eclipse/californium/core/test/ResourceAttributesTest.java
@@ -16,15 +16,30 @@
  *    Dominique Im Obersteg - parsers and initial implementation
  *    Daniel Pauli - parsers and initial implementation
  *    Kai Hudalla - logging
+ *    Achim Kraus (Bosch Software Innovations GmbH) - test limited search to 1 query.
  ******************************************************************************/
 package org.eclipse.californium.core.test;
 
+import java.io.IOException;
+import java.net.InetSocketAddress;
 import java.util.LinkedList;
+import java.util.List;
+import java.util.concurrent.ScheduledExecutorService;
 
 import org.eclipse.californium.category.Small;
 import org.eclipse.californium.core.CoapResource;
 import org.eclipse.californium.core.coap.Request;
+import org.eclipse.californium.core.coap.Response;
+import org.eclipse.californium.core.coap.CoAP.ResponseCode;
+import org.eclipse.californium.core.coap.EmptyMessage;
+import org.eclipse.californium.core.network.Endpoint;
 import org.eclipse.californium.core.network.EndpointManager;
+import org.eclipse.californium.core.network.EndpointObserver;
+import org.eclipse.californium.core.network.Exchange;
+import org.eclipse.californium.core.network.Exchange.Origin;
+import org.eclipse.californium.core.network.config.NetworkConfig;
+import org.eclipse.californium.core.network.interceptors.MessageInterceptor;
+import org.eclipse.californium.core.server.MessageDeliverer;
 import org.eclipse.californium.core.server.resources.DiscoveryResource;
 import org.eclipse.californium.core.server.resources.Resource;
 import org.junit.Assert;
@@ -88,5 +103,96 @@ public class ResourceAttributesTest {
 		        "</sensors/light>;if=\"sensor\";rt=\"light-lux\"",
 		        serialized);
 	}
+
+	@Test
+	public void testDiscoveryMultiFiltering() {
+		Request request = Request.newGet();
+		request.setURI("coap://localhost/.well-known/core?rt=light-lux&rt=temprature-cel");
 	
+		Exchange exchange = new Exchange(request, Origin.REMOTE);
+		exchange.setRequest(request);
+		exchange.setEndpoint(new DummyEndpoint());
+		
+		DiscoveryResource discovery = new DiscoveryResource(root);
+		
+		discovery.handleRequest(exchange);
+		System.out.println(exchange.getResponse().getPayloadString());
+		Assert.assertEquals(ResponseCode.BAD_OPTION, exchange.getResponse().getCode());
+	}
+	
+	private static class DummyEndpoint implements Endpoint {
+
+		@Override
+		public void start() throws IOException {
+		}
+
+		@Override
+		public void stop() {
+		}
+
+		@Override
+		public void destroy() {
+		}
+
+		@Override
+		public void clear() {
+		}
+
+		@Override
+		public boolean isStarted() {
+			return false;
+		}
+
+		@Override
+		public void setExecutor(ScheduledExecutorService executor) {
+		}
+
+		@Override
+		public void addObserver(EndpointObserver obs) {
+		}
+
+		@Override
+		public void removeObserver(EndpointObserver obs) {
+		}
+
+		@Override
+		public void addInterceptor(MessageInterceptor interceptor) {
+		}
+
+		@Override
+		public void removeInterceptor(MessageInterceptor interceptor) {
+		}
+
+		@Override
+		public List<MessageInterceptor> getInterceptors() {
+			return null;
+		}
+
+		@Override
+		public void sendRequest(Request request) {
+		}
+
+		@Override
+		public void sendResponse(Exchange exchange, Response response) {
+		}
+
+		@Override
+		public void sendEmptyMessage(Exchange exchange, EmptyMessage message) {
+		}
+
+		@Override
+		public void setMessageDeliverer(MessageDeliverer deliverer) {
+		}
+
+		@Override
+		public InetSocketAddress getAddress() {
+			return null;
+		}
+
+		@Override
+		public NetworkConfig getConfig() {
+			return null;
+		}
+		
+	}
 }
