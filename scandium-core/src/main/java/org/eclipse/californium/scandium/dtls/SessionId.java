20252e50bce5e6e3e5ce48208486f4ca3a9d779b
Achim Kraus <achim.kraus@bosch-si.com>
2017-08-23 09:42:37 +0200

    Reuse id as string.
    Signed-off-by: Achim Kraus <achim.kraus@bosch-si.com>

--- scandium-core/src/main/java/org/eclipse/californium/scandium/dtls/SessionId.java
+++ scandium-core/src/main/java/org/eclipse/californium/scandium/dtls/SessionId.java
@@ -17,6 +17,7 @@
  *    Kai Hudalla (Bosch Software Innovations GmbH) - add toString()
  *    Achim Kraus (Bosch Software Innovations GmbH) - use toHex() for compact
  *                                                    hex representation. 
+ *    Achim Kraus (Bosch Software Innovations GmbH) - reuse id as string
  ******************************************************************************/
 package org.eclipse.californium.scandium.dtls;
 
@@ -34,11 +35,12 @@ public final class SessionId {
 	// Members ////////////////////////////////////////////////////////
 	private static final SessionId EMPTY_SESSION_ID = new SessionId(new byte[0]);
 	private final byte[] id; // opaque SessionID<0..32>
-
+	private final String text; 
 	// Constructors ///////////////////////////////////////////////////
 
 	public SessionId() {
 		id = new Random(new SecureRandom()).getRandomBytes();
+		text = ByteArrayUtils.toHex(id);
 	}
 
 	/**
@@ -52,6 +54,7 @@ public final class SessionId {
 			throw new NullPointerException("Session ID must not be null");
 		}
 		this.id = Arrays.copyOf(sessionId, sessionId.length);
+		text = ByteArrayUtils.toHex(id);
 	}
 
 	// Methods ////////////////////////////////////////////////////////
@@ -117,6 +120,6 @@ public final class SessionId {
 	 */
 	@Override
 	public String toString() {
-		return ByteArrayUtils.toHex(id);
+		return text;
 	}
 }
