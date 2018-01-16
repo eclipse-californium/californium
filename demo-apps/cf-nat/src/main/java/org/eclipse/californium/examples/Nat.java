051b9fd2a3b02ece66d40f3a7a98e078cad75c1c
Achim Kraus <achim.kraus@bosch-si.com>
2018-01-16 12:08:11 +0100

    Add message dropping to nat.
    Signed-off-by: Achim Kraus <achim.kraus@bosch-si.com>

--- demo-apps/cf-nat/src/main/java/org/eclipse/californium/examples/Nat.java
+++ demo-apps/cf-nat/src/main/java/org/eclipse/californium/examples/Nat.java
@@ -12,6 +12,7 @@
  * 
  * Contributors:
  *    Achim Kraus (Bosch Software Innovations GmbH) - initial implementation.
+ *    Achim Kraus (Bosch Software Innovations GmbH) - add message dropping.
  ******************************************************************************/
 
 package org.eclipse.californium.examples;
@@ -30,16 +31,65 @@ import java.net.URISyntaxException;
 public class Nat {
 
 	public static void main(String[] args) {
-		if (args.length != 2) {
-			System.out.println("usage: [localinterface]:port destination:port");
+		if (args.length < 2 || args.length > 4) {
+			System.out.println(
+					"usage: [localinterface]:port destination:port [<messageDropping%>|-f<messageDropping%>|-b<messageDropping%>]");
+			System.out.println(
+					"       <messageDropping%>   : drops forward and backward messages with provided probability");
+			System.out.println("       -f<messageDropping%> : drops forward messages with provided probability");
+			System.out.println("       -b<messageDropping%> : drops backward messages with provided probability");
+			System.out.println("       use -f and/or -b, if you want to test with different probabilities.");
 			return;
 		}
 		NatUtil util = null;
 		try {
+			String line = null;
 			InetSocketAddress proxyAddress = create(args[0]);
 			InetSocketAddress destinationAddress = create(args[1]);
-			String line = null;
+
 			util = new NatUtil(proxyAddress, destinationAddress);
+			if (args.length > 2) {
+				try {
+					String mode = "";
+					String dropping = args[2];
+					if (dropping.startsWith("-f") || dropping.startsWith("-b")) {
+						mode = dropping.substring(0, 2);
+						dropping = dropping.substring(2);
+					}
+					int drops = Integer.parseInt(dropping);
+					if (mode.equals("-f")) {
+						util.setForwardMessageDropping(drops);
+						System.out.println("dropping " + drops + "% of forward messages.");
+					} else if (mode.equals("-b")) {
+						util.setBackwardMessageDropping(drops);
+						System.out.println("dropping " + drops + "% of backward messages.");
+					} else {
+						util.setMessageDropping(drops);
+						System.out.println("dropping " + drops + "% of messages.");
+					}
+					if (args.length > 3) {
+						String mode2 = "";
+						dropping = args[3];
+						if (dropping.startsWith("-f") || dropping.startsWith("-b")) {
+							mode2 = dropping.substring(0, 2);
+							dropping = dropping.substring(2);
+						}
+						if (mode.equals(mode2)) {
+							System.out.println(args[3] + " ignored, would overwrite " + args[2]);
+						}
+						drops = Integer.parseInt(dropping);
+						if (mode2.equals("-f")) {
+							util.setForwardMessageDropping(drops);
+							System.out.println("dropping " + drops + "% of forward messages.");
+						} else if (mode2.equals("-b")) {
+							util.setBackwardMessageDropping(drops);
+							System.out.println("dropping " + drops + "% of backward messages.");
+						}
+					}
+				} catch (NumberFormatException e) {
+					System.err.println("drops% " + args[2] + " is no valid number!");
+				}
+			}
 			BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
 			while ((line = in.readLine()) != null) {
 				if (line.equals("exit")) {
