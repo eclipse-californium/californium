f564a47a16d316a0b745d1617447e2de220d753b
Simon Bernard <sbernard@sierrawireless.com>
2018-04-09 16:04:30 +0200

    tests to check UNKNOWN IDENTITY and BAD PSK are ignored in a same way

--- scandium-core/src/test/java/org/eclipse/californium/scandium/DTLSConnectorTest.java
+++ scandium-core/src/test/java/org/eclipse/californium/scandium/DTLSConnectorTest.java
@@ -106,6 +106,7 @@ import org.eclipse.californium.scandium.dtls.SessionAdapter;
 import org.eclipse.californium.scandium.dtls.SessionId;
 import org.eclipse.californium.scandium.dtls.cipher.CipherSuite;
 import org.eclipse.californium.scandium.dtls.pskstore.InMemoryPskStore;
+import org.eclipse.californium.scandium.dtls.pskstore.PskStore;
 import org.eclipse.californium.scandium.dtls.pskstore.StaticPskStore;
 import org.eclipse.californium.scandium.rule.DtlsNetworkRule;
 import org.junit.After;
@@ -1473,18 +1474,26 @@ public class DTLSConnectorTest {
 	}
 
 	/**
-	 * Verifies that the connector terminates a handshake if the PSK identity provided by the
-	 * client is unknown.
-	 * 
-	 * @throws Exception if the test fails.
+	 * Verifies that connector ignores CLIENT KEY EXCHANGE with unknown PSK identity.
 	 */
 	@Test
-	public void testConnectorAbortsHandshakeOnUnknownPskIdentity() throws Exception {
+	public void testConnectorIgnoresUnknownPskIdentity() throws Exception {
+		ensureConnectorIgnoresBadCredentials(new StaticPskStore("unknownIdentity", CLIENT_IDENTITY_SECRET.getBytes()));
+	}
 
+	/**
+	 * Verifies that connector ignores FINISHED message with bad PSK.
+	 */
+	@Test
+	public void testConnectorIgnoresBadPsk() throws Exception {
+		ensureConnectorIgnoresBadCredentials(new StaticPskStore(CLIENT_IDENTITY, "bad_psk".getBytes()));
+	}
+
+	private void ensureConnectorIgnoresBadCredentials(PskStore pskStoreWithBadCredentials) throws Exception {
 		final CountDownLatch latch = new CountDownLatch(1);
 		clientConfig = new DtlsConnectorConfig.Builder()
 			.setAddress(clientEndpoint)
-			.setPskStore(new StaticPskStore("unknownIdentity", CLIENT_IDENTITY_SECRET.getBytes()))
+			.setPskStore(pskStoreWithBadCredentials)
 			.build();
 		client = new DTLSConnector(clientConfig);
 		client.start();
@@ -1510,8 +1519,8 @@ public class DTLSConnectorTest {
 		RawData data = RawData.outbound("Hello".getBytes(), new AddressEndpointContext(serverEndpoint), callback, false);
 		client.send(data);
 
-		assertTrue(latch.await(MAX_TIME_TO_WAIT_SECS, TimeUnit.SECONDS));
-		assertThat(alert.get().getLevel(), is(AlertLevel.FATAL));
+		assertFalse(latch.await(MAX_TIME_TO_WAIT_SECS, TimeUnit.SECONDS));
+		assertThat(alert.get(), is(nullValue()));
 	}
 
 	/**
