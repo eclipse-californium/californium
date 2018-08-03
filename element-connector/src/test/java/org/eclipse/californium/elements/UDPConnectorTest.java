723f1728ce642c216898b9c9f21a93424dd5d072
Achim Kraus <achim.kraus@bosch-si.com>
2018-08-03 13:27:26 +0200

    Add tests for MessageCallback and start/stop.
    Signed-off-by: Achim Kraus <achim.kraus@bosch-si.com>

--- element-connector/src/test/java/org/eclipse/californium/elements/UDPConnectorTest.java
+++ element-connector/src/test/java/org/eclipse/californium/elements/UDPConnectorTest.java
@@ -14,18 +14,23 @@
  *    Bosch Software Innovations - initial creation
  *    Achim Kraus (Bosch Software Innovations GmbH) - add test for
  *                                                    CorrelationContextMatcher
+ *    Achim Kraus (Bosch Software Innovations GmbH) - add tests for MessageCallback
+ *                                                    add tests for start/stop
  ******************************************************************************/
 package org.eclipse.californium.elements;
 
 import static org.hamcrest.CoreMatchers.is;
-import static org.hamcrest.Matchers.*;
-import static org.junit.Assert.*;
+import static org.hamcrest.Matchers.notNullValue;
+import static org.hamcrest.Matchers.sameInstance;
+import static org.junit.Assert.assertThat;
 
 import java.io.IOException;
 import java.net.InetAddress;
 import java.net.InetSocketAddress;
 import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.atomic.AtomicInteger;
 
+import org.eclipse.californium.elements.util.SimpleMessageCallback;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
@@ -33,43 +38,134 @@ import org.junit.Test;
 public class UDPConnectorTest {
 
 	UDPConnector connector;
+	UDPConnector destination;
 	TestEndpointContextMatcher matcher;
 
 	@Before
 	public void setup() throws IOException {
-		matcher = new TestEndpointContextMatcher(1);
+		matcher = new TestEndpointContextMatcher(1, 1);
 		connector = new UDPConnector(new InetSocketAddress(InetAddress.getLoopbackAddress(), 0));
 		connector.setEndpointContextMatcher(matcher);
 		connector.start();
+		destination = new UDPConnector(new InetSocketAddress(InetAddress.getLoopbackAddress(), 0));
+		destination.setRawDataReceiver(new RawDataChannel() {
+
+			@Override
+			public void receiveData(RawData raw) {
+			}
+		});
+		destination.start();
 	}
 
 	@After
 	public void stop() {
 		connector.destroy();
+		destination.destroy();
 	}
 
 	@Test
 	public void testSendMessageWithEndpointContext() throws InterruptedException {
 		byte[] data = { 0, 1, 2 };
-		InetSocketAddress dest = new InetSocketAddress(0);
-		EndpointContext context = new DtlsEndpointContext(dest, null, "session", "1", "CIPHER");
-		
+		InetSocketAddress dest = destination.getAddress();
+		EndpointContext context = new UdpEndpointContext(dest);
+
 		RawData message = RawData.outbound(data, context, null, false);
-		connector.setEndpointContextMatcher(matcher);
 		connector.send(message);
-		
+
 		matcher.await();
-		
+
 		assertThat(matcher.getMessageEndpointContext(), is(sameInstance(context)));
+	}
+
+	@Test
+	public void testMessageCallbackOnContextEstablished() throws InterruptedException {
+		byte[] data = { 0, 1, 2 };
+		InetSocketAddress dest = destination.getAddress();
+		EndpointContext context = new UdpEndpointContext(dest);
+
+		SimpleMessageCallback callback = new SimpleMessageCallback(1, true);
+		RawData message = RawData.outbound(data, context, callback, false);
+		connector.send(message);
+
+		callback.await(100);
+		assertThat(callback.toString(), callback.getEndpointContext(), is(notNullValue()));
+	}
+
+	@Test
+	public void testMessageCallbackOnSent() throws InterruptedException {
+		byte[] data = { 0, 1, 2 };
+		InetSocketAddress dest = destination.getAddress();
+		EndpointContext context = new UdpEndpointContext(dest);
+
+		SimpleMessageCallback callback = new SimpleMessageCallback(1, false);
+		RawData message = RawData.outbound(data, context, callback, false);
+		connector.send(message);
+
+		callback.await(100);
+		assertThat(callback.toString(), callback.isSent(), is(true));
+	}
+
+	@Test
+	public void testMessageCallbackOnError() throws InterruptedException {
+		byte[] data = { 0, 1, 2 };
+		InetSocketAddress dest = destination.getAddress();
+		EndpointContext context = new UdpEndpointContext(dest);
+
+		matcher = new TestEndpointContextMatcher(1, 0);
+		connector.setEndpointContextMatcher(matcher);
+
+		SimpleMessageCallback callback = new SimpleMessageCallback(1, false);
+		RawData message = RawData.outbound(data, context, callback, false);
+		connector.send(message);
+
+		callback.await(100);
+		assertThat(callback.toString(), callback.getError(), is(notNullValue()));
+	}
+
+	@Test
+	public void testStopCallsMessageCallbackOnError() throws InterruptedException {
+		testStopCallsMessageCallbackOnError(100, 20);
+	}
+
+	@Test
+	public void testStopCallsMessageCallbackOnErrorCirtical() throws InterruptedException {
+		testStopCallsMessageCallbackOnError(1, 20);
+	}
+
+	private void testStopCallsMessageCallbackOnError(final int pending, final int loops) throws InterruptedException {
+		byte[] data = { 0, 1, 2 };
+		InetSocketAddress dest = destination.getAddress();
+		EndpointContext context = new UdpEndpointContext(dest);
+
+		for (int loop = 0; loop < loops; ++loop) {
+			System.out.format("start/stop: %d/%d loops, %d msgs%n", loop, loops, pending);
+			TestEndpointContextMatcher matcher = new TestEndpointContextMatcher(pending, pending);
+			connector.setEndpointContextMatcher(matcher);
+
+			SimpleMessageCallback callback = new SimpleMessageCallback(pending, false);
+			for (int i = 0; i < pending; ++i) {
+				RawData message = RawData.outbound(data, context, callback, false);
+				connector.send(message);
+			}
+			connector.stop();
+			assertThat(loop + ": " + callback.toString(), callback.await(100), is(true));
+			try {
+				connector.start();
+				Thread.sleep(20);
+			} catch (IOException e) {
+			}
+		}
 	}
 
 	private static class TestEndpointContextMatcher implements EndpointContextMatcher {
 
 		private final CountDownLatch latchSendMatcher;
+		private final AtomicInteger matches;
 		private EndpointContext messageContext;
 
-		public TestEndpointContextMatcher(int count) {
+		public TestEndpointContextMatcher(int count, int matches) {
 			this.latchSendMatcher = new CountDownLatch(count);
+			this.matches = new AtomicInteger(matches);
 		}
 
 		public synchronized EndpointContext getMessageEndpointContext() {
@@ -92,13 +188,12 @@ public class UDPConnectorTest {
 				this.messageContext = messageContext;
 			}
 			latchSendMatcher.countDown();
-			return 0 < latchSendMatcher.getCount();
+			return 0 < matches.getAndDecrement();
 		}
-		
+
 		public void await() throws InterruptedException {
 			latchSendMatcher.await();
 		}
 
-	};
-
+	}
 }
