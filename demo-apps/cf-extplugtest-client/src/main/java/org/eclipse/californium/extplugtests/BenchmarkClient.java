e7b638141b41d1db2d752a3a2a940ec90ea2df23
Achim Kraus <achim.kraus@bosch-si.com>
2018-08-29 11:11:05 +0200

    Adjust tcp connection timeout to 5s.
    Signed-off-by: Achim Kraus <achim.kraus@bosch-si.com>

--- demo-apps/cf-extplugtest-client/src/main/java/org/eclipse/californium/extplugtests/BenchmarkClient.java
+++ demo-apps/cf-extplugtest-client/src/main/java/org/eclipse/californium/extplugtests/BenchmarkClient.java
@@ -124,7 +124,7 @@ public class BenchmarkClient {
 			config.setInt(Keys.MAX_ACTIVE_PEERS, 10);
 			config.setInt(Keys.MAX_PEER_INACTIVITY_PERIOD, 60 * 60 * 24); // 24h
 			config.setInt(Keys.TCP_CONNECTION_IDLE_TIMEOUT, 60 * 60 * 12); // 12h
-			config.setInt(Keys.TCP_CONNECT_TIMEOUT, 20);
+			config.setInt(Keys.TCP_CONNECT_TIMEOUT, 5 * 1000); // 5s
 			config.setInt(Keys.TCP_WORKER_THREADS, 2);
 			config.setInt(Keys.NETWORK_STAGE_RECEIVER_THREAD_COUNT, 1);
 			config.setInt(Keys.NETWORK_STAGE_SENDER_THREAD_COUNT, 1);
