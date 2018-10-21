b4d7ed17923b680a33e3960049f548ccb0b0d411
Achim Kraus <achim.kraus@bosch-si.com>
2018-10-21 20:53:56 +0200

    Add javadoc.
    Signed-off-by: Achim Kraus <achim.kraus@bosch-si.com>

--- californium-core/src/main/java/org/eclipse/californium/core/network/EndpointReceiver.java
+++ californium-core/src/main/java/org/eclipse/californium/core/network/EndpointReceiver.java
@@ -22,15 +22,39 @@ import org.eclipse.californium.core.coap.Response;
 import org.eclipse.californium.core.network.Exchange;
 
 /**
- * Endpoint message receiver-
+ * Endpoint message receiver. Passes exchange and message to endpoints
+ * protocol-stack.
  */
 public interface EndpointReceiver {
 
+	/**
+	 * Process received request.
+	 * 
+	 * @param exchange exchange of request
+	 * @param request received request
+	 */
 	void receiveRequest(Exchange exchange, Request request);
 
+	/**
+	 * Process received response.
+	 * 
+	 * @param exchange exchange of response
+	 * @param response received response
+	 */
 	void receiveResponse(Exchange exchange, Response response);
 
+	/**
+	 * Process received empty message.
+	 * 
+	 * @param exchange exchange of empty message
+	 * @param message received empty message
+	 */
 	void receiveEmptyMessage(Exchange exchange, EmptyMessage message);
 
-	void reject(final Message message);
+	/**
+	 * Reject (received) message.
+	 * 
+	 * @param message received message to reject
+	 */
+	void reject(Message message);
 }
