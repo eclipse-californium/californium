34146e648b37a28e0f1748c44e00e5b5cc16ce4d
Andrei-Marius Longhin <andrei.longhin1@gmail.com>
2019-02-18 10:07:06 +0000

    Expose special methods of multicast socket in UDP connector and add note for when we might want to call them
    Signed-off-by: Andrei-Marius Longhin <andrei.longhin1@gmail.com>

--- element-connector/src/main/java/org/eclipse/californium/elements/UdpMulticastConnector.java
+++ element-connector/src/main/java/org/eclipse/californium/elements/UdpMulticastConnector.java
@@ -19,6 +19,7 @@ import java.io.IOException;
 import java.net.InetAddress;
 import java.net.InetSocketAddress;
 import java.net.MulticastSocket;
+import java.net.SocketException;
 
 /**
  * {@link Connector} which inherits functionality of {@link UDPConnector}
@@ -29,34 +30,66 @@ import java.net.MulticastSocket;
 public class UdpMulticastConnector extends UDPConnector {
 
 	/**
+	 * Multicast socket used by the connector.
+	 */
+	private MulticastSocket socket;
+
+	/**
 	 * Multicast groups to join.
 	 */
 	private InetAddress[] multicastGroups;
 
 	/**
 	 * Creates a connector bound to given multicast group and IP Port
-	 * 
-	 * @param socketAddress local socket address
+	 *
+	 * @param localAddress local socket address
 	 * @param multicastGroups multicast groups to join
+	 * @throws IOException if an I/O exception occurs while creating the MulticastSocket
 	 */
-	public UdpMulticastConnector(InetSocketAddress localAddress, InetAddress... multicastGroups) {
+	public UdpMulticastConnector(InetSocketAddress localAddress, InetAddress... multicastGroups) throws IOException {
 		super(localAddress);
+		this.socket = new MulticastSocket(localAddress);
 		this.multicastGroups = multicastGroups;
 	}
 
+	/**
+	 * Start the connector
+	 *
+	 * Note: You may need to set the interface used for multicast manually before calling this
+	 * (see https://github.com/eclipse/californium/issues/872).
+	 *
+	 * @throws IOException if an I/O exception occurs while joining the multicast group or while
+	 * initialising the UDP connector with the multicast socket
+	 */
 	public synchronized void start() throws IOException {
 		if (this.running)
 			return;
 
-		// creates a multicast socket with the given port number
-		MulticastSocket socket = new MulticastSocket(localAddr);
-
 		// add the multicast socket to the specified multicast group for
 		// listening to multicast requests
 		for (InetAddress group : multicastGroups) {
-			socket.joinGroup(group);
+			this.socket.joinGroup(group);
 		}
-		init(socket);
+		init(this.socket);
 	}
 
+	/**
+	 * Retrieve the address of the network interface used for multicast packets.
+	 *
+	 * @return the address of the network interface used for multicast as an InetAddress object
+	 * @throws SocketException if the socket is already closed
+	 */
+	public InetAddress getMulticastInterface() throws SocketException {
+		return this.socket.getInterface();
+	}
+
+	/**
+	 * Update the address of the network interface used for multicast packets.
+	 *
+	 * @param inf the address of the new network interface as a InetAddress object
+	 * @throws SocketException if the socket is already closed
+	 */
+	public void setMulticastInterface(InetAddress inf) throws SocketException {
+		this.socket.setInterface(inf);
+	}
 }
