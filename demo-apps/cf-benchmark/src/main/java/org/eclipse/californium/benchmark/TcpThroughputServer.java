f9f6ee4cd177f28424811670bc0f8d34784ead10
Achim Kraus <achim.kraus@bosch-si.com>
2019-07-22 13:41:03 +0200

    Add license header.
    Signed-off-by: Achim Kraus <achim.kraus@bosch-si.com>

--- demo-apps/cf-benchmark/src/main/java/org/eclipse/californium/benchmark/TcpThroughputServer.java
+++ demo-apps/cf-benchmark/src/main/java/org/eclipse/californium/benchmark/TcpThroughputServer.java
@@ -1,3 +1,20 @@
+/*******************************************************************************
+ * Copyright (c) 2016, 2017 Amazon Web Services and others.
+ * <p>
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * and Eclipse Distribution License v1.0 which accompany this distribution.
+ * <p>
+ * The Eclipse Public License is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ * and the Eclipse Distribution License is available at
+ * http://www.eclipse.org/org/documents/edl-v10.html.
+ * <p>
+ * Contributors:
+ * Joe Magerramov (Amazon Web Services) - CoAP over TCP support.
+ * Achim Kraus (Bosch Software Innovations GmbH) - add NetworkConfig setup
+ ******************************************************************************/
+
 package org.eclipse.californium.benchmark;
 
 import org.eclipse.californium.core.CoapResource;
