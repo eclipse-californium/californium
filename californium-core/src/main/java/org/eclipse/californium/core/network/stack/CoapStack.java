51f62bb693a852e1a420b6c607b8e8bf6aa59bbb
Achim Kraus <achim.kraus@bosch-si.com>
2019-11-09 08:47:56 +0100

    Add license header.
    Signed-off-by: Achim Kraus <achim.kraus@bosch-si.com>

--- californium-core/src/main/java/org/eclipse/californium/core/network/stack/CoapStack.java
+++ californium-core/src/main/java/org/eclipse/californium/core/network/stack/CoapStack.java
@@ -1,3 +1,18 @@
+/*******************************************************************************
+ * Copyright (c) 2015, 2017 Institute for Pervasive Computing, ETH Zurich and others.
+ * 
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v2.0
+ * and Eclipse Distribution License v1.0 which accompany this distribution.
+ * 
+ * The Eclipse Public License is available at
+ *    http://www.eclipse.org/legal/epl-v20.html
+ * and the Eclipse Distribution License is available at
+ *    http://www.eclipse.org/org/documents/edl-v10.html.
+ * 
+ * Contributors:
+ *    refer to gitlog
+ ******************************************************************************/
 package org.eclipse.californium.core.network.stack;
 
 import org.eclipse.californium.core.coap.EmptyMessage;
