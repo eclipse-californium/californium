f26271cc7a8b6161920f0e337e5b56c223f00946
Achim Kraus <achim.kraus@bosch.io>
2020-04-20 20:30:12 +0200

    Add libcoap interoperability tests.
    Signed-off-by: Achim Kraus <achim.kraus@bosch.io>

---
+++ californium-interoperability-tests/src/test/java/org/eclipse/californium/interoperability/test/LibCoapClientInteroperabilityTest.java
@@ -0,0 +1,195 @@
+/*******************************************************************************
+ * Copyright (c) 2020 Bosch.IO GmbH and others.
+ * 
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v2.0
+ * and Eclipse Distribution License v1.0 which accompany this distribution.
+ * 
+ * The Eclipse Public License is available at
+ *    http://www.eclipse.org/legal/epl-v20.html
+ * and the Eclipse Distribution License is available at
+ *    http://www.eclipse.org/org/documents/edl-v10.html.
+ * 
+ * Contributors:
+ *    Achim Kraus (Bosch.IO GmbH) - initial implementation.
+ ******************************************************************************/
+package org.eclipse.californium.interoperability.test;
+
+import static org.junit.Assert.assertThat;
+import static org.junit.Assert.assertTrue;
+import static org.junit.Assume.assumeNotNull;
+import static org.junit.Assume.assumeTrue;
+import static org.hamcrest.CoreMatchers.*;
+import static org.hamcrest.number.OrderingComparison.greaterThan;
+
+import java.io.IOException;
+import java.net.InetAddress;
+import java.net.InetSocketAddress;
+
+import org.eclipse.californium.elements.rule.TestNameLoggerRule;
+import org.eclipse.californium.interoperability.test.OpenSslUtil.AuthenticationMode;
+import org.eclipse.californium.interoperability.test.ProcessUtil.ProcessResult;
+import org.eclipse.californium.scandium.dtls.cipher.CipherSuite;
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
+import org.junit.Rule;
+import org.junit.Test;
+
+/**
+ * Test for interoperability with libcoap client.
+ * 
+ * @see LibCoapProcessUtil
+ */
+public class LibCoapClientInteroperabilityTest {
+
+	@Rule
+	public TestNameLoggerRule name = new TestNameLoggerRule();
+
+	private static final InetSocketAddress BIND = new InetSocketAddress(InetAddress.getLoopbackAddress(),
+			ScandiumUtil.PORT);
+	private static final String DESTINATION = "127.0.0.1:" + ScandiumUtil.PORT;
+	private static final String DESTINATION_URL = "coaps://" + DESTINATION + "/";
+	private static final long TIMEOUT_MILLIS = 2000;
+
+	private static LibCoapProcessUtil processUtil;
+	private static CaliforniumUtil californiumUtil;
+
+	@BeforeClass
+	public static void init() throws IOException, InterruptedException {
+		processUtil = new LibCoapProcessUtil();
+		ProcessResult result = processUtil.getLibCoapVersion(TIMEOUT_MILLIS);
+		assumeNotNull(result);
+		assumeTrue(result.contains("coap-client v4\\.2\\.1 "));
+		assumeTrue(result.contains("OpenSSL - runtime 1\\.1\\.1,"));
+		californiumUtil = new CaliforniumUtil(false);
+	}
+
+	@AfterClass
+	public static void shutdown() throws InterruptedException {
+		if (californiumUtil != null) {
+			californiumUtil.shutdown();
+			californiumUtil = null;
+		}
+		if (processUtil != null) {
+			processUtil.shutdown();
+		}
+	}
+
+	@After
+	public void stop() throws InterruptedException {
+		if (californiumUtil != null) {
+			californiumUtil.shutdown();
+		}
+		processUtil.shutdown();
+		processUtil.setVerboseLevel(null);
+	}
+
+	@Test
+	public void testLibCoapClientPsk() throws Exception {
+		CipherSuite cipherSuite = CipherSuite.TLS_PSK_WITH_AES_128_CCM_8;
+		californiumUtil.start(BIND, null, cipherSuite);
+
+		processUtil.startupClient(DESTINATION_URL + "test", AuthenticationMode.PSK, "Hello, CoAP!",
+				cipherSuite);
+		connect("Hello, CoAP!", "Greetings!");
+	}
+
+	@Test
+	public void testLibCoapClientPsk1k() throws Exception {
+		CipherSuite cipherSuite = CipherSuite.TLS_PSK_WITH_AES_128_CCM_8;
+		californiumUtil.start(BIND, null, cipherSuite);
+
+		processUtil.startupClient(DESTINATION_URL + "large", AuthenticationMode.PSK, "Hello, CoAP!",
+				cipherSuite);
+		ProcessResult result = connect("Hello, CoAP!",
+				"###############################################################");
+		assertThat(result, is(notNullValue()));
+		assertThat(result.console, is(notNullValue()));
+		assertThat(result.console.length(), is(greaterThan(1024)));
+	}
+
+	@Test
+	public void testLibCoapClientPsk4k() throws Exception {
+		CipherSuite cipherSuite = CipherSuite.TLS_PSK_WITH_AES_128_CCM_8;
+		californiumUtil.start(BIND, null, cipherSuite);
+
+		processUtil.startupClient(DESTINATION_URL + "large?size=4096", AuthenticationMode.PSK, "Hello, CoAP!",
+				cipherSuite);
+		ProcessResult result = connect("Hello, CoAP!",
+				"###############################################################");
+		assertThat(result, is(notNullValue()));
+		assertThat(result.console, is(notNullValue()));
+		assertThat(result.console.length(), is(greaterThan(4096)));
+	}
+
+	@Test
+	public void testLibCoapClientEcdsa() throws Exception {
+		CipherSuite cipherSuite = CipherSuite.TLS_ECDHE_ECDSA_WITH_AES_128_CCM_8;
+		californiumUtil.start(BIND, null, cipherSuite);
+
+		processUtil.startupClient(DESTINATION_URL + "test", AuthenticationMode.CERTIFICATE, "Hello, CoAP!",
+				cipherSuite);
+		connect("Hello, CoAP!", "Greetings!");
+	}
+
+	@Test
+	public void testLibCoapClientEcdsaRsa() throws Exception {
+		CipherSuite cipherSuite = CipherSuite.TLS_ECDHE_ECDSA_WITH_AES_128_CCM_8;
+		californiumUtil.start(BIND, true, null, ScandiumUtil.TRUST_ROOT, cipherSuite);
+
+		processUtil.startupClient(DESTINATION_URL + "test", AuthenticationMode.CERTIFICATE, "Hello, CoAP!",
+				cipherSuite);
+		connect("Hello, CoAP!", "Greetings!");
+	}
+
+	@Test
+	public void testLibCoapClientEcdsaTrust() throws Exception {
+		CipherSuite cipherSuite = CipherSuite.TLS_ECDHE_ECDSA_WITH_AES_128_CCM_8;
+		californiumUtil.start(BIND, null, cipherSuite);
+
+		processUtil.startupClient(DESTINATION_URL + "test", AuthenticationMode.TRUST, "Hello, CoAP!", cipherSuite);
+		connect("Hello, CoAP!", "Greetings!");
+	}
+
+	@Test
+	public void testLibCoapClientEcdsaRsaTrust() throws Exception {
+		CipherSuite cipherSuite = CipherSuite.TLS_ECDHE_ECDSA_WITH_AES_128_CCM_8;
+		californiumUtil.start(BIND, true, null, ScandiumUtil.TRUST_ROOT, cipherSuite);
+
+		processUtil.startupClient(DESTINATION_URL + "test", AuthenticationMode.TRUST, "Hello, CoAP!", cipherSuite);
+		connect("Hello, CoAP!", "Greetings!");
+	}
+
+	@Test
+	public void testLibCoapClientCustomOptions() throws Exception {
+		processUtil.setVerboseLevel("6");
+		CipherSuite cipherSuite = CipherSuite.TLS_PSK_WITH_AES_128_CCM_8;
+		californiumUtil.start(BIND, null, cipherSuite);
+
+		processUtil.startupClient(DESTINATION_URL + "custom", AuthenticationMode.PSK, "Hello, CoAP!",
+				cipherSuite);
+		connect("Hello, CoAP!", "Content-Format:65525", "65022:\\\\x74\\\\x65\\\\x73\\\\x74", "Custom Greetings!");
+	}
+
+	@Test
+	public void testLibCoapClientLocation() throws Exception {
+		processUtil.setVerboseLevel("6");
+		CipherSuite cipherSuite = CipherSuite.TLS_PSK_WITH_AES_128_CCM_8;
+		californiumUtil.start(BIND, null, cipherSuite);
+
+		processUtil.startupClient(DESTINATION_URL + "event", AuthenticationMode.PSK, "Hello, CoAP!",
+				cipherSuite);
+		connect("Hello, CoAP!", "Response!", "Location-Path:command", "Location-Path:1234-abcde", "Location-Query:hono-command=blink");
+	}
+
+	public ProcessResult connect(String sendMessage, String... patterns) throws Exception {
+		if (patterns != null) {
+			for (String check : patterns) {
+				assertTrue("missing " + check, processUtil.waitConsole(check, TIMEOUT_MILLIS));
+			}
+		}
+		californiumUtil.assertReceivedData(sendMessage, TIMEOUT_MILLIS);
+		return processUtil.stop(TIMEOUT_MILLIS);
+	}
+}
--- californium-interoperability-tests/src/test/java/org/eclipse/californium/interoperability/test/ScandiumUtil.java
+++ californium-interoperability-tests/src/test/java/org/eclipse/californium/interoperability/test/ScandiumUtil.java
@@ -24,48 +24,25 @@ import static org.junit.Assert.fail;
 
 import java.io.IOException;
 import java.net.InetSocketAddress;
-import java.security.GeneralSecurityException;
-import java.security.cert.Certificate;
-import java.util.Arrays;
-import java.util.List;
 import java.util.concurrent.TimeUnit;
 
 import org.eclipse.californium.elements.AddressEndpointContext;
+import org.eclipse.californium.elements.Connector;
 import org.eclipse.californium.elements.EndpointContext;
 import org.eclipse.californium.elements.RawData;
 import org.eclipse.californium.elements.util.SimpleMessageCallback;
 import org.eclipse.californium.elements.util.SimpleRawDataChannel;
-import org.eclipse.californium.elements.util.SslContextUtil;
-import org.eclipse.californium.elements.util.SslContextUtil.Credentials;
 import org.eclipse.californium.scandium.DTLSConnector;
 import org.eclipse.californium.scandium.config.DtlsConnectorConfig;
-import org.eclipse.californium.scandium.dtls.CertificateType;
 import org.eclipse.californium.scandium.dtls.cipher.CipherSuite;
-import org.eclipse.californium.scandium.dtls.pskstore.StaticPskStore;
 
 /**
  * Scandium utility.
  * 
  * Configure and starts {@link DTLSConnector}.
  */
-public class ScandiumUtil {
+public class ScandiumUtil extends ConnectorUtil {
 
-	public static final int PORT = 5684;
-
-	private static final char[] KEY_STORE_PASSWORD = "endPass".toCharArray();
-	private static final String KEY_STORE_LOCATION = "certs/keyStore.jks";
-	private static final char[] TRUST_STORE_PASSWORD = "rootPass".toCharArray();
-	private static final String TRUST_STORE_LOCATION = "certs/trustStore.jks";
-	private static final String CLIENT_NAME = "client";
-	private static final String SERVER_NAME = "server";
-	private static final String SERVER_RSA_NAME = "serverrsa";
-	public static final String TRUST_CA = "ca";
-	public static final String TRUST_ROOT = "root";
-
-	/**
-	 * DTLS connector.
-	 */
-	private DTLSConnector connector;
 	/**
 	 * Raw data channel to receive data.
 	 */
@@ -74,51 +51,23 @@ public class ScandiumUtil {
 	 * Last received data.
 	 */
 	private RawData receivedData;
-	/**
-	 * Credentials for ECDSA base cipher suites.
-	 */
-	private SslContextUtil.Credentials credentials;
-	/**
-	 * Credentials for ECDSA base cipher suites with RSA chain.
-	 */
-	private SslContextUtil.Credentials credentialsRsa;
-	private Certificate[] trustCa;
-	private Certificate[] trustRoot;
-	private Certificate[] trustAll;
 
 	/**
 	 * Create new utility instance.
 	 * 
 	 * @param client {@code true} to use client credentials, {@code false}, for
-	 *               server credentials.
+	 *            server credentials.
 	 */
 	public ScandiumUtil(boolean client) {
-		try {
-			credentials = SslContextUtil.loadCredentials(SslContextUtil.CLASSPATH_SCHEME + KEY_STORE_LOCATION,
-					client ? CLIENT_NAME : SERVER_NAME, KEY_STORE_PASSWORD, KEY_STORE_PASSWORD);
-			credentialsRsa = client ? null
-					: SslContextUtil.loadCredentials(SslContextUtil.CLASSPATH_SCHEME + KEY_STORE_LOCATION,
-							SERVER_RSA_NAME, KEY_STORE_PASSWORD, KEY_STORE_PASSWORD);
-			trustCa = SslContextUtil.loadTrustedCertificates(SslContextUtil.CLASSPATH_SCHEME + TRUST_STORE_LOCATION,
-					TRUST_CA, TRUST_STORE_PASSWORD);
-			trustRoot = SslContextUtil.loadTrustedCertificates(SslContextUtil.CLASSPATH_SCHEME + TRUST_STORE_LOCATION,
-					TRUST_ROOT, TRUST_STORE_PASSWORD);
-			trustAll = new Certificate[0];
-		} catch (GeneralSecurityException e) {
-			e.printStackTrace();
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
+		super(client);
 	}
 
 	/**
 	 * Shutdown the connector.
 	 */
+	@Override
 	public void shutdown() {
-		if (connector != null) {
-			connector.destroy();
-			connector = null;
-		}
+		super.shutdown();
 		channel = null;
 		receivedData = null;
 	}
@@ -126,59 +75,39 @@ public class ScandiumUtil {
 	/**
 	 * Start connector.
 	 * 
-	 * @param bind         address to bind connector to
-	 * @param trust        alias of trusted certificate, or {@code null} to trust
-	 *                     all received certificates.
+	 * @param bind address to bind connector to
+	 * @param trust alias of trusted certificate, or {@code null} to trust all
+	 *            received certificates.
 	 * @param cipherSuites cipher suites to support.
 	 * @throws IOException if an error occurred starting the connector on the
-	 *                     provided bind address
+	 *             provided bind address
 	 */
 	public void start(InetSocketAddress bind, String trust, CipherSuite... cipherSuites) throws IOException {
-		start(bind, false, null, trust, cipherSuites);
+		build(bind, trust, cipherSuites);
+		start();
 	}
 
 	/**
 	 * Start connector.
 	 * 
-	 * @param bind         address to bind connector to
-	 * @param rsa          use mixed certifcate path (includes RSA certificate). Server only!
-	 * @param dtlsBuilder  preconfigured dtls builder. Maybe {@link null}.
-	 * @param trust        alias of trusted certificate, or {@code null} to trust
-	 *                     all received certificates.
+	 * @param bind address to bind connector to
+	 * @param rsa use mixed certifcate path (includes RSA certificate). Server
+	 *            only!
+	 * @param dtlsBuilder preconfigured dtls builder. Maybe {@link null}.
+	 * @param trust alias of trusted certificate, or {@code null} to trust all
+	 *            received certificates.
 	 * @param cipherSuites cipher suites to support.
 	 * @throws IOException if an error occurred starting the connector on the
-	 *                     provided bind address
+	 *             provided bind address
 	 */
 	public void start(InetSocketAddress bind, boolean rsa, DtlsConnectorConfig.Builder dtlsBuilder, String trust,
 			CipherSuite... cipherSuites) throws IOException {
-		List<CipherSuite> suites = Arrays.asList(cipherSuites);
-		if (dtlsBuilder == null) {
-			dtlsBuilder = new DtlsConnectorConfig.Builder();
-		}
-		dtlsBuilder.setAddress(bind);
-		dtlsBuilder.setRecommendedCipherSuitesOnly(false);
-		dtlsBuilder.setConnectionThreadCount(2);
-		dtlsBuilder.setReceiverThreadCount(2);
-		if (CipherSuite.containsPskBasedCipherSuite(suites)) {
-			dtlsBuilder.setPskStore(
-					new StaticPskStore(OpenSslUtil.OPENSSL_PSK_IDENTITY, OpenSslUtil.OPENSSL_PSK_SECRET));
-		}
-		if (CipherSuite.containsCipherSuiteRequiringCertExchange(suites)) {
-			if (credentials != null && dtlsBuilder.getIncompleteConfig().getPrivateKey() == null) {
-				Credentials credentials = rsa ? this.credentialsRsa : this.credentials;
-				dtlsBuilder.setIdentity(credentials.getPrivateKey(), credentials.getCertificateChain(),
-						CertificateType.X_509, CertificateType.RAW_PUBLIC_KEY);
-				if (TRUST_CA.equals(trust)) {
-					dtlsBuilder.setTrustStore(trustCa);
-				} else if (TRUST_ROOT.equals(trust)) {
-					dtlsBuilder.setTrustStore(trustRoot);
-				} else {
-					dtlsBuilder.setTrustStore(trustAll);
-				}
-			}
-		}
-		dtlsBuilder.setSupportedCipherSuites(suites);
-		connector = new DTLSConnector(dtlsBuilder.build());
+		build(bind, rsa, dtlsBuilder, trust, cipherSuites);
+		start();
+	}
+
+	private void start() throws IOException {
+		Connector connector = getConnector();
 		channel = new SimpleRawDataChannel(1);
 		connector.setRawDataReceiver(channel);
 		connector.start();
@@ -190,14 +119,14 @@ public class ScandiumUtil {
 	 * @param data data to send
 	 */
 	public void send(RawData data) {
-		connector.send(data);
+		getConnector().send(data);
 	}
 
 	/**
 	 * Wait for received data.
 	 * 
 	 * @param timeout timeout to wait for
-	 * @param unit    time unit of timeout
+	 * @param unit time unit of timeout
 	 * @return received data, or {@code null}, if no data is received in time.
 	 * @throws InterruptedException if interrupted during wait
 	 */
@@ -210,7 +139,7 @@ public class ScandiumUtil {
 	 * 
 	 * The message must be first received one.
 	 * 
-	 * @param message       message the receiving is to be asserted
+	 * @param message message the receiving is to be asserted
 	 * @param timeoutMillis timeout of message
 	 * @throws InterruptedException if interrupted during wait
 	 */
@@ -223,7 +152,7 @@ public class ScandiumUtil {
 	/**
 	 * Send message as response to the last received message.
 	 * 
-	 * @param message       message to send
+	 * @param message message to send
 	 * @param timeoutMillis timeout in millisecond.
 	 * @see #receivedData
 	 */
@@ -234,14 +163,14 @@ public class ScandiumUtil {
 	/**
 	 * Send message to destination.
 	 * 
-	 * @param message       message to send
-	 * @param destination   destination address
+	 * @param message message to send
+	 * @param destination destination address
 	 * @param timeoutMillis timeout in milliseconds
 	 */
 	public void send(String message, InetSocketAddress destination, long timeoutMillis) {
 		SimpleMessageCallback callback = new SimpleMessageCallback(1, false);
 		RawData raw = RawData.outbound(message.getBytes(), new AddressEndpointContext(destination), callback, false);
-		connector.send(raw);
+		send(raw);
 		try {
 			boolean sent = callback.isSent(timeoutMillis);
 			EndpointContext context = callback.getEndpointContext();
