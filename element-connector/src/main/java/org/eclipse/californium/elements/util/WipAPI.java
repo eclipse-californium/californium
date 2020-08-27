b683e67690fc3afd673394d83e3a679e4de5a96f
Achim Kraus <achim.kraus@bosch.io>
2020-08-27 09:15:42 +0200

    Add "work in progress" annotation to mark new APIs.
    Signed-off-by: Achim Kraus <achim.kraus@bosch.io>

---
+++ element-connector/src/main/java/org/eclipse/californium/elements/util/WipAPI.java
@@ -0,0 +1,26 @@
+/*******************************************************************************
+ * Copyright (c) 2020 Bosch IO GmbH and others.
+ * 
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v2.0
+ * and Eclipse Distribution License v1.0 which accompany this distribution.
+ * 
+ * The Eclipse Public License is available at
+ *    http://www.eclipse.org/legal/epl-v20.html
+ * and the Eclipse Distribution License is available at
+ *    http://www.eclipse.org/org/documents/edl-v10.html.
+ * 
+ * Contributors:
+ *    Bosch IO GmbH - initial creation
+ ******************************************************************************/
+package org.eclipse.californium.elements.util;
+
+/**
+ * Methods or classes marked by this annotation are "work in progress". They may
+ * change without major version change!
+ * 
+ * @since 2.5
+ */
+public @interface WipAPI {
+
+}
