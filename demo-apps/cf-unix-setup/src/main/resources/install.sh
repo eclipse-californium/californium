53333c17c0836b90fb0f02e2109ad1dcf3564072
Achim Kraus <achim.kraus@bosch.io>
2020-08-27 08:25:51 +0200

    Update to 2.5.0-SNAPSHOT and 2.4.0.
    Signed-off-by: Achim Kraus <achim.kraus@bosch.io>

--- demo-apps/cf-unix-setup/src/main/resources/install.sh
+++ demo-apps/cf-unix-setup/src/main/resources/install.sh
@@ -24,7 +24,7 @@
 # create non-su, non-login user
 sudo adduser --system --home /home/cali --disabled-login cali
 # move application from current folder
-sudo mv cf-plugtest-server-2.3.0.jar /home/cali/
+sudo mv cf-plugtest-server-2.4.0.jar /home/cali/
 # move service definition from current folder
 sudo mv cali.service /etc/systemd/system
 # reload service definitions
