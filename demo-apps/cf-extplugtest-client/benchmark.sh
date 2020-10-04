b984031e752844fa32fcd63030b6326d040e41cc
Achim Kraus <achim.kraus@bosch.io>
2020-10-04 07:56:36 +0200

    Fix shebang in shell scripts.
    Signed-off-by: Achim Kraus <achim.kraus@bosch.io>

--- demo-apps/cf-extplugtest-client/benchmark.sh
+++ demo-apps/cf-extplugtest-client/benchmark.sh
@@ -1,4 +1,21 @@
-# !/bin/sh
+#!/bin/sh
+
+#/*******************************************************************************
+# * Copyright (c) 2020 Bosch.IO GmbH and others.
+# * 
+# * All rights reserved. This program and the accompanying materials
+# * are made available under the terms of the Eclipse Public License v2.0
+# * and Eclipse Distribution License v1.0 which accompany this distribution.
+# * 
+# * The Eclipse Public License is available at
+# *    http://www.eclipse.org/legal/epl-v20.html
+# * and the Eclipse Distribution License is available at
+# *    http://www.eclipse.org/org/documents/edl-v10.html.
+# * 
+# * Contributors:
+# *    Achim Kraus (Bosch.IO GmbH) - initial script
+# ******************************************************************************/
+
 # create benchmark statistic
 
 echo "Californium CoAP Benchmark"
