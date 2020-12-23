bc4ec3eceb33fabe4d2a7f494ccc24bfd6e8904c
Achim Kraus <achim.kraus@bosch.io>
2020-12-23 16:21:33 +0100

    Update tests in integration-tests for junit 4.13.1.
    Signed-off-by: Achim Kraus <achim.kraus@bosch.io>

--- californium-tests/californium-integration-tests/src/test/java/org/eclipse/californium/integration/test/SecureBlockwiseTest.java
+++ californium-tests/californium-integration-tests/src/test/java/org/eclipse/californium/integration/test/SecureBlockwiseTest.java
@@ -17,7 +17,7 @@ package org.eclipse.californium.integration.test;
 
 import static org.hamcrest.CoreMatchers.is;
 import static org.hamcrest.CoreMatchers.notNullValue;
-import static org.junit.Assert.assertThat;
+import static org.hamcrest.MatcherAssert.assertThat;
 
 import java.util.Random;
 import java.util.concurrent.atomic.AtomicInteger;
