182849c445451ec051a163d4593ac5dbe95bfac2
Achim Kraus <achim.kraus@bosch.io>
2020-12-23 16:24:01 +0100

    Update tests in interoperability-tests for junit 4.13.1.
    Signed-off-by: Achim Kraus <achim.kraus@bosch.io>

--- californium-tests/californium-interoperability-tests/src/test/java/org/eclipse/californium/interoperability/test/CaliforniumUtil.java
+++ californium-tests/californium-interoperability-tests/src/test/java/org/eclipse/californium/interoperability/test/CaliforniumUtil.java
@@ -17,8 +17,8 @@ package org.eclipse.californium.interoperability.test;
 
 import static org.hamcrest.CoreMatchers.instanceOf;
 import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.MatcherAssert.assertThat;
 import static org.junit.Assert.assertNotNull;
-import static org.junit.Assert.assertThat;
 
 import java.io.IOException;
 import java.net.InetSocketAddress;
--- californium-tests/californium-interoperability-tests/src/test/java/org/eclipse/californium/interoperability/test/ScandiumUtil.java
+++ californium-tests/californium-interoperability-tests/src/test/java/org/eclipse/californium/interoperability/test/ScandiumUtil.java
@@ -16,9 +16,9 @@
 package org.eclipse.californium.interoperability.test;
 
 import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.MatcherAssert.assertThat;
 import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertNull;
-import static org.junit.Assert.assertThat;
 import static org.junit.Assert.assertTrue;
 import static org.junit.Assert.fail;
 
