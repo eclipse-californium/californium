cff04806e9d207e1ea071af74162b5e9d1841dbf
Achim Kraus <achim.kraus@bosch.io>
2021-10-25 18:49:26 +0200

    Sign the eddsa server certificate with eddsa.
    Signed-off-by: Achim Kraus <achim.kraus@bosch.io>

--- demo-certs/src/main/resources/create-keystores.sh
+++ demo-certs/src/main/resources/create-keystores.sh
@@ -43,6 +43,7 @@ TRUST_STORE_PWD=rootPass
 
 # to prevent java 11 from failing to open the other jks
 EDDSA_KEY_STORE=eddsaKeyStore.jks
+EDDSA_TRUST_STORE=eddsaTrustStore.jks
 
 DEFAULT_STORE_TYPE=JKS
 
@@ -51,6 +52,7 @@ ROOT_CER=root.cer
 CA_CER=ca.cer
 CA2_CER=ca2.cer
 CA_RSA_CER=caRsa.cer
+CA_EDDSA_CER=caEdDsa.cer
 SERVER_CER=server.cer
 SERVER_LARGE_CER=serverLarge.cer
 SERVER_RSA_CER=serverRsa.cer
@@ -63,8 +65,10 @@ CLIENT_RSA_CER=clientRsa.cer
 
 # android support - PKCS12
 TRUST_STORE_P12=trustStore.p12
+EDDSA_TRUST_STORE_P12=eddsaTrustStore.p12
 CA_TRUST_STORE_P12=caTrustStore.p12
 CA_RSA_TRUST_STORE_P12=caRsaTrustStore.p12
+CA_EDDSA_TRUST_STORE_P12=caEdDsaTrustStore.p12
 CLIENT_KEY_STORE_P12=client.p12
 CLIENT_EDDSA_KEY_STORE_P12=clientEdDsa.p12
 CLIENT_RSA_KEY_STORE_P12=clientRsa.p12
@@ -77,9 +81,11 @@ ROOT_TRUST_STORE_P12=rootTrustStore.p12
 
 # PEM 
 TRUST_STORE_PEM=trustStore.pem
+EDDSA_TRUST_STORE_PEM=eddsaTrustStore.pem
 ROOT_TRUST_STORE_PEM=rootTrustStore.pem
 CA_TRUST_STORE_PEM=caTrustStore.pem
 CA_RSA_TRUST_STORE_PEM=caRsaTrustStore.pem
+CA_EDDSA_TRUST_STORE_PEM=caEdDsaTrustStore.pem
 CLIENT_KEY_STORE_PEM=client.pem
 CLIENT_EDDSA_KEY_STORE_PEM=clientEdDsa.pem
 CLIENT_RSA_KEY_STORE_PEM=clientRsa.pem
@@ -107,10 +113,10 @@ TRUST_STORE_DER=trustStore.der
 VALIDITY=365
 
 remove_keys() {
-	rm -f $KEY_STORE $TRUST_STORE $EDDSA_KEY_STORE
-	rm -f $ROOT_CER $CA_CER $CA2_CER $CA_RSA_CER $SERVER_CER $SERVER_LARGE_CER $SERVER_RSA_CER $SERVER_CA_RSA_CER $CLIENT_CER $CLIENT_EXT_CER $CLIENT_EDDSA_CER $CLIENT_RSA_CER $SERVER_EDDSA_CER
-	rm -f $CLIENT_KEY_STORE_P12 $SERVER_KEY_STORE_P12 $SERVER_LARGE_KEY_STORE_P12 $SERVER_RSA_KEY_STORE_P12 $SERVER_CA_RSA_KEY_STORE_P12 $SERVER_RSA_KEY_STORE_P12 $TRUST_STORE_P12 $CA_TRUST_STORE_P12 $CA_RSA_TRUST_STORE_P12 $CLIENT_EDDSA_KEY_STORE_P12 $CLIENT_RSA_KEY_STORE_P12 $ROOT_TRUST_STORE_P12 $SERVER_EDDSA_KEY_STORE_P12
-	rm -f $CLIENT_KEY_STORE_PEM $SERVER_KEY_STORE_PEM $SERVER_LARGE_KEY_STORE_PEM $SERVER_RSA_KEY_STORE_PEM $SERVER_CA_RSA_KEY_STORE_PEM $TRUST_STORE_PEM $CA_TRUST_STORE_PEM $CA_RSA_TRUST_STORE_PEM $CLIENT_EDDSA_KEY_STORE_PEM $CLIENT_RSA_KEY_STORE_PEM $ROOT_TRUST_STORE_PEM $SERVER_EDDSA_KEY_STORE_PEM
+	rm -f $KEY_STORE $TRUST_STORE $EDDSA_KEY_STORE $EDDSA_TRUST_STORE
+	rm -f $ROOT_CER $CA_CER $CA2_CER $CA_RSA_CER $SERVER_CER $SERVER_LARGE_CER $SERVER_RSA_CER $SERVER_CA_RSA_CER $CLIENT_CER $CLIENT_EXT_CER $CLIENT_EDDSA_CER $CLIENT_RSA_CER $SERVER_EDDSA_CER $CA_EDDSA_CER
+	rm -f $CLIENT_KEY_STORE_P12 $SERVER_KEY_STORE_P12 $SERVER_LARGE_KEY_STORE_P12 $SERVER_RSA_KEY_STORE_P12 $SERVER_CA_RSA_KEY_STORE_P12 $SERVER_RSA_KEY_STORE_P12 $TRUST_STORE_P12 $CA_TRUST_STORE_P12 $CA_RSA_TRUST_STORE_P12 $CLIENT_EDDSA_KEY_STORE_P12 $CLIENT_RSA_KEY_STORE_P12 $ROOT_TRUST_STORE_P12 $SERVER_EDDSA_KEY_STORE_P12 $CA_EDDSA_TRUST_STORE_P12 $EDDSA_TRUST_STORE_P12
+	rm -f $CLIENT_KEY_STORE_PEM $SERVER_KEY_STORE_PEM $SERVER_LARGE_KEY_STORE_PEM $SERVER_RSA_KEY_STORE_PEM $SERVER_CA_RSA_KEY_STORE_PEM $TRUST_STORE_PEM $CA_TRUST_STORE_PEM $CA_RSA_TRUST_STORE_PEM $CLIENT_EDDSA_KEY_STORE_PEM $CLIENT_RSA_KEY_STORE_PEM $ROOT_TRUST_STORE_PEM $SERVER_EDDSA_KEY_STORE_PEM $CA_EDDSA_TRUST_STORE_PEM
 	rm -f $EC_PUBLIC_KEY_PEM $EC_PRIVATE_KEY_PEM $ED25519_PUBLIC_KEY_PEM $ED25519_PRIVATE_KEY_PEM $ED448_PUBLIC_KEY_PEM $ED448_PRIVATE_KEY_PEM
 	rm -f $CLIENT_PRIVATE_KEY_PEM $CLIENT_RSA_PRIVATE_KEY_PEM $SERVER_PRIVATE_KEY_PEM $SERVER_RSA_PRIVATE_KEY_PEM $SERVER_CA_RSA_PRIVATE_KEY_PEM
 }
@@ -122,13 +128,15 @@ remove_interop_keys() {
   rm -f ${DESTINATION_DIR}/$ROOT_TRUST_STORE_PEM
   rm -f ${DESTINATION_DIR}/$CA_TRUST_STORE_PEM
   rm -f ${DESTINATION_DIR}/$CA_RSA_TRUST_STORE_PEM 
+  rm -f ${DESTINATION_DIR}/$CA_EDDSA_TRUST_STORE_PEM 
+  rm -f ${DESTINATION_DIR}/$EDDSA_TRUST_STORE_PEM 
   rm -f ${DESTINATION_DIR}/$CLIENT_KEY_STORE_PEM 
   rm -f ${DESTINATION_DIR}/$CLIENT_EDDSA_KEY_STORE_PEM
   rm -f ${DESTINATION_DIR}/$CLIENT_RSA_KEY_STORE_PEM
   rm -f ${DESTINATION_DIR}/$SERVER_KEY_STORE_PEM 
   rm -f ${DESTINATION_DIR}/$SERVER_LARGE_KEY_STORE_PEM 
   rm -f ${DESTINATION_DIR}/$SERVER_RSA_KEY_STORE_PEM 
-  rm -f ${DESTINATION_DIR}/$SERVER_CA_RSA_KEY_STORE_PEM 
+  rm -f ${DESTINATION_DIR}/$SERVER_CA_RSA_KEY_STORE_PEM
   rm -f ${DESTINATION_DIR}/$SERVER_EDDSA_KEY_STORE_PEM 
   rm -f ${DESTINATION_DIR}/$EC_PRIVATE_KEY_PEM
   # gnutls keys
@@ -221,14 +229,16 @@ create_keys() {
    keytool -genkeypair -alias client -keyalg EC -dname 'C=CA,L=Ottawa,O=Eclipse IoT,OU=Californium,CN=cf-client' \
         -validity $VALIDITY -keypass $KEY_STORE_PWD -keystore $KEY_STORE -storepass $KEY_STORE_PWD
    keytool -keystore $KEY_STORE -storepass $KEY_STORE_PWD -certreq -alias client | \
-      keytool -keystore $TRUST_STORE -storepass $TRUST_STORE_PWD -alias ca -gencert -ext KU=dig -validity $VALIDITY -sigalg SHA256withECDSA -rfc > $CLIENT_CER
+        keytool -keystore $TRUST_STORE -storepass $TRUST_STORE_PWD -alias ca -gencert -ext KU=dig \
+        -validity $VALIDITY -sigalg SHA256withECDSA -rfc > $CLIENT_CER
    keytool -alias client -importcert -keystore $KEY_STORE -storepass $KEY_STORE_PWD -trustcacerts -file $CLIENT_CER
 
    echo "creating client rsa-key and certificate..."
    keytool -genkeypair -alias clientrsa -keyalg RSA -dname 'C=CA,L=Ottawa,O=Eclipse IoT,OU=Californium,CN=cf-client-rsa' \
         -validity $VALIDITY -keypass $KEY_STORE_PWD -keystore $KEY_STORE -storepass $KEY_STORE_PWD
    keytool -keystore $KEY_STORE -storepass $KEY_STORE_PWD -certreq -alias clientrsa | \
-      keytool -keystore $TRUST_STORE -storepass $TRUST_STORE_PWD -alias ca -gencert -ext KU=dig -validity $VALIDITY -sigalg SHA256withECDSA -rfc > $CLIENT_RSA_CER
+        keytool -keystore $TRUST_STORE -storepass $TRUST_STORE_PWD -alias ca -gencert -ext KU=dig \
+       -validity $VALIDITY -sigalg SHA256withECDSA -rfc > $CLIENT_RSA_CER
    keytool -alias clientrsa -importcert -keystore $KEY_STORE -storepass $KEY_STORE_PWD -trustcacerts -file $CLIENT_RSA_CER
 
    echo "creating self-signed key and certificate..."
@@ -243,7 +253,8 @@ create_keys() {
    keytool -genkeypair -alias clientext -keyalg EC -dname 'C=CA,L=Ottawa,O=Eclipse IoT,OU=Californium,CN=cf-clientext' \
         -validity $VALIDITY -keypass $KEY_STORE_PWD -keystore $KEY_STORE -storepass $KEY_STORE_PWD
    keytool -keystore $KEY_STORE -storepass $KEY_STORE_PWD -certreq -alias clientext | \
-      keytool -keystore $TRUST_STORE -storepass $TRUST_STORE_PWD -alias ca -gencert -ext KU=dig -ext EKU=clientAuth -validity $VALIDITY -sigalg SHA256withECDSA -rfc > $CLIENT_EXT_CER
+        keytool -keystore $TRUST_STORE -storepass $TRUST_STORE_PWD -alias ca -gencert -ext KU=dig -ext EKU=clientAuth \
+        -validity $VALIDITY -sigalg SHA256withECDSA -rfc > $CLIENT_EXT_CER
    # the keytool supports two modes of certificates import
    # - import a certificate chain (as it is)
    # - import a certificate (and amend the chain from the CA certificates in the store)
@@ -266,13 +277,25 @@ create_keys() {
       keytool -alias clienteddsa -importcert -keystore $EDDSA_KEY_STORE -storepass $KEY_STORE_PWD -trustcacerts \
            -file $CLIENT_EDDSA_CER -storetype $DEFAULT_STORE_TYPE
 
+      echo "import other key and certificate into eddsa trust ..."
+      keytool -importkeystore -destkeystore $EDDSA_TRUST_STORE -deststorepass $TRUST_STORE_PWD \
+           -srckeystore $TRUST_STORE -srcstorepass $TRUST_STORE_PWD -storetype $DEFAULT_STORE_TYPE
+
+      echo "creating CA EdDsa key and certificate..."
+      keytool -genkeypair -alias caeddsa -keyalg Ed25519 -dname 'C=CA,L=Ottawa,O=Eclipse IoT,OU=Californium,CN=cf-ca-eddsa' \
+           -ext BC=1 -validity $VALIDITY -keypass $TRUST_STORE_PWD -keystore $EDDSA_TRUST_STORE -storepass $TRUST_STORE_PWD
+      keytool -keystore $EDDSA_TRUST_STORE -storepass $TRUST_STORE_PWD -certreq -alias caeddsa | \
+           keytool -keystore $EDDSA_TRUST_STORE -storepass $TRUST_STORE_PWD -alias root -gencert -validity $VALIDITY \
+           -ext BC=1 -ext KU=keyCertSign,cRLSign -sigalg SHA256withECDSA -rfc > $CA_EDDSA_CER
+      keytool -alias caeddsa -importcert -keystore $EDDSA_TRUST_STORE -storepass $TRUST_STORE_PWD -file $CA_EDDSA_CER
+
       echo "creating server eddsa key and certificate..."
       keytool -genkeypair -alias servereddsa -keyalg Ed25519 -dname 'C=CA,L=Ottawa,O=Eclipse IoT,OU=Californium,CN=cf-server-eddsa' \
            -validity $VALIDITY -keypass $KEY_STORE_PWD -keystore $EDDSA_KEY_STORE -storepass $KEY_STORE_PWD -storetype $DEFAULT_STORE_TYPE
       keytool -keystore $EDDSA_KEY_STORE -storepass $KEY_STORE_PWD -certreq -alias servereddsa | \
-           keytool -keystore $TRUST_STORE -storepass $TRUST_STORE_PWD -alias ca -gencert -ext KU=dig \
+           keytool -keystore $EDDSA_TRUST_STORE -storepass $TRUST_STORE_PWD -alias caeddsa -gencert -ext KU=dig \
            -ext 'san=dns:localhost,ip:127.0.0.1,ip:::1,dns:californium.eclipseprojects.io,ip:35.185.40.182' \
-           -validity $VALIDITY -sigalg SHA256withECDSA -rfc > $SERVER_EDDSA_CER
+           -validity $VALIDITY -sigalg Ed25519 -rfc > $SERVER_EDDSA_CER
       keytool -alias servereddsa -importcert -keystore $EDDSA_KEY_STORE -storepass $KEY_STORE_PWD -trustcacerts \
            -file $SERVER_EDDSA_CER -storetype $DEFAULT_STORE_TYPE
 
@@ -293,8 +316,6 @@ export_p12() {
    fi	
    keytool $LEGACY -v -importkeystore -srckeystore $KEY_STORE -srcstorepass $KEY_STORE_PWD -alias client \
       -destkeystore $CLIENT_KEY_STORE_P12 -deststorepass $KEY_STORE_PWD -deststoretype PKCS12
-   keytool $LEGACY -v -importkeystore -srckeystore $EDDSA_KEY_STORE -srcstorepass $KEY_STORE_PWD -alias clienteddsa \
-      -destkeystore $CLIENT_EDDSA_KEY_STORE_P12 -deststorepass $KEY_STORE_PWD -deststoretype PKCS12
    keytool $LEGACY -v -importkeystore -srckeystore $KEY_STORE -srcstorepass $KEY_STORE_PWD -alias clientrsa \
       -destkeystore $CLIENT_RSA_KEY_STORE_P12 -deststorepass $KEY_STORE_PWD -deststoretype PKCS12
    keytool $LEGACY -v -importkeystore -srckeystore $KEY_STORE -srcstorepass $KEY_STORE_PWD -alias server \
@@ -305,8 +326,6 @@ export_p12() {
       -destkeystore $SERVER_RSA_KEY_STORE_P12 -deststorepass $KEY_STORE_PWD -deststoretype PKCS12
    keytool $LEGACY -v -importkeystore -srckeystore $KEY_STORE -srcstorepass $KEY_STORE_PWD -alias servercarsa \
       -destkeystore $SERVER_CA_RSA_KEY_STORE_P12 -deststorepass $KEY_STORE_PWD -deststoretype PKCS12
-   keytool $LEGACY -v -importkeystore -srckeystore $EDDSA_KEY_STORE -srcstorepass $KEY_STORE_PWD -alias servereddsa \
-      -destkeystore $SERVER_EDDSA_KEY_STORE_P12 -deststorepass $KEY_STORE_PWD -deststoretype PKCS12
    keytool $LEGACY -v -importkeystore -srckeystore $TRUST_STORE -srcstorepass $TRUST_STORE_PWD -alias root \
       -destkeystore $ROOT_TRUST_STORE_P12 -deststorepass $TRUST_STORE_PWD -deststoretype PKCS12
    keytool $LEGACY -v -importkeystore -srckeystore $TRUST_STORE -srcstorepass $TRUST_STORE_PWD -alias ca \
@@ -315,6 +334,20 @@ export_p12() {
       -destkeystore $CA_RSA_TRUST_STORE_P12 -deststorepass $TRUST_STORE_PWD -deststoretype PKCS12
    keytool $LEGACY -v -importkeystore -srckeystore $TRUST_STORE -srcstorepass $TRUST_STORE_PWD \
       -destkeystore $TRUST_STORE_P12 -deststorepass $TRUST_STORE_PWD -deststoretype PKCS12 
+
+   if [ -s $EDDSA_KEY_STORE ] ; then 
+      keytool $LEGACY -v -importkeystore -srckeystore $EDDSA_KEY_STORE -srcstorepass $KEY_STORE_PWD -alias clienteddsa \
+         -destkeystore $CLIENT_EDDSA_KEY_STORE_P12 -deststorepass $KEY_STORE_PWD -deststoretype PKCS12
+      keytool $LEGACY -v -importkeystore -srckeystore $EDDSA_KEY_STORE -srcstorepass $KEY_STORE_PWD -alias servereddsa \
+         -destkeystore $SERVER_EDDSA_KEY_STORE_P12 -deststorepass $KEY_STORE_PWD -deststoretype PKCS12 
+   fi
+   if [ -s $EDDSA_TRUST_STORE ] ; then 
+      keytool $LEGACY -v -importkeystore -srckeystore $EDDSA_TRUST_STORE -srcstorepass $TRUST_STORE_PWD \
+         -destkeystore $EDDSA_TRUST_STORE_P12 -deststorepass $TRUST_STORE_PWD -deststoretype PKCS12 
+      keytool $LEGACY -v -importkeystore -srckeystore $EDDSA_TRUST_STORE -srcstorepass $TRUST_STORE_PWD -alias caeddsa \
+         -destkeystore $CA_EDDSA_TRUST_STORE_P12 -deststorepass $TRUST_STORE_PWD -deststoretype PKCS12
+   fi
+
 }
 
 export_pem() {
@@ -326,13 +359,20 @@ export_pem() {
       openssl pkcs12 -in $SERVER_LARGE_KEY_STORE_P12 -passin pass:$KEY_STORE_PWD -nodes -out $SERVER_LARGE_KEY_STORE_PEM
       openssl pkcs12 -in $SERVER_RSA_KEY_STORE_P12 -passin pass:$KEY_STORE_PWD -nodes -out $SERVER_RSA_KEY_STORE_PEM
       openssl pkcs12 -in $SERVER_CA_RSA_KEY_STORE_P12 -passin pass:$KEY_STORE_PWD -nodes -out $SERVER_CA_RSA_KEY_STORE_PEM
-      openssl pkcs12 -in $SERVER_EDDSA_KEY_STORE_P12 -passin pass:$KEY_STORE_PWD -nodes -out $SERVER_EDDSA_KEY_STORE_PEM
+      if [ -s $SERVER_EDDSA_KEY_STORE_P12 ] ; then 
+         openssl pkcs12 -in $SERVER_EDDSA_KEY_STORE_P12 -passin pass:$KEY_STORE_PWD -nodes -out $SERVER_EDDSA_KEY_STORE_PEM
+      fi
       openssl pkcs12 -in $CLIENT_KEY_STORE_P12 -passin pass:$KEY_STORE_PWD -nodes -out $CLIENT_KEY_STORE_PEM
       openssl pkcs12 -in $CLIENT_RSA_KEY_STORE_P12 -passin pass:$KEY_STORE_PWD -nodes -out $CLIENT_RSA_KEY_STORE_PEM
-      openssl pkcs12 -in $CLIENT_EDDSA_KEY_STORE_P12 -passin pass:$KEY_STORE_PWD -nodes -out $CLIENT_EDDSA_KEY_STORE_PEM
+      if [ -s $SERVER_EDDSA_KEY_STORE_P12 ] ; then 
+         openssl pkcs12 -in $CLIENT_EDDSA_KEY_STORE_P12 -passin pass:$KEY_STORE_PWD -nodes -out $CLIENT_EDDSA_KEY_STORE_PEM
+      fi
       openssl pkcs12 -in $ROOT_TRUST_STORE_P12 -passin pass:$TRUST_STORE_PWD -nokeys -out $ROOT_TRUST_STORE_PEM
       openssl pkcs12 -in $CA_TRUST_STORE_P12 -passin pass:$TRUST_STORE_PWD -nokeys -out $CA_TRUST_STORE_PEM
       openssl pkcs12 -in $CA_RSA_TRUST_STORE_P12 -passin pass:$TRUST_STORE_PWD -nokeys -out $CA_RSA_TRUST_STORE_PEM
+      if [ -s $CA_EDDSA_TRUST_STORE_P12 ] ; then 
+         openssl pkcs12 -in $CA_EDDSA_TRUST_STORE_P12 -passin pass:$TRUST_STORE_PWD -nodes -out $CA_EDDSA_TRUST_STORE_PEM
+      fi
       openssl pkcs12 -in $TRUST_STORE_P12 -passin pass:$TRUST_STORE_PWD -nokeys -out $TRUST_STORE_PEM
       openssl ecparam -genkey -name prime256v1 -noout -out $EC_PRIVATE_KEY_PEM
       openssl ec -in $EC_PRIVATE_KEY_PEM -pubout -out $EC_PUBLIC_KEY_PEM
@@ -368,6 +408,7 @@ copy_pem() {
   cp $ROOT_TRUST_STORE_PEM $DESTINATION_DIR
   cp $CA_TRUST_STORE_PEM $DESTINATION_DIR
   cp $CA_RSA_TRUST_STORE_PEM $DESTINATION_DIR
+  cp $CA_EDDSA_TRUST_STORE_PEM $DESTINATION_DIR
   cp $CLIENT_KEY_STORE_PEM $DESTINATION_DIR
   cp $CLIENT_RSA_KEY_STORE_PEM $DESTINATION_DIR
   cp $CLIENT_EDDSA_KEY_STORE_PEM $DESTINATION_DIR
