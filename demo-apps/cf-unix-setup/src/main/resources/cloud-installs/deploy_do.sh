8e6e48e1bbc219fffe5be944503c3c957a8491b9
Achim Kraus <achim.kraus@bosch.io>
2021-11-16 12:05:14 +0100

    Add firewall setup for digitalocean.
    Signed-off-by: Achim Kraus <achim.kraus@bosch.io>

--- demo-apps/cf-unix-setup/src/main/resources/cloud-installs/deploy_do.sh
+++ demo-apps/cf-unix-setup/src/main/resources/cloud-installs/deploy_do.sh
@@ -34,6 +34,7 @@ if [ "$1" = "create" ]  ; then
    echo "create digitalocean server"
    
    doctl compute droplet create cali \
+    --tag-name cali \
     --image "ubuntu-20-04-x64" \
     --enable-ipv6 \
     --region "fra1" \
@@ -41,11 +42,17 @@ if [ "$1" = "create" ]  ; then
     --ssh-keys "${ssh_key_id}" \
     --user-data-file "cloud-config.yaml" 	
 
+   doctl compute firewall create \
+     --name cali \
+     --tag-names cali \
+     --inbound-rules="protocol:tcp,ports:22,address:0.0.0.0/0,address:::/0 protocol:udp,ports:5684,address:0.0.0.0/0,address:::/0" \
+     --outbound-rules="protocol:tcp,ports:all,address:0.0.0.0/0,address:::/0 protocol:udp,ports:all,address:0.0.0.0/0,address:::/0 protocol:icmp,address:0.0.0.0/0,address:::/0"
+
    echo "wait to give vm time to finish the installation!"
    sleep 30 
 	
-   doctl compute droplet get cali --format PublicIPv4,PublicIPv6
-
+   doctl compute droplet get cali --format ID,PublicIPv4,PublicIPv6
+   
    ip=$(doctl compute droplet get cali --template {{.PublicIPv4}})
 
    echo "use: ssh root@${ip} to login!"
@@ -57,6 +64,13 @@ if [ "$1" = "delete" ]  ; then
    echo "delete digitalocean server"
 
    doctl compute droplet delete cali
+
+   id=$(doctl compute firewall list --format "ID,Name" | grep cali | cut -sd ' ' -f 1)
+
+   if [ -n "${id}" ] ; then
+      echo "delete digitalocean fw ${id}"
+      doctl compute firewall delete ${id}
+   fi
 
    exit 0
 fi
