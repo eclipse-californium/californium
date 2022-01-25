107fd6d8d324aa5e9c23fb8a943dcdf4564d833e
Achim Kraus <achim.kraus@bosch.io>
2022-01-25 18:24:39 +0100

    Cleanup LinkFormat.
    Add unit tests.
    Extend ResourceAttributes with copy function.
    
    Signed-off-by: Achim Kraus <achim.kraus@bosch.io>

--- californium-core/src/main/java/org/eclipse/californium/core/server/resources/ResourceAttributes.java
+++ californium-core/src/main/java/org/eclipse/californium/core/server/resources/ResourceAttributes.java
@@ -33,20 +33,20 @@ import org.eclipse.californium.core.coap.LinkFormat;
  * such as title, resource type or interface description. These attributes will
  * also be included in the link description of the resource they belong to. For
  * example, if a title was specified, the link description for a sensor resource
- * might look like this <code>&lt;/sensors&gt;;title="Sensor Index"</code>.
+ * might look like this {@code </sensors>;title="Sensor Index"}.
  */
 public class ResourceAttributes {
-	
+
 	/** Contains the resource's attributes specified in the CoRE Link Format. */
 	private final ConcurrentMap<String, AttributeValues> attributes;
-	
+
 	/**
 	 * Instantiates a new resource attributes.
 	 */
 	public ResourceAttributes() {
 		attributes = new ConcurrentHashMap<String, AttributeValues>();
 	}
-	
+
 	/**
 	 * Gets the number of attributes.
 	 *
@@ -55,11 +55,11 @@ public class ResourceAttributes {
 	public int getCount() {
 		return attributes.size();
 	}
-	
+
 	/**
 	 * Gets the resource title.
 	 *
-	 * @return the title
+	 * @return the title. {@code null}, if not available.
 	 */
 	public String getTitle() {
 		if (containsAttribute(LinkFormat.TITLE)) {
@@ -77,7 +77,7 @@ public class ResourceAttributes {
 	public void setTitle(String title) {
 		findAttributeValues(LinkFormat.TITLE).setOnly(title);
 	}
-	
+
 	/**
 	 * Adds a resource type.
 	 *
@@ -86,14 +86,7 @@ public class ResourceAttributes {
 	public void addResourceType(String type) {
 		findAttributeValues(LinkFormat.RESOURCE_TYPE).add(type);
 	}
-	
-	/**
-	 * Clear all resource types.
-	 */
-	public void clearResourceType() {
-		attributes.remove(LinkFormat.RESOURCE_TYPE);
-	}
-	
+
 	/**
 	 * Gets all resource types.
 	 *
@@ -102,7 +95,14 @@ public class ResourceAttributes {
 	public List<String> getResourceTypes() {
 		return getAttributeValues(LinkFormat.RESOURCE_TYPE);
 	}
-	
+
+	/**
+	 * Clear all resource types.
+	 */
+	public void clearResourceType() {
+		attributes.remove(LinkFormat.RESOURCE_TYPE);
+	}
+
 	/**
 	 * Adds an interface description.
 	 *
@@ -111,7 +111,7 @@ public class ResourceAttributes {
 	public void addInterfaceDescription(String description) {
 		findAttributeValues(LinkFormat.INTERFACE_DESCRIPTION).add(description);
 	}
-	
+
 	/**
 	 * Gets all interface descriptions.
 	 *
@@ -120,7 +120,16 @@ public class ResourceAttributes {
 	public List<String> getInterfaceDescriptions() {
 		return getAttributeValues(LinkFormat.INTERFACE_DESCRIPTION);
 	}
-	
+
+	/**
+	 * Clear all interface descriptions.
+	 * 
+	 * @since 3.3
+	 */
+	public void clearInterfaceDescriptions() {
+		attributes.remove(LinkFormat.RESOURCE_TYPE);
+	}
+
 	/**
 	 * Sets the maximum size estimate.
 	 *
@@ -129,7 +138,7 @@ public class ResourceAttributes {
 	public void setMaximumSizeEstimate(String size) {
 		findAttributeValues(LinkFormat.MAX_SIZE_ESTIMATE).setOnly(size);
 	}
-	
+
 	/**
 	 * Sets the maximum size estimate.
 	 *
@@ -138,7 +147,7 @@ public class ResourceAttributes {
 	public void setMaximumSizeEstimate(int size) {
 		findAttributeValues(LinkFormat.MAX_SIZE_ESTIMATE).setOnly(Integer.toString(size));
 	}
-	
+
 	/**
 	 * Gets the maximum size estimate.
 	 *
@@ -147,7 +156,7 @@ public class ResourceAttributes {
 	public String getMaximumSizeEstimate() {
 		return findAttributeValues(LinkFormat.MAX_SIZE_ESTIMATE).getFirst();
 	}
-	
+
 	/**
 	 * Adds a content type specified by an integer.
 	 *
@@ -156,7 +165,7 @@ public class ResourceAttributes {
 	public void addContentType(int type) {
 		findAttributeValues(LinkFormat.CONTENT_TYPE).add(Integer.toString(type));
 	}
-	
+
 	/**
 	 * Gets all content types as list.
 	 *
@@ -165,21 +174,21 @@ public class ResourceAttributes {
 	public List<String> getContentTypes() {
 		return getAttributeValues(LinkFormat.CONTENT_TYPE);
 	}
-	
+
 	/**
 	 * Clear all content types.
 	 */
 	public void clearContentType() {
 		attributes.remove(LinkFormat.CONTENT_TYPE);
 	}
-	
+
 	/**
 	 * Marks the resource as observable.
 	 */
 	public void setObservable() {
 		findAttributeValues(LinkFormat.OBSERVABLE).setOnly("");
 	}
-	
+
 	/**
 	 * Checks if the resource is observable.
 	 *
@@ -188,7 +197,7 @@ public class ResourceAttributes {
 	public boolean hasObservable() {
 		return !getAttributeValues(LinkFormat.OBSERVABLE).isEmpty();
 	}
-	
+
 	/**
 	 * Replaces the value for the specified attribute with the specified value.
 	 * If another value has been set for the attribute name, it will be removed.
@@ -199,7 +208,7 @@ public class ResourceAttributes {
 	public void setAttribute(String attr, String value) {
 		findAttributeValues(attr).setOnly(value);
 	}
-	
+
 	/**
 	 * Adds an arbitrary attribute with no value.
 	 *
@@ -208,7 +217,7 @@ public class ResourceAttributes {
 	public void addAttribute(String attr) {
 		addAttribute(attr, "");
 	}
-	
+
 	/**
 	 * Adds the specified value to the other values of the specified attribute
 	 * name.
@@ -219,7 +228,17 @@ public class ResourceAttributes {
 	public void addAttribute(String attr, String value) {
 		findAttributeValues(attr).add(value);
 	}
-	
+
+	/**
+	 * Checks, if the specified attribute is available.
+	 *
+	 * @param attr the attribute
+	 * @return {@code true}, if available, {@code false}, otherwise.
+	 */
+	public boolean containsAttribute(String attr) {
+		return attributes.containsKey(attr);
+	}
+
 	/**
 	 * Removes all values for the specified attribute
 	 *
@@ -228,48 +247,68 @@ public class ResourceAttributes {
 	public void clearAttribute(String attr) {
 		attributes.remove(attr);
 	}
-	
+
 	/**
-	 * Returns <tt>true</tt> if this object contains the specified attribute.
-	 *
-	 * @param attr the attribute
-	 * @return true, if successful
-	 */
-	public boolean containsAttribute(String attr) {
-		return attributes.containsKey(attr);
-	}
-	
-	/**
-	 * Returns a {@link Set} view of the attribute names. If the map is modified
-	 * while an iteration over the set is in progress (except through the
-	 * iterator's own <tt>remove</tt> operation), the results of the iteration
-	 * are undefined. The set supports element removal, which removes the
-	 * corresponding mapping from the map, via the <tt>Iterator.remove</tt>,
-	 * <tt>Set.remove</tt>, <tt>removeAll</tt>, <tt>retainAll</tt>, and
-	 * <tt>clear</tt> operations. It does not support the <tt>add</tt> or
-	 * <tt>addAll</tt> operations.
+	 * Returns a {@link Set} view of the attribute names.
+	 * 
+	 * If the map is modified while an iteration over the set is in progress
+	 * (except through the iterator's own <tt>remove</tt> operation), the
+	 * results of the iteration are undefined. The set supports element removal,
+	 * which removes the corresponding mapping from the map, via the
+	 * <tt>Iterator.remove</tt>, <tt>Set.remove</tt>, <tt>removeAll</tt>,
+	 * <tt>retainAll</tt>, and <tt>clear</tt> operations. It does not support
+	 * the <tt>add</tt> or <tt>addAll</tt> operations.
 	 * 
 	 * @return a set view of the attribute names
 	 */
 	public Set<String> getAttributeKeySet() {
 		return attributes.keySet();
 	}
-	
+
 	/**
 	 * Gets all values for the specified attribute.
 	 *
-	 * @param attr the atrribute
-	 * @return the attribute values
+	 * @param attr the attribute
+	 * @return the attribute values. If no values available, return a empty
+	 *         list.
 	 */
 	public List<String> getAttributeValues(String attr) {
 		AttributeValues list = attributes.get(attr);
-		if (list != null) return list.getAll();
-		else return Collections.emptyList();
+		if (list != null)
+			return list.getAll();
+		else
+			return Collections.emptyList();
 	}
-	
+
+	/**
+	 * Copy provided resource attributes.
+	 * 
+	 * @param other other resource attributes
+	 * @since 3.3
+	 */
+	public void copy(ResourceAttributes other) {
+		if (this == other) {
+			return;
+		}
+		attributes.clear();
+		for (String attrName : other.getAttributeKeySet()) {
+			List<String> values = other.getAttributeValues(attrName);
+			if (values.isEmpty()) {
+				addAttribute(attrName);
+			} else {
+				for (String attrValue : values) {
+					addAttribute(attrName, attrValue);
+				}
+			}
+		}
+	}
+
 	/**
 	 * Find the attribute values for the specified attribute.
 	 *
+	 * If not available, create new attribute values using the provided
+	 * attribute name.
+	 * 
 	 * @param attr the attribute
 	 * @return the attribute values
 	 */
@@ -278,21 +317,22 @@ public class ResourceAttributes {
 		if (list == null) {
 			list = new AttributeValues();
 			AttributeValues prev = attributes.putIfAbsent(attr, list);
-			if (prev != null) return prev;
+			if (prev != null) {
+				return prev;
+			}
 		}
 		return list;
 	}
-	
+
 	/**
 	 * The class AttributeValues contains a list of all values for a specific
 	 * attribute.
 	 */
 	private final static class AttributeValues {
-		 
+
 		/** The list. */
-		private final List<String> list = 
-				Collections.synchronizedList(new LinkedList<String>());
-		
+		private final List<String> list = Collections.synchronizedList(new LinkedList<String>());
+
 		/**
 		 * Gets all values.
 		 *
@@ -301,7 +341,7 @@ public class ResourceAttributes {
 		private List<String> getAll() {
 			return list;
 		}
-		
+
 		/**
 		 * Adds the specified value to the list.
 		 *
@@ -310,17 +350,19 @@ public class ResourceAttributes {
 		private void add(String value) {
 			list.add(value);
 		}
-		
+
 		/**
 		 * Gets the first value of the list.
 		 *
 		 * @return the first value
 		 */
 		private synchronized String getFirst() {
-			if (list.isEmpty()) return "";
-			else return list.get(0);
+			if (list.isEmpty())
+				return "";
+			else
+				return list.get(0);
 		}
-		
+
 		/**
 		 * Adds the specified value but removes all others.
 		 *
---
+++ californium-core/src/test/java/org/eclipse/californium/core/coap/LinkFormatTest.java
@@ -0,0 +1,171 @@
+/*******************************************************************************
+ * Copyright (c) 2022 Bosch IO GmbH and others.
+ * 
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v2.0
+ * and Eclipse Distribution License v1.0 which accompany this distribution.
+ * 
+ * The Eclipse Public License is available at
+ *    http://www.eclipse.org/legal/epl-v20.html
+ * and the Eclipse Distribution License is available at
+ *    http://www.eclipse.org/org/documents/edl-v10.html.
+ * 
+ * Contributors:
+ *    Bosch IO GmbH - initial creation
+ ******************************************************************************/
+package org.eclipse.californium.core.coap;
+
+import static org.eclipse.californium.core.coap.MediaTypeRegistry.APPLICATION_CBOR;
+import static org.eclipse.californium.core.coap.MediaTypeRegistry.APPLICATION_JSON;
+import static org.eclipse.californium.core.coap.MediaTypeRegistry.APPLICATION_XML;
+import static org.eclipse.californium.core.coap.MediaTypeRegistry.TEXT_PLAIN;
+import static org.hamcrest.CoreMatchers.containsString;
+import static org.hamcrest.CoreMatchers.hasItem;
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.CoreMatchers.not;
+import static org.hamcrest.CoreMatchers.notNullValue;
+import static org.hamcrest.CoreMatchers.nullValue;
+import static org.hamcrest.MatcherAssert.assertThat;
+
+import java.util.Arrays;
+import java.util.Set;
+
+import org.eclipse.californium.core.CoapResource;
+import org.eclipse.californium.core.WebLink;
+import org.eclipse.californium.core.server.resources.ResourceAttributes;
+import org.eclipse.californium.elements.category.Small;
+import org.eclipse.californium.elements.rule.TestNameLoggerRule;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.experimental.categories.Category;
+
+/**
+ * Unit test cases validating behavior of the {@link LinkFormat} class.
+ *
+ */
+@Category(Small.class)
+public class LinkFormatTest {
+
+	@Rule
+	public TestNameLoggerRule name = new TestNameLoggerRule();
+
+	@Test
+	public void testSerialize() {
+		CoapResource node = new CoapResource("node");
+		node.add(new CoapResource("child1"));
+		CoapResource child = new CoapResource("child2");
+		node.add(child);
+		node.add(new CoapResource("child3"));
+		child.add(new CoapResource("grandchild"));
+
+		String tree = LinkFormat.serializeTree(node);
+		assertThat(tree, containsString("<node/child1>"));
+		assertThat(tree, containsString("<node/child2>"));
+		assertThat(tree, containsString("<node/child2/grandchild>"));
+	}
+
+	@Test
+	public void testSerializeWithAttributes() {
+		CoapResource node = new CoapResource("node");
+		CoapResource child = new CoapResource("child");
+		child.getAttributes().setTitle("Children of the Node");
+		child.getAttributes().setMaximumSizeEstimate(4096);
+		child.getAttributes().setObservable();
+		child.getAttributes().addContentType(TEXT_PLAIN);
+		child.getAttributes().addContentType(APPLICATION_JSON);
+		child.getAttributes().addContentType(APPLICATION_XML);
+		child.getAttributes().addResourceType("test");
+		node.add(child);
+		node.add(new CoapResource("next"));
+
+		String tree = LinkFormat.serializeTree(node);
+		assertThat(tree, containsString("<node/child>"));
+		assertThat(tree, containsString(";ct=\"0 50 41\""));
+		assertThat(tree, containsString(";rt=\"test\""));
+		assertThat(tree, containsString(";sz=4096"));
+		assertThat(tree, containsString(";title=\"Children of the Node\""));
+	}
+
+	@Test
+	public void testParse() {
+		String tree = "<node/child2>,<node/child2/grandchild>,<node/child3>,<node/child1>";
+
+		Set<WebLink> links = LinkFormat.parse(tree);
+		assertThat(WebLink.findByUri(links, "node/child1"), is(notNullValue()));
+		assertThat(WebLink.findByUri(links, "node/child2"), is(notNullValue()));
+		assertThat(WebLink.findByUri(links, "node/child2/grandchild"), is(notNullValue()));
+		assertThat(WebLink.findByUri(links, "node"), is(nullValue()));
+	}
+
+	@Test
+	public void testParseWithAttributes() {
+		String tree = "<node/child>;ct=\"0 50 41\";obs;rt=\"test\";sz=4096;title=\"Children of the Node\",<node/next>";
+
+		Set<WebLink> links = LinkFormat.parse(tree);
+		assertThat(WebLink.findByUri(links, "node"), is(nullValue()));
+		WebLink link = WebLink.findByUri(links, "node/child");
+		assertThat(link, is(notNullValue()));
+		ResourceAttributes attributes = link.getAttributes();
+		assertThat(attributes.getTitle(), is("Children of the Node"));
+		assertThat(attributes.getMaximumSizeEstimate(), is("4096"));
+		assertThat(attributes.getResourceTypes(), hasItem("test"));
+		assertThat(attributes.getContentTypes(), hasItem("0"));
+		assertThat(attributes.getContentTypes(), hasItem("50"));
+		assertThat(attributes.getContentTypes(), hasItem("41"));
+	}
+
+	@Test
+	public void testSerializeWithMatchingLink() {
+		CoapResource node = new CoapResource("node");
+		CoapResource child = new CoapResource("child1");
+		child.getAttributes().setTitle("Children of the Node");
+		child.getAttributes().setMaximumSizeEstimate(4096);
+		child.getAttributes().setObservable();
+		child.getAttributes().addContentType(TEXT_PLAIN);
+		child.getAttributes().addContentType(APPLICATION_XML);
+		child.getAttributes().addResourceType("test-in");
+		node.add(child);
+		child = new CoapResource("child2");
+		child.getAttributes().setTitle("Children of the Node");
+		child.getAttributes().addContentType(TEXT_PLAIN);
+		child.getAttributes().addContentType(APPLICATION_JSON);
+		child.getAttributes().addResourceType("test-out");
+		node.add(child);
+
+		// wildcard
+		String tree = LinkFormat.serializeTree(node, Arrays.asList("rt=test*"));
+		assertThat(tree, containsString("<node/child1>"));
+		assertThat(tree, containsString("<node/child2>"));
+
+		tree = LinkFormat.serializeTree(node, Arrays.asList("rt=test"));
+		assertThat(tree, is(""));
+
+		tree = LinkFormat.serializeTree(node, Arrays.asList("rt=test-in"));
+		assertThat(tree, containsString("<node/child1>"));
+		assertThat(tree, not(containsString("<node/child2>")));
+
+		// * not at the end => no wildcard
+		tree = LinkFormat.serializeTree(node, Arrays.asList("rt=te*t-in"));
+		assertThat(tree, is(""));
+
+		tree = LinkFormat.serializeTree(node, Arrays.asList("href=node/child2"));
+		assertThat(tree, not(containsString("<node/child1>")));
+		assertThat(tree, containsString("<node/child2>"));
+
+		tree = LinkFormat.serializeTree(node, Arrays.asList("href=node/child*"));
+		assertThat(tree, containsString("<node/child1>"));
+		assertThat(tree, containsString("<node/child2>"));
+
+		tree = LinkFormat.serializeTree(node, Arrays.asList("ct=" + APPLICATION_XML));
+		assertThat(tree, containsString("<node/child1>"));
+		assertThat(tree, not(containsString("<node/child2>")));
+
+		tree = LinkFormat.serializeTree(node, Arrays.asList("ct=" + TEXT_PLAIN));
+		assertThat(tree, containsString("<node/child1>"));
+		assertThat(tree, containsString("<node/child2>"));
+
+		tree = LinkFormat.serializeTree(node, Arrays.asList("ct=" + APPLICATION_CBOR));
+		assertThat(tree, is(""));
+	}
+
+}
