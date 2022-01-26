bc953f81833060adcf5f21a79539187099e484ba
Achim Kraus <achim.kraus@bosch.io>
2022-01-26 08:11:09 +0100

    Extend WebLink.
    Add non-standard attributes to toString().
    Add findByUri().
    
    Signed-off-by: Achim Kraus <achim.kraus@bosch.io>

--- californium-core/src/main/java/org/eclipse/californium/core/WebLink.java
+++ californium-core/src/main/java/org/eclipse/californium/core/WebLink.java
@@ -19,6 +19,10 @@
  ******************************************************************************/
 package org.eclipse.californium.core;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.Collections;
 import java.util.List;
 
 import org.eclipse.californium.core.coap.LinkFormat;
@@ -26,29 +30,31 @@ import org.eclipse.californium.core.server.resources.ResourceAttributes;
 import org.eclipse.californium.elements.util.StringUtil;
 
 /**
- * The WebLink class can be used to programmatically browse a remote CoAP endoint.
- * It uses the {@link ResourceAttributes} class to hold the CoRE Link Format attributes.
- * The {@link CoapClient#discover()} method returns a list of WebLinks for this.
+ * The WebLink class can be used to programmatically browse a remote CoAP
+ * endoint. It uses the {@link ResourceAttributes} class to hold the CoRE Link
+ * Format attributes. The {@link CoapClient#discover()} method returns a list of
+ * WebLinks for this.
  * 
  * TODO: Add support for absolute URIs and URI checking.
  */
 public class WebLink implements Comparable<WebLink> {
+
 	private String uri;
 	private final ResourceAttributes attributes;
-	
+
 	public WebLink(String uri) {
 		this.uri = uri;
 		this.attributes = new ResourceAttributes();
 	}
-	
+
 	public String getURI() {
 		return this.uri;
 	}
-	
+
 	public ResourceAttributes getAttributes() {
 		return attributes;
 	}
-	
+
 	/**
 	 * Renders the Web link information as a multi-line string, which can be
 	 * displayed in console clients.
@@ -61,15 +67,24 @@ public class WebLink implements Comparable<WebLink> {
 		builder.append('<');
 		builder.append(this.uri);
 		builder.append('>');
+		List<String> attributes = new ArrayList<>(this.attributes.getAttributeKeySet());
+		List<String> standard = Arrays.asList(LinkFormat.RESOURCE_TYPE, LinkFormat.INTERFACE_DESCRIPTION,
+				LinkFormat.CONTENT_TYPE, LinkFormat.MAX_SIZE_ESTIMATE, LinkFormat.OBSERVABLE);
 		if (this.attributes.containsAttribute(LinkFormat.TITLE)) {
 			builder.append(' ').append(this.attributes.getTitle());
 		}
-		append(builder, LinkFormat.RESOURCE_TYPE);
-		append(builder, LinkFormat.INTERFACE_DESCRIPTION);
-		append(builder, LinkFormat.CONTENT_TYPE);
-		append(builder, LinkFormat.MAX_SIZE_ESTIMATE);
-		append(builder, LinkFormat.OBSERVABLE);
+		append(builder, standard);
+		attributes.remove(LinkFormat.TITLE);
+		attributes.removeAll(standard);
+		Collections.sort(attributes);
+		append(builder, attributes);
 		return builder.toString();
+	}
+
+	private void append(StringBuilder builder, List<String> attributes) {
+		for (String attribute : attributes) {
+			append(builder, attribute);
+		}
 	}
 
 	private void append(StringBuilder builder, String attributeName) {
@@ -81,9 +96,27 @@ public class WebLink implements Comparable<WebLink> {
 			}
 		}
 	}
-	
+
 	@Override
 	public int compareTo(WebLink other) {
 		return this.uri.compareTo(other.getURI());
 	}
+
+	/**
+	 * Find {@link WebLink} by URI.
+	 * 
+	 * @param links collection of links
+	 * @param uri URI
+	 * @return web-link matching the URI
+	 * @since 3.3
+	 */
+	public static WebLink findByUri(Collection<WebLink> links, String uri) {
+		for (WebLink link : links) {
+			if (link.getURI().equals(uri)) {
+				return link;
+			}
+		}
+		return null;
+	}
+
 }
