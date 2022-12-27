130e0c6a54619240324974033a13f955175b0456
Bartłomiej Wiśniewski <b.wisniewski@avsystem.com>
2022-12-27 09:23:35 +0100

    fix copyright template

--- scandium-core/src/main/java/org/eclipse/californium/scandium/DatagramFilterExtended.java
+++ scandium-core/src/main/java/org/eclipse/californium/scandium/DatagramFilterExtended.java
@@ -1,17 +1,17 @@
 /*******************************************************************************
- * Copyright (c) 2022 Bosch.IO GmbH and others.
- * 
+ * Copyright (c) 2022 AVSystem and others.
+ *
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v2.0
  * and Eclipse Distribution License v1.0 which accompany this distribution.
- * 
+ *
  * The Eclipse Public License is available at
  *    http://www.eclipse.org/legal/epl-v20.html
  * and the Eclipse Distribution License is available at
  *    http://www.eclipse.org/org/documents/edl-v10.html.
- * 
+ *
  * Contributors:
- *    Bosch IO.GmbH - initial creation
+ *    Bartłomiej Wiśniewski (AVSystem) - initial creation
  ******************************************************************************/
 package org.eclipse.californium.scandium;
 
@@ -19,7 +19,6 @@ import org.eclipse.californium.elements.util.PublicAPIExtension;
 import org.eclipse.californium.scandium.dtls.Record;
 
 import java.net.DatagramPacket;
-import java.net.InetSocketAddress;
 
 /**
  * Extension of DatagramFilter
