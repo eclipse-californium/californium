7ae4efa25eee31d0ae33073bc57094321e574382
Achim Kraus <achim.kraus@cloudcoap.net>
2024-10-10 09:10:56 +0200

    Remove reducing the HelloVerifyRequests for specific cases.
    Signed-off-by: Achim Kraus <achim.kraus@cloudcoap.net>

--- scandium-core/src/main/java/org/eclipse/californium/scandium/dtls/resumption/ConnectionStoreResumptionVerifier.java
+++ scandium-core/src/main/java/org/eclipse/californium/scandium/dtls/resumption/ConnectionStoreResumptionVerifier.java
@@ -16,16 +16,12 @@
 package org.eclipse.californium.scandium.dtls.resumption;
 
 import org.eclipse.californium.scandium.DTLSConnector;
-import org.eclipse.californium.scandium.dtls.ClientHello;
 import org.eclipse.californium.scandium.dtls.ConnectionId;
 import org.eclipse.californium.scandium.dtls.DTLSSession;
-import org.eclipse.californium.scandium.dtls.ExtendedMasterSecretMode;
 import org.eclipse.californium.scandium.dtls.HandshakeResultHandler;
-import org.eclipse.californium.scandium.dtls.ResumingServerHandshaker;
 import org.eclipse.californium.scandium.dtls.ResumptionSupportingConnectionStore;
 import org.eclipse.californium.scandium.dtls.ResumptionVerificationResult;
 import org.eclipse.californium.scandium.dtls.SessionId;
-import org.eclipse.californium.scandium.util.SecretUtil;
 import org.eclipse.californium.scandium.util.ServerNames;
 
 /**
@@ -39,7 +35,7 @@ import org.eclipse.californium.scandium.util.ServerNames;
  * 
  * @since 3.0
  */
-public class ConnectionStoreResumptionVerifier implements ExtendedResumptionVerifier {
+public class ConnectionStoreResumptionVerifier implements ResumptionVerifier {
 
 	/**
 	 * Connection store to lookup the dtls session.
@@ -84,34 +80,6 @@ public class ConnectionStoreResumptionVerifier implements ExtendedResumptionVeri
 			throw new NullPointerException("Connection store must not be null!");
 		}
 		this.connectionStore = connectionStore;
-	}
-
-	@Override
-	public boolean skipRequestHelloVerify(SessionId sessionId) {
-		boolean result = false;
-		ResumptionSupportingConnectionStore store = connectionStore;
-		if (store != null) {
-			DTLSSession session = store.find(sessionId);
-			result = session != null;
-			SecretUtil.destroy(session);
-		}
-		return result;
-	}
-
-	@Override
-	public boolean skipRequestHelloVerify(ClientHello clientHello, boolean sniEnabled,
-			ExtendedMasterSecretMode extendedMasterSecretMode) {
-		boolean result = false;
-		ResumptionSupportingConnectionStore store = connectionStore;
-		if (store != null) {
-			DTLSSession session = store.find(clientHello.getSessionId());
-			if (session != null) {
-				result = ResumingServerHandshaker.validateResumption(session, clientHello, sniEnabled,
-						extendedMasterSecretMode);
-				SecretUtil.destroy(session);
-			}
-		}
-		return result;
 	}
 
 	@Override
